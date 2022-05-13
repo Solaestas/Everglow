e856b45e11c3fff9123092065f7c761a8b3a04b6
ye_you <28271655@qq.com>
2022-05-14 00:58:27 +0800

    简化new : slient

--- Everglow-UnitTest/Commons/ModuleManagerTest.cs
+++ Everglow-UnitTest/Commons/ModuleManagerTest.cs
@@ -1,17 +1,18 @@
 using Everglow.Sources.Commons;
 using Moq;
 using Microsoft.VisualStudio.TestTools.UnitTesting;
+using Everglow.Sources.Commons.ModuleSystem;
 
 namespace Everglow_UnitTest
 {
     [TestClass]
     public class ModuleManagerTest
     {
-        //private readonly ModuleManager m_moduleManager;
+        private readonly ModuleManager m_moduleManager;
 
         public ModuleManagerTest()
         {
-            ModuleManager.Instance.Load();
+            m_moduleManager = new ModuleManager();
         }
 
         [TestMethod]
@@ -21,10 +22,10 @@ namespace Everglow_UnitTest
             mockModule.Setup(m => m.Name).Returns("Test");
 
             // Add a module named "Test"
-            ModuleManager.AddModule(mockModule.Object);
+            m_moduleManager.AddModule(mockModule.Object);
 
             // Should be able to get the object
-            var module = ModuleManager.GetModule("Test");
+            var module = m_moduleManager.GetModule("Test");
 
             Assert.AreEqual("Test", module.Name);
         }
