163e6cd43c4fd791cf0650654dee1fe212ae800a
Mr.Skirt <bdfzchen@163.com>
2022-05-13 14:29:37 +0800

    Added test files

---
+++ Sources/Commons/IModule.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Everglow.Sources.Commons
+{
+    internal interface IModule
+    {
+        public string Name { get; }
+        public string Description { get; }
+
+        public void Load();
+
+        public void Unload();
+    }
+}
---
+++ Sources/Modules/Test/Class1.cs
@@ -0,0 +1,12 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Everglow.Sources.Modules.Test
+{
+    internal class Class1
+    {
+    }
+}
