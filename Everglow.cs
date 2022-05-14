a912d2c52db82853de17808853b93a69194fd014
Mr.Skirt <bdfzchen@163.com>
2022-05-14 15:48:49 +0800

    fix using

--- Everglow.cs
+++ Everglow.cs
@@ -1,7 +1,19 @@
+global using Microsoft.Xna.Framework;
+global using Microsoft.Xna.Framework.Graphics;
+global using Mono.Cecil.Cil;
+global using MonoMod.Cil;
+global using System;
 global using System.Collections.Generic;
+global using System.Diagnostics;
+global using System.IO;
+global using System.Linq;
+global using System.Reflection;
 global using Terraria;
+global using Terraria.DataStructures;
+global using Terraria.GameContent;
 global using Terraria.ID;
 global using Terraria.ModLoader;
+global using ReLogic.Content;
 
 global using Item_id = System.Int32;
 global using Buff_id = System.Int32;
