7a91be0b8e79ef03017506959205b858a12727dc
Omnielement <877183629@qq.com>
2022-05-14 23:22:29 +0800

    Add some UI texture.Add the test item to trigger Lantern Moon. Add LanternMoon framework.

---
+++ Sources/Modules/MythModule/Common/MythContent.cs
@@ -0,0 +1,30 @@
+﻿using Microsoft.Xna.Framework.Graphics;
+
+namespace Everglow.Sources.Modules.MythModule.Common
+{
+    public class MythContent
+    { 
+        public static Texture2D QuickTexture(string path)
+        {
+            return ModContent.Request<Texture2D>("Everglow/Sources/Modules/MythModule/" + path).Value;//对于神话模块专用的获取图片封装
+        }
+        public static short SetStaticDefaultsGlowMask(ModItem modItem)
+        {
+            if (!Terraria.Main.dedServ)
+            {
+                ReLogic.Content.Asset<Texture2D>[] glowMasks = new ReLogic.Content.Asset<Texture2D>[TextureAssets.GlowMask.Length + 1];
+                for (int i = 0; i < TextureAssets.GlowMask.Length; i++)
+                {
+                    glowMasks[i] = TextureAssets.GlowMask[i];
+                }
+                glowMasks[glowMasks.Length - 1] = ModContent.Request<Texture2D>("Everglow/Sources/Modules/MythModule/Glows/" + modItem.GetType().Name + "_glow");
+                TextureAssets.GlowMask = glowMasks;
+                return (short)(glowMasks.Length - 1);
+            }
+            else
+            {
+                return 0;
+            }
+        }
+    }
+}
---
+++ Sources/Modules/MythModule/LanternMoon/Projectiles/BloodLamp.cs
@@ -0,0 +1,169 @@
+using Terraria.Audio;
+using Terraria.Localization;
+using Everglow.Sources.Modules.MythModule.Common;
+namespace Everglow.Sources.Modules.MythModule.LanternMoon.Projectiles
+{
+    public class BloodLamp : ModProjectile
+    {
+        public override void SetStaticDefaults()
+        {
+            DisplayName.SetDefault("Blood Lamp");
+        }
+        public override void SetDefaults()
+        {
+            Projectile.width = 38;
+            Projectile.height = 60;
+            Projectile.aiStyle = -1;
+            Projectile.friendly = false;
+            Projectile.hostile = false;
+            Projectile.ignoreWater = true;
+            Projectile.tileCollide = false;
+            Projectile.timeLeft = 600;
+            Projectile.scale = 1;
+        }
+        Texture2D[] BLantern = new Texture2D[16];
+        bool[] NoPedal = new bool[16];
+        float PearlRot = 0;
+        float PearlOmega = 0;
+        public override ModProjectile Clone(Projectile projectile)
+        {
+            var clone = base.Clone(projectile) as BloodLamp;
+            BLantern = new Texture2D[16];
+            NoPedal = new bool[16];
+            PearlRot = 0;
+            PearlOmega = 0;
+            return clone;
+        }
+        public override void AI()
+        {
+            Projectile.rotation = Projectile.velocity.X * 0.05f;
+            Projectile.velocity *= 0.9f * Projectile.timeLeft / 600f;
+            if (Projectile.velocity.Length() > 0.3f)
+            {
+                Projectile.velocity.Y -= 0.25f * Projectile.timeLeft / 600f;
+            }
+            Vector2 Cen = new Vector2(19f, 19f);
+            if (Projectile.timeLeft < 10)
+            {
+                for (int x = 1; x < 16; x++)
+                {
+                    if(!NoPedal[x])
+                    {
+                        if (Main.rand.Next(Projectile.timeLeft) < 3)
+                        {
+                            NoPedal[x] = true;
+                            if (x == 1)
+                            {
+                                Gore.NewGore(Projectile.GetSource_Death(), Projectile.Center,Vector2.Zero,ModContent.GoreType<Gores.BloodLanternBody>());
+                            }
+                            if (x == 2)
+                            {
+                                Cen = new Vector2(12f, 9f) - new Vector2(6f, 7f);
+                                Dust.NewDust(Projectile.Center + Cen - BLantern[x].Size() / 2f,0,0,ModContent.DustType<Dusts.BloodPedal>());
+                            }
+                            if (x == 3)
+                            {
+                                Cen = new Vector2(26f, 9f) - new Vector2(6f, 7f);
+                                Dust.NewDust(Projectile.Center + Cen - BLantern[x].Size() / 2f, 0, 0, ModContent.DustType<Dusts.BloodPedal>());
+                            }
+                            if (x == 4)
+                            {
+                                Cen = new Vector2(19f, 8f) - new Vector2(6f, 7f);
+                                Dust.NewDust(Projectile.Center + Cen - BLantern[x].Size() / 2f, 0, 0, ModContent.DustType<Dusts.BloodPedal>());
+                            }
+                            if (x == 5)
+                            {
+                                Cen = new Vector2(34f, 14f) - new Vector2(6f, 7f);
+                                Dust.NewDust(Projectile.Center + Cen - BLantern[x].Size() / 2f, 0, 0, ModContent.DustType<Dusts.BloodPedal>());
+                            }
+                            if (x == 6)
+                            {
+                                Cen = new Vector2(4f, 14f) - new Vector2(6f, 7f);
+                                Dust.NewDust(Projectile.Center + Cen - BLantern[x].Size() / 2f, 0, 0, ModContent.DustType<Dusts.BloodPedal>());
+                            }
+                            if (x == 7)
+                            {
+                                Cen = new Vector2(8f, 10f) - new Vector2(6f, 7f);
+                                Dust.NewDust(Projectile.Center + Cen - BLantern[x].Size() / 2f, 0, 0, ModContent.DustType<Dusts.BloodPedal>());
+                            }
+                            if (x == 8)
+                            {
+                                Cen = new Vector2(30f, 10f) - new Vector2(6f, 7f);
+                                Dust.NewDust(Projectile.Center + Cen - BLantern[x].Size() / 2f, 0, 0, ModContent.DustType<Dusts.BloodPedal>());
+                            }
+                            if (x == 9)
+                            {
+                                Cen = new Vector2(8f, 25f) - new Vector2(6f, 7f);
+                                Dust.NewDust(Projectile.Center + Cen - BLantern[x].Size() / 2f, 0, 0, ModContent.DustType<Dusts.BloodPedal>());
+                            }
+                            if (x == 10)
+                            {
+                                Cen = new Vector2(30f, 25f) - new Vector2(6f, 7f);
+                                Dust.NewDust(Projectile.Center + Cen - BLantern[x].Size() / 2f, 0, 0, ModContent.DustType<Dusts.BloodPedal>());
+                            }
+                            if (x == 11)
+                            {
+                                Cen = new Vector2(32f, 21f) - new Vector2(6f, 7f);
+                                Dust.NewDust(Projectile.Center + Cen - BLantern[x].Size() / 2f, 0, 0, ModContent.DustType<Dusts.BloodPedal>());
+                            }
+                            if (x == 12)
+                            {
+                                Cen = new Vector2(6f, 21f) - new Vector2(6f, 7f);
+                                Dust.NewDust(Projectile.Center + Cen - BLantern[x].Size() / 2f, 0, 0, ModContent.DustType<Dusts.BloodPedal>());
+                            }
+                            if (x == 13)
+                            {
+                                Cen = new Vector2(19f, 23f) - new Vector2(6f, 7f);
+                                Dust.NewDust(Projectile.Center + Cen - BLantern[x].Size() / 2f, 0, 0, ModContent.DustType<Dusts.BloodPedal>());
+                            }
+                            if (x == 14)
+                            {
+                                Cen = new Vector2(23f, 16f) - new Vector2(6f, 7f);
+                                Dust.NewDust(Projectile.Center + Cen - BLantern[x].Size() / 2f, 0, 0, ModContent.DustType<Dusts.BloodPedal>());
+                            }
+                            if (x == 15)
+                            {
+                                Cen = new Vector2(15f, 16f) - new Vector2(6f, 7f);
+                                Dust.NewDust(Projectile.Center + Cen - BLantern[x].Size() / 2f, 0, 0, ModContent.DustType<Dusts.BloodPedal>());
+                            }
+                        }
+                    }
+                }
+            }
+        }
+
+        public override void PostDraw(Color lightColor)
+        {
+            for (int x = -1; x < 15; x++)
+            {
+                BLantern[x + 1] = MythContent.QuickTexture("LanternMoon/Projectiles/BloodLampFrame/BloodLamp_" + x.ToString());
+            }
+            PearlOmega += (Projectile.rotation - PearlRot) / 75f;
+            PearlOmega *= 0.95f;
+            PearlRot += PearlOmega;
+            Color color = Lighting.GetColor((int)(Projectile.Center.X / 16d), (int)(Projectile.Center.Y / 16d));
+            for (int x = 0; x < 16; x++)
+            {
+                float Rot = Projectile.rotation;
+                Vector2 Cen = new Vector2(19f, 19f);
+                if (x >= 2)
+                {
+                    Rot = -(float)(Math.Sin(Main.time / 26d + x)) / 7f + Projectile.rotation;
+                }
+                if(x == 0)
+                {
+                    Rot = PearlRot;
+                    Cen = new Vector2(19f, 51f);
+                }
+                if(!NoPedal[x] || x < 1)
+                {
+                    Main.spriteBatch.Draw(BLantern[x], Projectile.Center - Main.screenPosition + Cen - BLantern[x].Size() / 2f/*坐标校正*/, null, color, Rot, Cen, 1, SpriteEffects.None, 0);
+                }
+            }
+        }
+        public override bool PreDraw(ref Color lightColor)
+        {
+            return false;
+        }
+    }
+}
