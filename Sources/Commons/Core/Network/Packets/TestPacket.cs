63c478d1de2cc5a67f3cd6530de7e28eabc09fde
ye_you <28271655@qq.com>
2022-05-15 13:58:10 +0800

    Package

--- Sources/Commons/Core/Network/Packets/TestPacket.cs
+++ Sources/Commons/Core/Network/Packets/TestPacket.cs
@@ -1,7 +1,57 @@
-﻿using Everglow.Sources.Commons.Network.PacketHandle;
+﻿using Everglow.Sources.Commons.ModuleSystem;
+using Everglow.Sources.Commons.Network.PacketHandle;
 
 namespace Everglow.Sources.Commons.Core.Network.Packets
 {
+    //public class TestSystem : ModPlayer
+    //{
+    //    public override void SyncPlayer(int toWho, int fromWho, bool newPlayer)
+    //    {
+    //        if(newPlayer)
+    //        {
+    //            Everglow.PacketResolver.Send(new RequestWorldVersion());
+    //        }
+    //    }
+    //}
+    //public class RequestWorldVersion : IPacket
+    //{
+    //    public void Receive(BinaryReader reader, int whoAmI)
+    //    {
+    //    }
+
+    //    public void Send(BinaryWriter writer)
+    //    {
+    //    }
+    //}
+    //public class SendWorldVersion : IPacket
+    //{
+    //    public ulong version;
+    //    public void Receive(BinaryReader reader, int whoAmI)
+    //    {
+    //        version = reader.ReadUInt64();
+    //    }
+    //    public void Send(BinaryWriter writer)
+    //    {
+    //        writer.Write(Main.ActiveWorldFileData.WorldGeneratorVersion);
+    //    }
+    //}
+    //[HandlePacket(typeof(RequestWorldVersion))]
+    //public class RequestWorldVersionHandler : IPacketHandler
+    //{
+    //    public void Handle(IPacket packet, int whoAmI)
+    //    {
+    //        Everglow.PacketResolver.Send(new SendWorldVersion(), whoAmI);
+    //    }
+    //}
+    //[HandlePacket(typeof(SendWorldVersion))]
+    //public class SendWorldVersionHandler : IPacketHandler
+    //{
+    //    public void Handle(IPacket packet, int whoAmI)
+    //    {
+    //        Main.NewText((packet as SendWorldVersion).version);
+    //        Main.NewText(1);
+    //    }
+    //}
     public class TestPacket : IPacket
     {
         public int TestValue { get; set; }
--- Sources/Commons/Function/FeatureFlags/CompileTimeFeatureFlags.cs
+++ Sources/Commons/Function/FeatureFlags/CompileTimeFeatureFlags.cs
@@ -6,6 +6,7 @@
         /// 表示封包ID的表示是否要使用Int32，如果关闭则使用byte，可以节省一点带宽
         /// 但是之后封包数量多了byte就表示不下了
         /// </summary>
-        public const bool NetworkPacketIDUseInt32 = false;
+        public static readonly bool NetworkPacketIDUseInt32 = true;
+
     }
 }
