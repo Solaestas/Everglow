0edc8e86de04b66bc8f858cd788f147fe67673f6
ye_you <28271655@qq.com>
2022-05-16 16:47:44 +0800

    invasion

---
+++ Sources/Modules/ZY/InvasionSystem/Invasion.cs
@@ -0,0 +1,33 @@
+﻿using Everglow.Sources.Commons.ModuleSystem;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Everglow.Sources.Modules.ZY.InvasionSystem
+{
+    [ModuleDependency(typeof(InvasionSystem))]
+    internal abstract class Invasion : IModule
+    {
+        public abstract string Name { get; }
+        public const int VanillaCount = 5;
+        //原版最高为5，所以这里从114开始
+        private static int InvasionsCount = 114;
+        public int InvasionID { get; private set; }
+        public void Load()
+        {
+            InvasionID = InvasionsCount++;
+        }
+
+        public void Unload()
+        {
+
+        }
+
+        public virtual void Draw()
+        {
+
+        }
+    }
+}
---
+++ Sources/Modules/ZY/InvasionSystem/InvasionSystem.cs
@@ -0,0 +1,36 @@
+﻿using Everglow.Sources.Commons.ModuleSystem;
+
+namespace Everglow.Sources.Modules.ZY.InvasionSystem
+{
+    internal class InvasionSystem : IModule
+    {
+        private Dictionary<int, Invasion> invasions = new Dictionary<int, Invasion>();
+        public string Name => "InvasionSystem";
+        public bool InvasionBegin<T>() where T : Invasion
+        {
+            var invasion = Everglow.ModuleManager.FindModule<T>();
+            if(invasion is null || invasion.Count() > 1)
+            return false;
+            return false;
+        }
+        public void Load()
+        {
+            On.Terraria.Main.DrawInterface_15_InvasionProgressBars += Main_DrawInterface_15_InvasionProgressBars;
+        }
+
+        private void Main_DrawInterface_15_InvasionProgressBars(On.Terraria.Main.orig_DrawInterface_15_InvasionProgressBars orig)
+        {
+            if(Main.invasionProgressMode <= Invasion.VanillaCount)
+            {
+                orig();
+                return;
+            }
+            invasions[Main.invasionProgressMode]
+        }
+
+        public void Unload()
+        {
+
+        }
+    }
+}
