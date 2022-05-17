e33a9361cbddb993a129e10d2c7c00221d012c33
Felixyang777 <1761610526@qq.com>
2022-05-17 14:16:39 +0800

    add FoodBuff dictionary

---
+++ Sources/Modules/Food/Buffs/PizzaBuff.cs
@@ -0,0 +1,23 @@
+﻿using Terraria;
+using Terraria.ModLoader;
+
+namespace Everglow.Sources.Modules.Food.Buffs
+{
+	public class PizzaBuff : ModBuff
+	{
+		public override void SetStaticDefaults()
+		{
+			DisplayName.SetDefault("MilkCarton Buff");
+			Description.SetDefault("Grants +4 defense.");
+			Main.buffNoTimeDisplay[Type] = false;
+			Main.debuff[Type] = false; // 添加这个，这样护士在治疗时就不会去除buff
+		}
+
+		public override void Update(Player player, ref int buffIndex)
+		{
+			player.statDefense += 4; // 加4防御
+		}
+	}
+}
+
+	
