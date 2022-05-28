518b2c1559c30766454feab2bbb8ca99951c4a8e
Felixyang777 <1761610526@qq.com>
2022-05-28 13:37:45 +0800

    cellphone fix

---
+++ Sources/Modules/Food/Items/Cellphone.cs
@@ -0,0 +1,32 @@
+﻿namespace Everglow.Sources.Modules.Food.Items
+{
+    public class Cellphone : GlobalItem
+    {
+        public override void UpdateInventory(Item item, Player player)
+        {
+            if (item.type == ItemID.CellPhone) 
+            {
+                FoodSatietyInfoDisplayplayer SatietyInfo = player.GetModPlayer<FoodSatietyInfoDisplayplayer>();
+                SatietyInfo.AccBloodGlucoseMonitor = true;//显示当前饱食度
+
+                ThirstystateInfoDisplayplayer ThirstystateInfo = player.GetModPlayer<ThirstystateInfoDisplayplayer>();
+                ThirstystateInfo.AccOsmoticPressureMonitor = true;//显示渴觉状态
+            }
+        }
+    }
+    public class CellphoneRecipe : ModSystem
+    {
+        public override void PostAddRecipes()
+        {
+            for (int i = 0; i < Recipe.numRecipes; i++)
+            {
+                Recipe recipe = Main.recipe[i];
+                if (recipe.TryGetIngredient(ItemID.PDA, out Item ingredient))
+                {
+                    recipe.AddIngredient(ModContent.ItemType<OsmoticPressureMonitor>());
+                    recipe.AddIngredient(ModContent.ItemType<BloodGlucoseMonitor>());
+                }
+            }
+        }
+    }
+}
