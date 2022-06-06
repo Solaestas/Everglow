aef44354f11f4d8b8753ab922034891293cba4ef
MrSkirt <bdfzchen@163.com>
2022-06-06 23:22:29 +0800

    save state

--- Sources/Modules/ExampleModule/ExampleSystem.cs
+++ Sources/Modules/ExampleModule/ExampleSystem.cs
@@ -28,6 +28,7 @@ namespace Everglow.Sources.Modules.ExampleModule
             Main.OnResolutionChanged += Main_OnResolutionChanged;
             m_renderTargetIndex = 0;
 
+
             Main.OnRenderTargetsInitialized += Main_OnRenderTargetsInitialized;
             Main.OnPreDraw += Main_OnPreDraw;
             // On.Terraria.GameContent.Shaders.WaterShaderData.Apply += WaterShaderData_Apply;
@@ -67,6 +68,39 @@ namespace Everglow.Sources.Modules.ExampleModule
             c.Emit(Mono.Cecil.Cil.OpCodes.Ldsfld, typeof(ExampleSystem).GetField("ReplaceEffectPass"));
             c.EmitDelegate<Action<WaterShaderData, EffectPass>>((shaderData, effect) =>
             {
+                var targetPos = shaderData.Shader.Parameters["uTargetPosition"].GetValueVector2();
+                var imageOffset = shaderData.Shader.Parameters["uImageOffset"].GetValueVector2();
+                var screenPos = Main.screenPosition - targetPos;
+                var a = shaderData.Shader.Parameters["uColor"].GetValueVector3();
+                var a1 = shaderData.Shader.Parameters["uOpacity"].GetValueSingle();
+                var a2 = shaderData.Shader.Parameters["uSecondaryColor"].GetValueVector3();
+                var a3 = shaderData.Shader.Parameters["uTime"].GetValueSingle();
+                var a4 = shaderData.Shader.Parameters["uScreenResolution"].GetValueVector2();
+                var a5 = shaderData.Shader.Parameters["uScreenPosition"].GetValueVector2();
+                var a6 = shaderData.Shader.Parameters["uTargetPosition"].GetValueVector2();
+                var a7 = shaderData.Shader.Parameters["uImageOffset"].GetValueVector2();
+                var a8 = shaderData.Shader.Parameters["uIntensity"].GetValueSingle();
+                var a9 = shaderData.Shader.Parameters["uProgress"].GetValueSingle();
+                var a10 = shaderData.Shader.Parameters["uDirection"].GetValueVector2();
+                var a11 = shaderData.Shader.Parameters["uZoom"].GetValueVector2();
+                var a13 = shaderData.Shader.Parameters["uImageSize1"].GetValueVector2();
+                var a14 = shaderData.Shader.Parameters["uImageSize2"].GetValueVector2();
+                var a15 = shaderData.Shader.Parameters["uImageSize3"].GetValueVector2();
+
+
+                var shader = m_waveDisortionScreen.Value;
+                shader.Parameters["cb0"].SetValue(new Vector4(1 / a13.X, 1 / a13.Y, 0, 0));
+                shader.Parameters["cb1"].SetValue(new Vector4(a9 * 0.1f, 0, 0, 0));
+                shader.Parameters["cb2"].SetValue(new Vector4(-a9 * 0.1f, 0, 0, 0));
+                shader.Parameters["cb3"].SetValue(new Vector4(a11, 0, 0));
+                shader.Parameters["cb4"].SetValue(new Vector4(0.04f, 0.444f, 0, 0));
+                shader.Parameters["cb5"].SetValue(new Vector4(1f / a15.X, 1f / a15.Y, 0, 0));
+                shader.Parameters["cb6"].SetValue(new Vector4(Main.screenWidth, Main.screenHeight, 0, 0));
+                shader.Parameters["cb7"].SetValue(new Vector4(screenPos, 0, 0));
+                shader.Parameters["cb8"].SetValue(new Vector4(targetPos, 0, 0));
+                shader.Parameters["cb9"].SetValue(new Vector4(a8, 0, 0, 0));
+                shader.Parameters["cb10"].SetValue(new Vector4(imageOffset, 0, 0));
+
                 effect.Apply();
             });
         }
@@ -97,9 +131,14 @@ namespace Everglow.Sources.Modules.ExampleModule
                 device.SetRenderTarget(null);
                 return;
             }
+            var effect = m_waveEffect.Value;
+            if (effect == null)
+            {
+                return;
+            }
             int nextIndex = (m_renderTargetIndex + 1) % 2;
             var spriteBatch = Main.spriteBatch;
-            var effect = m_waveEffect.Value;
+
             device.SetRenderTarget(m_waveDataTarget[m_renderTargetIndex]);
             device.Clear(new Color(0.5f, 0.5f, 0.5f, 1.0f));
             spriteBatch.Begin(SpriteSortMode.Immediate, BlendState.Opaque, 
@@ -129,20 +168,13 @@ namespace Everglow.Sources.Modules.ExampleModule
 
             //}
 
-            var shader = m_waveDisortionScreen.Value;
-            Vector2 value = new Vector2(Main.screenWidth, Main.screenHeight) * 0.5f * (Vector2.One - Vector2.One / Main.GameViewMatrix.Zoom);
-            shader.Parameters["cb0"].SetValue(new Vector4(1 / 256f, 1 / 256f, 0, 0));
-            shader.Parameters["cb1"].SetValue(new Vector4(Main.GlobalTimeWrappedHourly * 0.01f, 0, 0, 0));
-            shader.Parameters["cb2"].SetValue(new Vector4(-Main.GlobalTimeWrappedHourly * 0.01f, 0, 0, 0));
-            shader.Parameters["cb3"].SetValue(new Vector4(1.0f, 1.0f, 0, 0));
-            shader.Parameters["cb4"].SetValue(new Vector4(0.04f, 0.04444f, 0, 0));
-            shader.Parameters["cb5"].SetValue(new Vector4(0.0008f, 0.0009f, 0, 0));
-            shader.Parameters["cb6"].SetValue(new Vector4(Main.screenWidth, Main.screenHeight, 0, 0));
-            shader.Parameters["cb7"].SetValue(new Vector4(Main.screenPosition, 0, 0));
-            shader.Parameters["cb8"].SetValue(new Vector4(192, 192, 0, 0));
-            shader.Parameters["cb9"].SetValue(new Vector4(1.0f, 0, 0, 0));
-            shader.Parameters["cb10"].SetValue(new Vector4(0, -0.0014f, 0, 0));
 
+            Vector2 value = new Vector2(Main.screenWidth, Main.screenHeight) * 0.5f * (Vector2.One - Vector2.One / Main.GameViewMatrix.Zoom);
+            Vector2 value2 = (Main.drawToScreen ? Vector2.Zero : new Vector2(Main.offScreenRange, Main.offScreenRange)) - Main.screenPosition - value;
+
+
+
+            //-(value2 * 0.25f - _lastDistortionDrawOffset) / new Vector2(_distortionTarget.Width, _distortionTarget.Height)
         }
 
         private void Main_OnResolutionChanged(Vector2 obj)
