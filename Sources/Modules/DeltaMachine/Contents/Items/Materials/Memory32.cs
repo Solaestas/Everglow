1e968841e60fdbb76f64aff5e3df70b9a82a5d50
Colin <1805094167@qq.com>
2022-06-08 15:27:26 +0800

    新增两个物品; 新增戴尔塔机器模块用户交互界面类.

---
+++ Sources/Modules/DeltaMachine/Contents/Items/Materials/Memory32.cs
@@ -0,0 +1,27 @@
+﻿using Everglow.Sources.Modules.DeltaMachine.Globals.Items;
+
+namespace Everglow.Sources.Modules.DeltaMachine.Contents.Items.Materials
+{
+    /// <summary>
+    /// 戴尔塔机器模块: 物品.32G内存条.
+    /// </summary>
+    public class Memory32 : ModItem , IDeltaItem
+    {
+        public override void SetStaticDefaults( )
+        {
+            DisplayName.SetDefault( "内存条 32G" );
+            Tooltip.SetDefault( "" +
+                "允许你将其装载在 [永恒雪山]\n" +
+                "公司所出产的武器上以增加计算能力." );
+            base.SetStaticDefaults( );
+        }
+        public override void SetDefaults( )
+        {
+            Item.width = 48;
+            Item.height = 20;
+            Item.rare = ItemRarityID.LightRed;
+            Item.maxStack = 4;
+            base.SetDefaults( );
+        }
+    }
+}
---
+++ Sources/Modules/DeltaMachine/DeltaCoreUserInterface.cs
@@ -0,0 +1,23 @@
+﻿using Everglow.Sources.Commons.Core.UI;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Everglow.Sources.Modules.DeltaMachine
+{
+    /// <summary>
+    /// 戴尔塔机器模块中负责维护用户交互界面的类.
+    /// </summary>
+    public class DeltaCoreUserInterface : ContainerPage
+    {
+        public static List<Container> RockContainers;
+
+        protected override void InitializeContainer( )
+        {
+            RockContainers = new List<Container>( );
+            base.InitializeContainer( );
+        }
+    }
+}
