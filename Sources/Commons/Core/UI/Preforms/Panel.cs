e37333f2ad6dcd55d4f70a56c1c1dd355c5f6693
Colin <1805094167@qq.com>
2022-06-09 17:54:54 +0800

    回滚至戴尔塔机器模块提交前.

--- Sources/Commons/Core/UI/Preforms/Panel.cs
+++ Sources/Commons/Core/UI/Preforms/Panel.cs
@@ -1,36 +1,61 @@
-﻿using Terraria.GameContent;
+﻿using Microsoft.Xna.Framework;
+using Microsoft.Xna.Framework.Graphics;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using Terraria.GameContent;
 
 namespace Everglow.Sources.Commons.Core.UI.Preforms
 {
     /// <summary>
     /// 一块可拖动的底板.
     /// <para>[!] 若你想关闭它的拖动功能, </para>
-    /// <para>[!] 请在*初始化后*将 <seealso cref="ContainerEvents.Drag"/> 设置为 <see href="false"/>.</para>
+    /// <para>[!] 请在||| 初始化后 |||将 <seealso cref="ContainerEvents.Drop"/> 设置为 <see href="false"/>.</para>
     /// </summary>
     public class Panel : Container
     {
+        private int _borderSize;
         public Texture2D Image { get; private set; }
 
         public Panel( ) { }
-        public Panel( Texture2D img )
+        public Panel( Texture2D img, int borderSize )
         {
             Image = img;
+            _borderSize = borderSize;
         }
 
         protected override void InitializeContainer( )
         {
-            Events.Drag = true;
+            Events.Drop = true;
             Events.CanGetForPointer = false;
-            EnableScissor = true;
             base.InitializeContainer( );
         }
+
         protected override void DrawSelf( )
         {
-            if ( Image != null )
-                Main.spriteBatch.Draw( Image, BaseRectangle, ContainerElement.Color );
+            Main.spriteBatch.End( );
+            RasterizerState OverflowHiddenRasterizerState = new RasterizerState
+            {
+                CullMode = CullMode.None,
+                ScissorTestEnable = true
+            };
+            Rectangle clippingRectangle = BaseRectangle;
+            Main.spriteBatch.GraphicsDevice.ScissorRectangle = clippingRectangle;
+            Main.spriteBatch.GraphicsDevice.RasterizerState = OverflowHiddenRasterizerState;
+            Main.spriteBatch.Begin( SpriteSortMode.Deferred, BlendState.AlphaBlend, SamplerState.AnisotropicClamp, DepthStencilState.None, OverflowHiddenRasterizerState, null );
+            if( Image != null )
+                Main.spriteBatch.Draw( Image, BaseRectangle , ContainerElement.Color  );
             else
                 Main.spriteBatch.Draw( TextureAssets.MagicPixel.Value, BaseRectangle, ContainerElement.Color );
             base.DrawSelf( );
+        }
+        protected override void PostDraw( )
+        {
+            base.PostDraw( );
+            Main.spriteBatch.End( );
+            Main.spriteBatch.Begin( SpriteSortMode.Deferred, BlendState.AlphaBlend, SamplerState.PointWrap, null, null );
         }
     }
 }
