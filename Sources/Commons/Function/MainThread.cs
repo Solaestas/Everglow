afa921e2362078f23c50746d6b7fddd4ba302ac1
ye_you <28271655@qq.com>
2022-06-12 21:06:31 +0800

    Update

--- Sources/Commons/Function/MainThread.cs
+++ Sources/Commons/Function/MainThread.cs
@@ -1,40 +1,82 @@
 ﻿using Everglow.Sources.Commons.Core.ModuleSystem;
 
-namespace Everglow.Sources.Commons.Function
-{
-    internal class MainThread : IModule
-    {
-        private Stack<Action> tasks = new Stack<Action>();
-        public string Name => "MainThread";
-        public void Load()
-        {
-            On.Terraria.Main.Update += Main_Update;
-        }
+namespace Everglow.Sources.Commons.Function;
 
-        public void Unload()
+internal class Future<T>
+{
+    private bool isLoaded;
+    private T value;
+    public T Value
+    {
+        get
         {
-            tasks = null;
-        }
-        public static void Add(Action task) => Everglow.ModuleManager.GetModule<MainThread>().tasks.Push(task);
-        public static Color[] GetColors(Texture2D texture)
-        {
-            Color[] colors = new Color[texture.Width * texture.Height];
-            Add(() => texture.GetData(colors));
-            return colors;
-        }
-        /// <summary>
-        /// 挂在Update上保证服务端也能执行
-        /// </summary>
-        /// <param name="orig"></param>
-        /// <param name="self"></param>
-        /// <param name="gameTime"></param>
-        private void Main_Update(On.Terraria.Main.orig_Update orig, Main self, GameTime gameTime)
-        {
-            if (tasks.Count != 0)
+            if(!IsLoaded)
             {
-                tasks.Pop().Invoke();
+                throw new InvalidOperationException("Hasn't been Loaded");
             }
-            orig(self, gameTime);
+            return value;
         }
+        set
+        {
+            if(IsLoaded)
+            {
+                throw new InvalidOperationException("Has been Loaded");
+            }
+            isLoaded = true;
+            this.value = value;
+        }
+    }
+    public bool IsLoaded => isLoaded;
+}
+internal class MainThread : IModule
+{
+    private Stack<Action> tasks = new Stack<Action>();
+    public string Name => "MainThread";
+    public void Load()
+    {
+        On.Terraria.Main.Update += Main_Update;
+    }
+
+    public void Unload()
+    {
+        tasks = null;
+    }
+    public static void Call(Action task)
+    {
+        Everglow.ModuleManager.GetModule<MainThread>().tasks.Push(task);
+    }
+    public static Future<Color[]> GetColors(Texture2D texture)
+    {
+        Future<Color[]> future = new Future<Color[]>();
+        Call(() =>
+        {
+            Color[] temp = new Color[texture.Width * texture.Height];
+            texture.GetData(temp);
+            future.Value = temp;
+        });
+        return future;
+    }
+    public static Future<T> GetData<T>(Func<T> func)
+    {
+        Future<T> future = new Future<T>();
+        Call(() =>
+        {
+            future.Value = func();
+        });
+        return future;
+    }
+    /// <summary>
+    /// 挂在Update上保证服务端也能执行
+    /// </summary>
+    /// <param name="orig"></param>
+    /// <param name="self"></param>
+    /// <param name="gameTime"></param>
+    private void Main_Update(On.Terraria.Main.orig_Update orig, Main self, GameTime gameTime)
+    {
+        if (tasks.Count != 0)
+        {
+            tasks.Pop().Invoke();
+        }
+        orig(self, gameTime);
     }
 }
