a1db93ad6c255ba755cac8b68f347010ad491556
yiyang233 <1787058089@qq.com>
2022-06-24 18:29:34 +0800

    添加贯虹之槊（未完善）

---
+++ Sources/Modules/MEACModule/Items/VortexVanquisherItem.cs
@@ -0,0 +1,49 @@
+﻿using Everglow.Sources.Modules.MEACModule.Projectiles;
+using Everglow.Sources.Modules.MythModule.TheFirefly.Projectiles;
+using Everglow.Sources.Modules.MythModule.TheFirefly.WorldGeneration;
+namespace Everglow.Sources.Modules.MEACModule.Items
+{
+    public class VortexVanquisherItem : ModItem
+    {
+        public override void SetStaticDefaults()
+        {
+            Tooltip.SetDefault("");
+        }
+
+        public override void SetDefaults()
+        {
+            Item.useStyle = 1;
+            Item.width = 1;
+            Item.height = 1;
+            Item.useAnimation = 5;
+            Item.useTime = 5;
+            Item.shootSpeed = 5f;
+            Item.knockBack = 2.5f;
+            Item.damage = 30;
+            Item.rare = ItemRarityID.Green;
+
+            Item.DamageType = DamageClass.Melee;
+            Item.noMelee = true;
+            Item.noUseGraphic = true;
+
+            Item.value = Item.sellPrice(gold: 1);
+        }
+        public override bool CanUseItem(Player player)
+        {
+            if (base.CanUseItem(player))
+            {
+                if (Main.myPlayer == player.whoAmI)
+                {
+                    Projectile proj= Projectile.NewProjectileDirect(player.GetSource_ItemUse(Item), player.Center, Vector2.Zero, ModContent.ProjectileType<VortexVanquisher>(), player.GetWeaponDamage(Item), Item.knockBack, player.whoAmI);
+                    proj.scale *= Item.scale;
+                }
+                return false;
+            }
+            return base.CanUseItem(player);
+        }
+        public override void AddRecipes()
+        {
+
+        }
+    }
+}
