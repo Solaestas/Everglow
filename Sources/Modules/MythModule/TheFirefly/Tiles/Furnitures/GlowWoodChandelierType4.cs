c06b00f271f7a0d0a6dcb986f6a774517f5b83bc
Felixyang777 <1761610526@qq.com>
2022-07-02 14:31:55 +0800

    new furnitures

---
+++ Sources/Modules/MythModule/TheFirefly/Tiles/Furnitures/GlowWoodChandelierType4.cs
@@ -0,0 +1,64 @@
+using Everglow.Sources.Modules.MythModule.Bosses.CorruptMoth.Dusts;
+using Microsoft.Xna.Framework;
+using Microsoft.Xna.Framework.Graphics;
+using ReLogic.Content;
+using Terraria;
+using Terraria.DataStructures;
+using Terraria.Enums;
+using Terraria.GameContent.Drawing;
+using Terraria.ID;
+using Terraria.ModLoader;
+using Terraria.ObjectData;
+
+namespace Everglow.Sources.Modules.MythModule.TheFirefly.Tiles.Furnitures
+{
+    public class GlowWoodChandelierType4 : ModTile
+    {
+        private Asset<Texture2D> flameTexture;
+        public override void SetStaticDefaults()
+        {
+            // Properties
+            Main.tileFrameImportant[Type] = true;
+            Main.tileNoAttach[Type] = true;
+            Main.tileLavaDeath[Type] = true;
+            Main.tileLighted[Type] = true;
+            Main.tileSolid[Type] = false;
+            Main.tileNoFail[Type] = true;
+            TileID.Sets.HasOutlines[Type] = true;
+            TileID.Sets.CanBeSleptIn[Type] = true; // Facilitates calling ModifySleepingTargetInfo
+            TileID.Sets.InteractibleByNPCs[Type] = true; // Town NPCs will palm their hand at this tile
+            TileID.Sets.IsValidSpawnPoint[Type] = true;
+
+            
+            AddToArray(ref TileID.Sets.RoomNeeds.CountsAsTorch);
+
+            DustType = ModContent.DustType<BlueGlow>();
+            AdjTiles = new int[] { TileID.Chandeliers };
+
+            // Placement
+            TileObjectData.newTile.CopyFrom(TileObjectData.Style3x3); 
+            TileObjectData.newTile.AnchorTop = new AnchorData(AnchorType.SolidTile, TileObjectData.newTile.Width, 0);
+            TileObjectData.newTile.AnchorBottom = default(AnchorData);
+            TileObjectData.newTile.CoordinateHeights = new[] { 16, 16, 16 };
+            TileObjectData.addTile(Type);
+
+            // Etc
+            ModTranslation name = CreateMapEntryName();
+            name.SetDefault("GlowWood Chandelier");
+            AddMapEntry(new Color(0, 14, 175), name);
+
+        }
+
+        public override void ModifyLight(int i, int j, ref float r, ref float g, ref float b)
+        {
+            r = 0.1f;
+            g = 0.9f;
+            b = 1f;
+        }
+                
+        public override void KillMultiTile(int x, int y, int frameX, int frameY)
+        {
+            Item.NewItem(new EntitySource_TileBreak(x, y), x * 16, y * 16, 48, 32, ModContent.ItemType<Items.Furnitures.GlowWoodChandelierType4>());
+        }
+    }
+}
