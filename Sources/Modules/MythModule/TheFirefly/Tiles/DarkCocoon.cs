4c324dc69f5783bd7923ec3ec78102dad416e5c0
Omnielement <877183629@qq.com>
2022-07-09 14:56:10 +0800

    现在不闪退了

--- Sources/Modules/MythModule/TheFirefly/Tiles/DarkCocoon.cs
+++ Sources/Modules/MythModule/TheFirefly/Tiles/DarkCocoon.cs
@@ -46,26 +46,47 @@ namespace Everglow.Sources.Modules.MythModule.TheFirefly.Tiles
             {
                 if (Main.rand.NextBool(8))
                 {
+                    Tile t1 = Main.tile[i, j - 1];
+                    Tile t2 = Main.tile[i, j - 2];
+                    Tile t3 = Main.tile[i, j - 3];
                     switch (Main.rand.Next(1, 4))
                     {
                         case 1:
-                            WorldGen.PlaceTile(i, j - 2, (ushort)ModContent.TileType<Tiles.BlackStarShrubSmall>());
+                            t1.TileType = (ushort)ModContent.TileType<Tiles.BlackStarShrubSmall>();
+                            t2.TileType = (ushort)ModContent.TileType<Tiles.BlackStarShrubSmall>();
+                            t1.HasTile = true;
+                            t2.HasTile = true;
                             short numa = (short)(Main.rand.Next(0, 6) * 48);
-                            Main.tile[i, j - 2].TileFrameX = numa;
-                            Main.tile[i, j - 1].TileFrameX = numa;
+                            t1.TileFrameX = numa;
+                            t2.TileFrameX = numa;
+                            t1.TileFrameY = 16;
+                            t2.TileFrameX = 0;
                             break;
                         case 2:
-                            WorldGen.PlaceTile(i, j - 2, (ushort)ModContent.TileType<Tiles.BlackStarShrubSmall>());
+                            t1.TileType = (ushort)ModContent.TileType<Tiles.BlackStarShrubSmall>();
+                            t2.TileType = (ushort)ModContent.TileType<Tiles.BlackStarShrubSmall>();
+                            t1.HasTile = true;
+                            t2.HasTile = true;
                             short num = (short)(Main.rand.Next(0, 6) * 48);
-                            Main.tile[i, j - 2].TileFrameX = num;
-                            Main.tile[i, j - 1].TileFrameX = num;
+                            t2.TileFrameX = num;
+                            t1.TileFrameX = num;
+                            t1.TileFrameY = 16;
+                            t2.TileFrameX = 0;
                             break;
                         case 3:
-                            WorldGen.PlaceTile(i, j - 3, (ushort)ModContent.TileType<Tiles.BlackStarShrub>());
+                            t1.TileType = (ushort)ModContent.TileType<Tiles.BlackStarShrub>();
+                            t2.TileType = (ushort)ModContent.TileType<Tiles.BlackStarShrub>();
+                            t3.TileType = (ushort)ModContent.TileType<Tiles.BlackStarShrub>();
+                            t1.HasTile = true;
+                            t2.HasTile = true;
+                            t3.HasTile = true;
                             short num1 = (short)(Main.rand.Next(0, 6) * 72);
-                            Main.tile[i, j - 3].TileFrameX = num1;
-                            Main.tile[i, j - 2].TileFrameX = num1;
-                            Main.tile[i, j - 1].TileFrameX = num1;
+                            t3.TileFrameX = num1;
+                            t2.TileFrameX = num1;
+                            t1.TileFrameX = num1;
+                            t1.TileFrameY = 32;
+                            t2.TileFrameX = 16;
+                            t3.TileFrameX = 0;
                             break;
                     }
                 }
