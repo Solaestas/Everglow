e85f1879213dafe83dffeea1b2ad7fb03a106e9a
ye_you <28271655@qq.com>
2022-07-14 14:20:15 +0800

    MapIO

---
+++ Sources/Modules/ZYModule/Commons/Function/MapIO/AirTile.cs
@@ -0,0 +1,32 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using Terraria.ObjectData;
+
+namespace I.MapIO
+{
+    internal class AirTile : ModTile
+    {
+        public override void SetStaticDefaults()
+        {
+            Main.tileSolid[Type] = true;
+            Main.tileMergeDirt[Type] = true;
+            Main.tileBlockLight[Type] = true;
+            AddMapEntry(Color.White);
+        }
+    }
+
+    internal class AirTileItem : ModItem
+    {
+        public override void SetDefaults()
+        {
+            Item.useTime = 5;
+            Item.useAnimation = 5;
+            Item.createTile = ModContent.TileType<AirTile>();
+            Item.useStyle = ItemUseStyleID.Swing;
+            Item.autoReuse = true;
+        }
+    }
+}
