af9fb4523af9c48ec3caa3b03e15f36438267088
Omnielement <877183629@qq.com>
2022-07-22 15:28:02 +0800

    加入一个乐器

---
+++ Sources/Modules/MythModule/OmniElementItems/Projectiles/LilyHarpProj.cs
@@ -0,0 +1,40 @@
+﻿using Everglow.Sources.Modules.MythModule.Common;
+using Everglow.Sources.Commons.Function.Vertex;
+using Everglow.Sources.Modules.MythModule.Bosses.CorruptMoth.Dusts;
+using Everglow.Sources.Modules.MythModule.TheFirefly.Dusts;
+
+namespace Everglow.Sources.Modules.MythModule.OmniElementItems.Projectiles
+{
+    internal class LilyHarpProj : ModProjectile
+    {
+        public override void SetDefaults()
+        {
+            Projectile.width = 70;
+            Projectile.height = 78;
+            Projectile.friendly = false;
+            Projectile.DamageType = DamageClass.Magic;
+            Projectile.aiStyle = -1;
+            Projectile.penetrate = -1;
+            Projectile.timeLeft = 10000;
+            Projectile.hostile = false;
+            Projectile.tileCollide = false;
+            Projectile.ignoreWater = true;
+        }
+        public override void AI()
+        {
+            Player player = Main.player[Projectile.owner];
+            Projectile.Center = player.Center + new Vector2(player.direction * 12, -12 * player.gravDir);
+            Projectile.spriteDirection = player.direction;
+            Projectile.velocity *= 0;
+            if(player.itemTime <= 0)
+            {
+                Projectile.Kill();
+            }
+        }
+       
+        public override bool PreDraw(ref Color lightColor)
+        {
+            return true;
+        }
+    }
+}
