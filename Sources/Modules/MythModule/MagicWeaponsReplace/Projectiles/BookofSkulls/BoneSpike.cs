2dc80eba98ba88ab3838e38776014008805b7da6
Omnielement <877183629@qq.com>
2022-09-18 16:35:49 +0800

    penetrate->1

--- Sources/Modules/MythModule/MagicWeaponsReplace/Projectiles/BookofSkulls/BoneSpike.cs
+++ Sources/Modules/MythModule/MagicWeaponsReplace/Projectiles/BookofSkulls/BoneSpike.cs
@@ -14,7 +14,7 @@ namespace Everglow.Sources.Modules.MythModule.MagicWeaponsReplace.Projectiles.Bo
             Projectile.height = 14;
             Projectile.friendly = false;
             Projectile.hostile = false;
-            Projectile.penetrate = 6;
+            Projectile.penetrate = 1;
             Projectile.timeLeft = 10000;
             Projectile.DamageType = DamageClass.MagicSummonHybrid;
             Projectile.tileCollide = false;
