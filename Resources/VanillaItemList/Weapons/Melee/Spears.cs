132735a157ef35f2538d0f25493155dc4b867637
Felixyang777 <1761610526@qq.com>
2022-10-01 12:33:18 +0800

    矛类

---
+++ Resources/VanillaItemList/Weapons/Melee/Spears.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Everglow.Resources.VanillaItemList.Weapons.Melee
+{
+    public class Spears : GlobalItem
+    {
+        private static List<int> vanillaSpears;
+        public override void Unload()
+        {
+            vanillaSpears = null;
+        }
+
+        public Spears()
+        {
+            vanillaSpears = new List<int>
+            {
+                
+            };
+        }
+    }
+}
