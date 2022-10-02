88f24e6c3f3cd8278e663bde06580f7ea0f09062
幽白银 <3122247915@qq.com>
2022-10-02 11:13:23 +0800

    对UI系统进行更新，同步为master的进度

--- Sources/Commons/Core/UI/UIElements/VerticalScrollbar.cs
+++ Sources/Commons/Core/UI/UIElements/VerticalScrollbar.cs
@@ -29,18 +29,24 @@ namespace Everglow.Sources.Commons.Core.UI.UIElements
             {
                 if (value <= 1f && value >= 0f)
                     wheelValue = value;
+                if (inner != null)
+                {
+                    float height = Info.Size.Y - 26f;
+                    inner.Info.Top.Pixel = height * wheelValue;
+                }
             }
         }
+        public float WhellValueMult = 1f;
         public VerticalScrollbar(float wheelValue = 0f)
         {
-            Info.Width=new PositionStyle(20f, 0f);
+            Info.Width = new PositionStyle(20f, 0f);
             Info.Left = new PositionStyle(-20f, 1f);
             Info.Height = new PositionStyle(-20f, 1f);
             Info.Top = new PositionStyle(10f, 0f);
             Info.TopMargin.Pixel = 5f;
             Info.ButtomMargin.Pixel = 5f;
             Info.IsSensitive = true;
-            uiScrollbarTexture = ModContent.Request<Texture2D>("Everglow/Sources/Commons/Core/UI/Images/VerticalScrollbar", AssetRequestMode.ImmediateLoad).Value;
+            uiScrollbarTexture = ModContent.Request<Texture2D>("BingoMod/UI/Images/VerticalScrollbar", AssetRequestMode.ImmediateLoad).Value;
             WheelValue = wheelValue;
         }
         public override void LoadEvents()
@@ -62,7 +68,7 @@ namespace Everglow.Sources.Commons.Core.UI.UIElements
         public override void OnInitialization()
         {
             base.OnInitialization();
-            inner = new UIImage(ModContent.Request<Texture2D>("Everglow/Sources/Commons/Core/UI/Images/VerticalScrollbarInner", AssetRequestMode.ImmediateLoad).Value, Color.White);
+            inner = new UIImage(ModContent.Request<Texture2D>("BingoMod/UI/Images/VerticalScrollbarInner", AssetRequestMode.ImmediateLoad).Value, Color.White);
             inner.Info.Left.Pixel = -(inner.Info.Width.Pixel - Info.Width.Pixel) / 2f;
             Register(inner);
         }
@@ -87,17 +93,17 @@ namespace Everglow.Sources.Commons.Core.UI.UIElements
             if (!isMouseHover)
                 whell = state.ScrollWheelValue;
 
-            //if (isMouseHover && whell != state.ScrollWheelValue)
-            //{
-            //    inner.Info.Top.Pixel -= (float)(state.ScrollWheelValue - whell) / 10f;
-            //    if (inner.Info.Top.Pixel > height)
-            //        inner.Info.Top.Pixel = height;
-            //    else if (inner.Info.Top.Pixel < 0)
-            //        inner.Info.Top.Pixel = 0;
-            //    whell = state.ScrollWheelValue;
-            //    WheelValue = inner.Info.Top.Pixel / height;
-            //    needCalculation = true;
-            //}
+            if (isMouseHover && whell != state.ScrollWheelValue)
+            {
+                inner.Info.Top.Pixel -= (float)(state.ScrollWheelValue - whell) / 30f * WhellValueMult;
+                if (inner.Info.Top.Pixel > height)
+                    inner.Info.Top.Pixel = height;
+                else if (inner.Info.Top.Pixel < 0)
+                    inner.Info.Top.Pixel = 0;
+                whell = state.ScrollWheelValue;
+                WheelValue = inner.Info.Top.Pixel / height;
+                needCalculation = true;
+            }
             if (isMouseDown && mouseY != Main.mouseY)
             {
                 inner.Info.Top.Pixel += (float)Main.mouseY - (float)mouseY;
@@ -115,16 +121,16 @@ namespace Everglow.Sources.Commons.Core.UI.UIElements
         }
         protected override void DrawSelf(SpriteBatch sb)
         {
-            sb.Draw(uiScrollbarTexture, new Rectangle(Info.HitBox.X + (Info.HitBox.Width - uiScrollbarTexture.Width) / 2, 
-                Info.HitBox.Y - 12, uiScrollbarTexture.Width, 12), 
+            sb.Draw(uiScrollbarTexture, new Rectangle(Info.HitBox.X + (Info.HitBox.Width - uiScrollbarTexture.Width) / 2,
+                Info.HitBox.Y - 12, uiScrollbarTexture.Width, 12),
                 new Rectangle(0, 0, uiScrollbarTexture.Width, 12), Color.White * alpha);
 
-            sb.Draw(uiScrollbarTexture, new Rectangle(Info.HitBox.X + (Info.HitBox.Width - uiScrollbarTexture.Width) / 2, 
-                Info.HitBox.Y, uiScrollbarTexture.Width, Info.HitBox.Height), 
+            sb.Draw(uiScrollbarTexture, new Rectangle(Info.HitBox.X + (Info.HitBox.Width - uiScrollbarTexture.Width) / 2,
+                Info.HitBox.Y, uiScrollbarTexture.Width, Info.HitBox.Height),
                 new Rectangle(0, 12, uiScrollbarTexture.Width, uiScrollbarTexture.Height - 24), Color.White * alpha);
 
-            sb.Draw(uiScrollbarTexture, new Rectangle(Info.HitBox.X + (Info.HitBox.Width - uiScrollbarTexture.Width) / 2, 
-                Info.HitBox.Y + Info.HitBox.Height, uiScrollbarTexture.Width, 12), 
+            sb.Draw(uiScrollbarTexture, new Rectangle(Info.HitBox.X + (Info.HitBox.Width - uiScrollbarTexture.Width) / 2,
+                Info.HitBox.Y + Info.HitBox.Height, uiScrollbarTexture.Width, 12),
                 new Rectangle(0, uiScrollbarTexture.Height - 12, uiScrollbarTexture.Width, 12), Color.White * alpha);
         }
     }
