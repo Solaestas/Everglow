6953cfe50f1327db51cf3dbc031cdbe60456f541
Omnielement <877183629@qq.com>
2022-10-10 18:01:36 +0800

    加了一把剑

---
+++ Sources/Modules/MythModule/TheFirefly/Items/Weapons/GlowWoodSword.cs
@@ -0,0 +1,35 @@
+﻿using Everglow.Sources.Modules.MythModule.TheFirefly.Projectiles;
+using Everglow.Sources.Modules.MythModule.TheFirefly.WorldGeneration;
+using Everglow.Sources.Modules.MythModule.Common;
+using Terraria.DataStructures;
+using ReLogic.Graphics;
+using Terraria.GameContent;
+using Terraria.ID;
+
+namespace Everglow.Sources.Modules.MythModule.TheFirefly.Items.Weapons
+{
+    public class GlowWoodSword : ModItem
+    {
+        public override void SetStaticDefaults()
+        {
+            GetGlowMask = MythContent.SetStaticDefaultsGlowMask(this);
+        }
+        public static short GetGlowMask = 0;
+        public override void SetDefaults()
+        {
+            Item.glowMask = GetGlowMask;
+            Item.damage = 13;
+            Item.DamageType = DamageClass.Melee;
+            Item.width = 56;
+            Item.height = 56;
+            Item.useTime = 20;
+            Item.useAnimation = 20;
+            Item.useStyle = ItemUseStyleID.Swing;
+            Item.knockBack = 1f;
+            Item.value = Item.sellPrice(0, 0, 0, 70);
+            Item.rare = ItemRarityID.White;
+            Item.UseSound = SoundID.Item1;
+            Item.autoReuse = false;
+        }
+    }
+}
