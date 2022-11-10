bfe9641403928cf4ef5590a54a4145cc56cc4e6d
Setnour6 <70764728+Setnour6@users.noreply.github.com>
2022-11-10 18:25:05 -0500

    Fix missing braces on FFBB

--- Sources/Modules/MythModule/TheFirefly/Items/Accessories/FireflyBorageBadge.cs
+++ Sources/Modules/MythModule/TheFirefly/Items/Accessories/FireflyBorageBadge.cs
@@ -23,22 +23,32 @@ namespace Everglow.Sources.Modules.MythModule.TheFirefly.Items.Accessories
             if (CorruptMoth.CorruptMothNPC != null && CorruptMoth.CorruptMothNPC.active)
             {
                 if (player.statDefense <= 48)
+                {
                     player.statDefense -= (player.statDefense / 4); //48 / 4, would be player.statDefense / 4, up to a value change of -12.
+                }
                 else
+                {
                     player.statDefense -= 12;
+                }
                 player.GetDamage(DamageClass.Generic) *= 1.18f;
                 player.GetAttackSpeed(DamageClass.SummonMeleeSpeed) *= 1.1f;
                 player.GetAttackSpeed(DamageClass.Melee) *= 1.1f;
                 if (player.GetCritChance(DamageClass.Melee) > 0)
+                {
                     player.GetTotalCritChance(DamageClass.Melee);
                     player.GetCritChance(DamageClass.Melee) = 0;
+                }
                 if (player.GetCritChance(DamageClass.MeleeNoSpeed) > 0)
+                {
                     player.GetTotalCritChance(DamageClass.MeleeNoSpeed);
                     player.GetCritChance(DamageClass.MeleeNoSpeed) = 0;
+                }
                 if (player.GetCritChance(DamageClass.SummonMeleeSpeed) > 0)
+                {
                     player.GetTotalCritChance(DamageClass.SummonMeleeSpeed);
                     player.GetCritChance(DamageClass.SummonMeleeSpeed) = 0;
-                player.GetCritChance(DamageClass.SummonMeleeSpeed) *= 0;
+                    player.GetCritChance(DamageClass.SummonMeleeSpeed) *= 0;
+                }
             } // Does anyone know how to set all melee weapon crit chances to 0? I can set them to negatives but it would look weird. ~Setnour6
         }
 
