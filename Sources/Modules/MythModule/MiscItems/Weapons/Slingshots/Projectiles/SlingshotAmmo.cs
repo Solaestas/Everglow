1928185fe04bdd1cf57a58494bfdba7fb0d2d508
Omnielement <877183629@qq.com>
2022-11-16 20:06:07 +0800

    好了100%

--- Sources/Modules/MythModule/MiscItems/Weapons/Slingshots/Projectiles/SlingshotAmmo.cs
+++ Sources/Modules/MythModule/MiscItems/Weapons/Slingshots/Projectiles/SlingshotAmmo.cs
@@ -11,18 +11,18 @@ namespace Everglow.Sources.Modules.MythModule.MiscItems.Weapons.Slingshots.Proje
         }
         public override void SetDefaults()
         {
-            Projectile.width = 10;
-            Projectile.height = 10;
+            Projectile.width = 20;
+            Projectile.height = 20;
             Projectile.friendly = true;
-            Projectile.hostile = true;
+            Projectile.hostile = false;
             Projectile.DamageType = DamageClass.Ranged;
             Projectile.aiStyle = -1;
-            Projectile.penetrate = 3;
+            Projectile.penetrate = 6;
             Projectile.timeLeft = 3600;
             Projectile.usesLocalNPCImmunity = true;
             Projectile.localNPCHitCooldown = 20;
             ProjectileID.Sets.TrailingMode[Projectile.type] = 0;
-            ProjectileID.Sets.TrailCacheLength[Projectile.type] = 15;
+            ProjectileID.Sets.TrailCacheLength[Projectile.type] = 30;
             SetDef();
         }
         public virtual void SetDef()
@@ -55,32 +55,45 @@ namespace Everglow.Sources.Modules.MythModule.MiscItems.Weapons.Slingshots.Proje
                 Projectile.velocity = Projectile.oldVelocity;
             }
             TimeTokill--;
-            Projectile.velocity.Y += 0.17f;
+            if(TimeTokill < 0)
+            {
+                Projectile.velocity.Y += 0.17f;
+            }
+            else
+            {
+                if(TimeTokill < 10)
+                {
+                    Projectile.damage = 0;
+                    Projectile.friendly = false;
+                }
+                Projectile.velocity *= 0f;
+            }
+
         }
 
         public override bool OnTileCollide(Vector2 oldVelocity)
         {
+            TimeTokill = 30;
+            Projectile.tileCollide = false;
+            Projectile.ignoreWater = true;
             AmmoHit();
             return false;
         }
         public override void OnHitNPC(NPC target, int damage, float knockback, bool crit)
         {
+            Projectile.ai[0] *= 0.4f;
+            Projectile.damage = (int)(Projectile.damage * 0.7);
+            Projectile.velocity *= 0.7f;
             AmmoHit();
         }
         public void AmmoHit()
         {
             Projectile.velocity = Projectile.oldVelocity;
-            Projectile.NewProjectile(Projectile.InheritSource(Projectile), Projectile.Center + Projectile.velocity, Vector2.Zero, ModContent.ProjectileType<NormalHit>(), (int)((double)Projectile.damage), Projectile.knockBack, Projectile.owner, 1f, Main.rand.NextFloat(6.283f));
+            Projectile.NewProjectile(Projectile.InheritSource(Projectile), Projectile.Center + Projectile.velocity, Vector2.Zero, ModContent.ProjectileType<NormalHit>(), Projectile.damage, Projectile.knockBack, Projectile.owner,Projectile.velocity.Length(), Main.rand.NextFloat(6.283f));
             for (int x = 0; x < 5; x++)
             {
                 Dust.NewDust(Projectile.position, Projectile.width, Projectile.height, 0, 0f, 0f, 0, default(Color), 0.7f);
             }
-            TimeTokill = 15;
-            Projectile.friendly = false;
-            Projectile.damage = 0;
-            Projectile.tileCollide = false;
-            Projectile.ignoreWater = true;
-            Projectile.aiStyle = -1;
         }
         public override bool PreDraw(ref Color lightColor)
         {
@@ -95,20 +108,22 @@ namespace Everglow.Sources.Modules.MythModule.MiscItems.Weapons.Slingshots.Proje
         }
         public override void PostDraw(Color lightColor)
         {
-            Projectile.ai[0] = TimeTokill;
-            float DrawC = Math.Clamp((Projectile.velocity.Length() - 12) / 24f, 0, 1f);
-            Main.spriteBatch.End();
-            Main.spriteBatch.Begin(SpriteSortMode.Immediate, BlendState.AlphaBlend, SamplerState.PointClamp, DepthStencilState.Default, RasterizerState.CullNone, null, Main.GameViewMatrix.TransformationMatrix);
+            float DrawC = Projectile.ai[0] * Projectile.ai[0];
+
             List<Vertex2D> bars = new List<Vertex2D>();
-            int TrueL = 1;
+            int TrueL = 0;
             for (int i = 1; i < Projectile.oldPos.Length; ++i)
             {
                 if (Projectile.oldPos[i] == Vector2.Zero)
                 {
+                    if(i == 1)
+                    {
+                        return;
+                    }
                     break;
                 }
 
-                TrueL++;
+                TrueL = i;
             }
             for (int i = 1; i < Projectile.oldPos.Length; ++i)
             {
@@ -123,27 +138,23 @@ namespace Everglow.Sources.Modules.MythModule.MiscItems.Weapons.Slingshots.Proje
                     width *= Projectile.timeLeft / 30f;
                 }
                 var normalDir = Projectile.oldPos[i - 1] - Projectile.oldPos[i];
-                if (normalDir.Length() < 0.2f)
-                {
-                    normalDir = Projectile.velocity / Projectile.velocity.Length();
-                }
-                normalDir = Vector2.Normalize(new Vector2(-normalDir.Y, normalDir.X));
+                normalDir = Utils.SafeNormalize(new Vector2(-normalDir.Y, normalDir.X),Vector2.Zero);
 
                 var factor = i / (float)TrueL;
                 var color = Color.Lerp(new Color(DrawC, DrawC, DrawC, 0), new Color(0, 0, 0, 0), factor);
-                var w = MathHelper.Lerp(1f, 0.05f, factor);
 
-                bars.Add(new Vertex2D(Projectile.oldPos[i] + normalDir * width + new Vector2(10, 10) - Main.screenPosition, color, new Vector3((float)Math.Sqrt(factor), 1, w)));
-                bars.Add(new Vertex2D(Projectile.oldPos[i] + normalDir * -width + new Vector2(10, 10) - Main.screenPosition, color, new Vector3((float)Math.Sqrt(factor), 0, w)));
+                bars.Add(new Vertex2D(Projectile.oldPos[i] + normalDir * -width + new Vector2(10, 10) - Main.screenPosition + Projectile.velocity, color, new Vector3(1, 0, 0)));
+                bars.Add(new Vertex2D(Projectile.oldPos[i] + normalDir * width + new Vector2(10, 10) - Main.screenPosition + Projectile.velocity, color, new Vector3(1, 1, 0)));
+
             }
+
             if (bars.Count > 2)
             {
                 Texture2D t = MythContent.QuickTexture("MagicWeaponsReplace/Projectiles/EShoot");
                 Main.graphics.GraphicsDevice.Textures[0] = t;
-                Main.graphics.GraphicsDevice.DrawUserPrimitives(PrimitiveType.TriangleList, bars.ToArray(), 0, bars.Count - 2);
-                Main.spriteBatch.End();
-                Main.spriteBatch.Begin(SpriteSortMode.Immediate, BlendState.AlphaBlend, SamplerState.PointClamp, DepthStencilState.Default, RasterizerState.CullNone, null, Main.GameViewMatrix.TransformationMatrix);
+                Main.graphics.GraphicsDevice.DrawUserPrimitives(PrimitiveType.TriangleStrip, bars.ToArray(), 0, bars.Count - 2);
             }
+
         }
     }
 }
--- Sources/Modules/MythModule/MiscItems/Weapons/Slingshots/Projectiles/SlingshotProjectile.cs
+++ Sources/Modules/MythModule/MiscItems/Weapons/Slingshots/Projectiles/SlingshotProjectile.cs
@@ -1,5 +1,7 @@
 ﻿using Terraria.Audio;
 using Everglow.Sources.Modules.MythModule.Common;
+using Everglow.Sources.Commons.Function.Vertex;
+
 namespace Everglow.Sources.Modules.MythModule.MiscItems.Weapons.Slingshots.Projectiles
 {
     public abstract class SlingshotProjectile : ModProjectile
@@ -16,13 +18,37 @@ namespace Everglow.Sources.Modules.MythModule.MiscItems.Weapons.Slingshots.Proje
         {
 
         }
+        /// <summary>
+        /// 内部变量,别动
+        /// </summary>
         internal bool Release = true;
+        /// <summary>
+        /// 内部变量,别动
+        /// </summary>
         internal int Power = 0;
+        /// <summary>
+        /// 默认NormalAmmo(常规弹)
+        /// </summary>
         internal int ShootProjType = ModContent.ProjectileType<NormalAmmo>();
+        /// <summary>
+        /// 中心到弹幕绑绳子位置的距离,默认14
+        /// </summary>
         internal int SlingshotLength = 14;
+        /// <summary>
+        /// Y形弹弓两头的距离,默认5
+        /// </summary>
+        internal int SplitBranchDis = 5;
+        /// <summary>
+        /// 最大蓄力,默认120(2s时间)
+        /// </summary>
+        internal int MaxPower = 120;
+        //TODO:以下的MouseLeft需要联机同步
         public override void AI()
         {
-            Power++;
+            if(Power < MaxPower)
+            {
+                Power++;
+            }
             Player player = Main.player[Projectile.owner];
             player.heldProj = Projectile.whoAmI;
             if (Power == 24 && Main.mouseLeft)
@@ -59,8 +85,7 @@ namespace Everglow.Sources.Modules.MythModule.MiscItems.Weapons.Slingshots.Proje
                 Projectile.Center = Main.player[Projectile.owner].MountedCenter + Vector2.Normalize(MouseToPlayer) * 15f + new Vector2(0, -4);
                 SoundEngine.PlaySound(new SoundStyle("Everglow/Sources/Modules/MythModule/MiscItems/Weapons/Slingshots/Sounds/SlingshotShoot"), Projectile.Center);
 
-                Projectile p = Projectile.NewProjectileDirect(Projectile.GetSource_FromAI(), Projectile.Center + SlingshotStringHead, -Vector2.Normalize(MinusShootDir) * (float)(Power / 5f + 8f), ShootProjType, (int)(Projectile.damage * (1 + Power / 40f)), Projectile.knockBack, player.whoAmI);
-                Main.NewText(p.damage);
+                Projectile.NewProjectileDirect(Projectile.GetSource_FromAI(), Projectile.Center + SlingshotStringHead, -Vector2.Normalize(MinusShootDir) * (float)(Power / 5f + 8f), ShootProjType, (int)(Projectile.damage * (1 + Power / 40f)), Projectile.knockBack, player.whoAmI, Power / 450f);
 
                 Projectile.timeLeft = 5;
                 Release = false;
@@ -70,7 +95,7 @@ namespace Everglow.Sources.Modules.MythModule.MiscItems.Weapons.Slingshots.Proje
                 Projectile.Center = Main.player[Projectile.owner].MountedCenter + Vector2.Normalize(MouseToPlayer) * 15f + new Vector2(0, -4);
             }
         }
-
+       
         public override bool PreDraw(ref Color lightColor)
         {
             return false;
@@ -95,11 +120,11 @@ namespace Everglow.Sources.Modules.MythModule.MiscItems.Weapons.Slingshots.Proje
             }
             Vector2 MouseToPlayer = Main.MouseWorld - player.MountedCenter;
             Vector2 SlingshotStringHead = new Vector2(SlingshotLength, -SlingshotLength).RotatedBy(DrawRot) + Projectile.Center - Main.MouseWorld;
-            Vector2 SlingshotStringTail = new Vector2(SlingshotLength, -SlingshotLength).RotatedBy(DrawRot) + Vector2.Normalize(SlingshotStringHead) * Power * 0.3125f;
+            Vector2 SlingshotStringTail = new Vector2(SlingshotLength, -SlingshotLength).RotatedBy(DrawRot) + Vector2.Normalize(SlingshotStringHead) * Power * 0.2625f;
             if (player.direction == -1)
             {
                 SlingshotStringHead = new Vector2(SlingshotLength, -SlingshotLength).RotatedBy(DrawRot + Math.PI / 2d) + Projectile.Center - Main.MouseWorld;
-                SlingshotStringTail = new Vector2(SlingshotLength, -SlingshotLength).RotatedBy(DrawRot + Math.PI / 2d) + Vector2.Normalize(SlingshotStringHead) * Power * 0.3125f;
+                SlingshotStringTail = new Vector2(SlingshotLength, -SlingshotLength).RotatedBy(DrawRot + Math.PI / 2d) + Vector2.Normalize(SlingshotStringHead) * Power * 0.2625f;
             }
 
             Vector2 SlingshotStringTailToPlayer = player.MountedCenter - (Projectile.Center + SlingshotStringTail);
@@ -125,6 +150,63 @@ namespace Everglow.Sources.Modules.MythModule.MiscItems.Weapons.Slingshots.Proje
                 DrawRot = Projectile.rotation - MathF.PI * 0.25f;
             }
             Main.spriteBatch.Draw(TexMain, Projectile.Center - Main.screenPosition, null, drawColor, DrawRot, TexMain.Size() / 2f, 1f, spriteEffect, 0);
+            DrawString();
+        }
+        public virtual void DrawString()
+        {
+            Player player = Main.player[Projectile.owner];
+            Color drawColor = Lighting.GetColor((int)(Projectile.Center.X / 16.0), (int)(Projectile.Center.Y / 16.0));
+            float DrawRot;
+            if (Projectile.Center.X < player.MountedCenter.X)
+            {
+                DrawRot = Projectile.rotation - MathF.PI / 4f;
+            }
+            else
+            {
+                DrawRot = Projectile.rotation - MathF.PI * 0.25f;
+            }
+            Vector2 HeadCenter = new Vector2(SlingshotLength, -SlingshotLength).RotatedBy(DrawRot);
+            if (player.direction == -1)
+            {
+                HeadCenter = new Vector2(SlingshotLength, -SlingshotLength).RotatedBy(DrawRot + Math.PI / 2d);
+            }
+            HeadCenter += Projectile.Center - Main.screenPosition;
+            Vector2 SlingshotStringHead = new Vector2(SlingshotLength, -SlingshotLength).RotatedBy(DrawRot) + Projectile.Center - Main.MouseWorld;
+            Vector2 SlingshotStringTail = new Vector2(SlingshotLength, -SlingshotLength).RotatedBy(DrawRot) + Vector2.Normalize(SlingshotStringHead) * Power * 0.2625f;
+            if (player.direction == -1)
+            {
+                SlingshotStringHead = new Vector2(SlingshotLength, -SlingshotLength).RotatedBy(DrawRot + Math.PI / 2d) + Projectile.Center - Main.MouseWorld;
+                SlingshotStringTail = new Vector2(SlingshotLength, -SlingshotLength).RotatedBy(DrawRot + Math.PI / 2d) + Vector2.Normalize(SlingshotStringHead) * Power * 0.2625f;
+            }
+            SlingshotStringTail += Projectile.Center - Main.screenPosition;
+            Vector2 Head1 = HeadCenter + Utils.SafeNormalize(HeadCenter.RotatedBy(Math.PI / 8 + DrawRot), Vector2.Zero) * SplitBranchDis;
+            Vector2 Head2 = HeadCenter - Utils.SafeNormalize(HeadCenter.RotatedBy(Math.PI / 8 + DrawRot), Vector2.Zero) * SplitBranchDis;
+            if (player.direction == -1)
+            {
+                Head1 = HeadCenter + Utils.SafeNormalize(HeadCenter.RotatedBy(Math.PI / 8 * 5 + DrawRot), Vector2.Zero) * SplitBranchDis;
+                Head2 = HeadCenter - Utils.SafeNormalize(HeadCenter.RotatedBy(Math.PI / 8 * 5 + DrawRot), Vector2.Zero) * SplitBranchDis;
+            }
+            Main.spriteBatch.End();
+            Main.spriteBatch.Begin(SpriteSortMode.Immediate, BlendState.AlphaBlend, SamplerState.PointClamp, DepthStencilState.Default, RasterizerState.CullNone, null, Main.GameViewMatrix.TransformationMatrix);
+            DrawTexLine(Head1, SlingshotStringTail, 1, drawColor, MythContent.QuickTexture("MiscItems/Weapons/Slingshots/Projectiles/String"));
+            DrawTexLine(Head2, SlingshotStringTail, 1, drawColor, MythContent.QuickTexture("MiscItems/Weapons/Slingshots/Projectiles/String"));
+            Main.spriteBatch.End();
+            Main.spriteBatch.Begin(SpriteSortMode.Immediate, BlendState.AlphaBlend, SamplerState.PointClamp, DepthStencilState.Default, RasterizerState.CullNone, null, Main.GameViewMatrix.TransformationMatrix);
+        }
+        public void DrawTexLine(Vector2 StartPos, Vector2 EndPos, float width, Color color, Texture2D tex)
+        {
+            Vector2 Width = Vector2.Normalize(StartPos - EndPos).RotatedBy(Math.PI / 2d) * width;
+
+            List<Vertex2D> vertex2Ds = new List<Vertex2D>();
+
+            vertex2Ds.Add(new Vertex2D(StartPos + Width, color, new Vector3(0, 0, 0)));
+            vertex2Ds.Add(new Vertex2D(StartPos - Width, color, new Vector3(0, 1, 0)));
+
+            vertex2Ds.Add(new Vertex2D(EndPos + Width, color, new Vector3(1, 0, 0)));
+            vertex2Ds.Add(new Vertex2D(EndPos - Width, color, new Vector3(1, 1, 0)));
+
+            Main.graphics.GraphicsDevice.Textures[0] = tex;
+            Main.graphics.GraphicsDevice.DrawUserPrimitives(PrimitiveType.TriangleStrip, vertex2Ds.ToArray(), 0, vertex2Ds.Count - 2);
         }
     }
 }
