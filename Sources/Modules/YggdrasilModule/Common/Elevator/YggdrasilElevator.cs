7997f42d51ea671d0e3c1bc438320c365583904b
Omnielement <877183629@qq.com>
2022-11-26 22:19:11 +0800

    testItem

--- Sources/Modules/YggdrasilModule/Common/Elevator/YggdrasilElevator.cs
+++ Sources/Modules/YggdrasilModule/Common/Elevator/YggdrasilElevator.cs
@@ -1,14 +1,27 @@
 using Everglow.Sources.Modules.ZYModule.Commons.Core;
 using Everglow.Sources.Modules.ZYModule.Commons.Core.DataStructures;
+using Everglow.Sources.Modules.ZYModule.Commons.Function;
 using Everglow.Sources.Modules.ZYModule.TileModule.Tiles;
 
 namespace Everglow.Sources.Modules.YggdrasilModule.Common.Elevator
 {
-    internal class YggdrasilElevator : DynamicTile
+    internal class YggdrasilElevator : DBlock
     {
-        public override Direction MoveCollision(AABB aabb, ref Vector2 velocity, ref Vector2 move, bool ignorePlats = false)
+        public override void AI()
         {
-            throw new NotImplementedException();
+            size = new Vector2(192, 32);
+            velocity = new Vector2(0, -0.5f);
+            base.AI();
+        }
+    }
+    public class ElevatorSummonSystem : ModSystem
+    {
+        public override void PostUpdateEverything()
+        {
+            if(Main.mouseRight && Main.mouseRightRelease)
+            {
+               
+            }
         }
     }
 }
