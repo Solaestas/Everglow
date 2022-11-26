4af4ddedd07587f8a9fb12feda93c77257782054
Omnielement <877183629@qq.com>
2022-11-26 23:54:22 +0800

    new texture

--- Sources/Modules/YggdrasilModule/Common/Elevator/YggdrasilElevator.cs
+++ Sources/Modules/YggdrasilModule/Common/Elevator/YggdrasilElevator.cs
@@ -11,7 +11,15 @@ namespace Everglow.Sources.Modules.YggdrasilModule.Common.Elevator
         {
             size = new Vector2(192, 32);
             velocity = new Vector2(0, -0.5f);
-            base.AI();
+        }
+        public override Color MapColor => new Color(122, 91, 79);
+        public override void Draw()
+        {
+            Main.spriteBatch.Draw(YggdrasilContent.QuickTexture("Common/Elevator/SkyTreeLiftLarge"), position - Main.screenPosition, new Rectangle(0, 0, (int)size.X, (int)size.Y), Color.White);
+        }
+        public override void DrawToMap(Vector2 mapTopLeft, Vector2 mapX2Y2AndOff, Rectangle? mapRect, float mapScale)
+        {
+            base.DrawToMap(mapTopLeft, mapX2Y2AndOff, mapRect, mapScale);
         }
     }
     public class ElevatorSummonSystem : ModSystem
