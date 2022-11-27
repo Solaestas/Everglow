71039a0d0700f18f8427a31de5b4ecece37c7ceb
Felixyang777 <1761610526@qq.com>
2022-11-27 16:34:20 +0800

    炒锅完成

--- Sources/Modules/FoodModule/Items/Weapons/FryingPanItem.cs
+++ Sources/Modules/FoodModule/Items/Weapons/FryingPanItem.cs
@@ -11,6 +11,11 @@ namespace Everglow.Sources.Modules.FoodModule.Items.Weapons
 {
     public class FryingPanItem : ModItem
     {
+        public override void SetStaticDefaults()
+        {
+            DisplayName.SetDefault("炒锅");
+            Tooltip.SetDefault("左键作为回旋镖投掷，长按右键作为近战武器");
+        }
         public override void SetDefaults()
         {
             Item.useStyle = ItemUseStyleID.Swing;
@@ -27,8 +32,8 @@ namespace Everglow.Sources.Modules.FoodModule.Items.Weapons
             Item.noMelee = true;
             Item.noUseGraphic = true;
 
-            Item.shootSpeed = 15;
-            Item.shoot = ModContent.ProjectileType<FryingPanNonMelee>();
+            Item.shootSpeed = 7.5f;
+            Item.shoot = ModContent.ProjectileType<FryingPan>();
 
             Item.value = Item.sellPrice(gold: 2);
         }
@@ -38,8 +43,15 @@ namespace Everglow.Sources.Modules.FoodModule.Items.Weapons
         {
             if (base.CanUseItem(player))
             {
-                if (Main.myPlayer == player.whoAmI)
+                if (Main.myPlayer == player.whoAmI )
                 {
+                    foreach (Projectile projectile in Main.projectile)
+                    {
+                        if (projectile.owner == player.whoAmI && projectile.type == ModContent.ProjectileType<FryingPan>() && projectile.active)
+                        {
+                            return false;
+                        }
+                    }
                     if (player.altFunctionUse == 2)//右键
                     {
                         Projectile proj = Projectile.NewProjectileDirect(player.GetSource_ItemUse(Item), player.Center, Vector2.Zero, ModContent.ProjectileType<FryingPan>(), player.GetWeaponDamage(Item), Item.knockBack, player.whoAmI);
@@ -61,7 +73,8 @@ namespace Everglow.Sources.Modules.FoodModule.Items.Weapons
         {
             if (player.altFunctionUse != 2)//左键
             {
-                Projectile.NewProjectile(source, player.Center, velocity, type, damage, knockback, Main.LocalPlayer.whoAmI, 0f, 0f);
+                Projectile proj = Projectile.NewProjectileDirect(source, player.Center, velocity, type, damage, knockback, Main.LocalPlayer.whoAmI, 0f, 0f);
+                proj.scale *= Item.scale;
             }
             
             return false;
