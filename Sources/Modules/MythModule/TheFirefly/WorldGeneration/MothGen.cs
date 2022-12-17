bc00781636e3abc5233a48a3f524889ed2ba5fa2
Omnielement <877183629@qq.com>
2022-12-17 22:21:01 +0800

    world Generation

--- Sources/Modules/MythModule/TheFirefly/WorldGeneration/MothGen.cs
+++ Sources/Modules/MythModule/TheFirefly/WorldGeneration/MothGen.cs
@@ -21,7 +21,7 @@ namespace Everglow.Sources.Modules.MythModule.TheFirefly.WorldGeneration
         {
             if (Main.mouseRight && Main.mouseRightRelease)
             {
-                //TestCode;
+                //Main.NewText(SubWorldModule.SubworldSystem.IsActive<MothWorld>());
             }
 
         }
@@ -351,56 +351,6 @@ namespace Everglow.Sources.Modules.MythModule.TheFirefly.WorldGeneration
                     }
                 }
             });
-            //int width = colors.GetLength(0);
-            //int height = colors.GetLength(1);
-            //for (int y = 0; y < height; y += 1)
-            //{
-            //    for (int x = 0; x < width; x += 1)
-            //    {
-            //        Tile tile = Main.tile[x + a, y + b];
-            //        switch (type)//21是箱子
-            //        {
-            //            case 0:
-            //                if (colors[x, y] == new Color(255, 0, 0, 255))
-            //                {
-            //                    if (tile.TileType != 21 && Main.tile[x + a, y + b - 1].TileType != 21)
-            //                    {
-            //                        tile.ClearEverything();
-            //                    }
-            //                }
-            //                break;
-            //            case 1:
-            //                if (colors[x, y] == new Color(56, 48, 61, 255))
-            //                {
-            //                    if (tile.TileType != 21 && Main.tile[x + a, y + b - 1].TileType != 21)
-            //                    {
-            //                        tile.TileType = (ushort)ModContent.TileType<Tiles.DarkCocoon>();
-            //                        tile.HasTile = true;
-            //                    }
-            //                }
-            //                if (colors[x, y] == new Color(0, 0, 255, 255))
-            //                {
-            //                    if (tile.TileType != 21 && Main.tile[x + a, y + b - 1].TileType != 21)
-            //                    {
-            //                        tile.LiquidType = LiquidID.Water;
-            //                        tile.LiquidAmount = 200;
-            //                        tile.HasTile = false;
-            //                        //WorldGen.PlaceLiquid(x, y, byte.MaxValue, 255);
-            //                    }
-            //                }
-            //                break;
-            //            case 2:
-            //                if (colors[x, y] == new Color(0, 0, 5, 255))
-            //                {
-            //                    if (tile.TileType != 21 && Main.tile[x + a, y + b - 1].TileType != 21)
-            //                    {
-            //                        tile.WallType = (ushort)ModContent.WallType<Walls.DarkCocoonWall>();
-            //                    }
-            //                }
-            //                break;
-            //        }
-            //    }
-            //}
         }
 
         /// <summary>
@@ -426,6 +376,13 @@ namespace Everglow.Sources.Modules.MythModule.TheFirefly.WorldGeneration
             Main.statusText = "CocoonAnotherStart";
             ShapeTile("Cocoon.bmp", a, b, 3);
             SmoothMothTile(a, b);
+            for(int x = 20;x < Main.maxTilesX - 20;x++)
+            {
+                for (int y = 20; y < Main.maxTilesY- 20; y++)
+                {
+                    RandomUpdate(x, y, ModContent.TileType<DarkCocoon>());
+                }
+            }
         }
 
         public static void BuildWorldMothCave()
@@ -445,6 +402,14 @@ namespace Everglow.Sources.Modules.MythModule.TheFirefly.WorldGeneration
             Main.statusText = "CocoonAnotherStart";
             ShapeTile("WorldCocoon.bmp", a, b, 3);
             SmoothMothTile(a, b);
+
+            for (int x = 0; x < 28; x++)
+            {
+                for (int y = 0; y < 24; y++)
+                {
+                    RandomUpdate(a + x,b + y, ModContent.TileType<DarkCocoon>());
+                }
+            }
         }
 
         private static int GetCrash(int PoX, int PoY)
@@ -543,6 +508,12 @@ namespace Everglow.Sources.Modules.MythModule.TheFirefly.WorldGeneration
             }
             return new Point16(PoX, PoY);
         }
+        /// <summary>
+        /// 判定是否平坦
+        /// </summary>
+        /// <param name="point"></param>
+        /// <param name="Width"></param>
+        /// <returns></returns>
         private static bool IsTileSmooth(Point point, int Width = 22)
         {
             if (point.X > Main.maxTilesX - 20 || point.Y > Main.maxTilesY - 20 || point.X < 20 || point.Y < 20)
@@ -580,6 +551,228 @@ namespace Everglow.Sources.Modules.MythModule.TheFirefly.WorldGeneration
                         WorldGen.TileFrame(x + a, y + b, true, false);
                     }
                     WorldGen.SquareWallFrame(x + a, y + b, true);
+                }
+            }
+        }
+        public static void BuildFluorescentTree(int i, int j, int height = 0)
+        {
+            if (j < 30)
+            {
+                return;
+            }
+            int Height = Main.rand.Next(7, height);
+
+            for (int g = 0; g < Height; g++)
+            {
+                Tile tile = Main.tile[i, j - g];
+                if (g > 3)
+                {
+                    if (Main.rand.NextBool(5))
+                    {
+                        Tile tileLeft = Main.tile[i - 1, j - g];
+                        tileLeft.TileType = (ushort)ModContent.TileType<FluorescentTree>();
+                        tileLeft.TileFrameY = 4;
+                        tileLeft.TileFrameX = (short)Main.rand.Next(4);
+                        tileLeft.HasTile = true;
+                    }
+                    if (Main.rand.NextBool(5))
+                    {
+                        Tile tileRight = Main.tile[i + 1, j - g];
+                        tileRight.TileType = (ushort)ModContent.TileType<FluorescentTree>();
+                        tileRight.TileFrameY = 5;
+                        tileRight.TileFrameX = (short)Main.rand.Next(4);
+                        tileRight.HasTile = true;
+                    }
+                }
+                if (g == 0)
+                {
+                    tile.TileType = (ushort)ModContent.TileType<FluorescentTree>();
+                    tile.TileFrameY = 0;
+                    tile.TileFrameX = 0;
+                    tile.HasTile = true;
+                    continue;
+                }
+                if (g == 1)
+                {
+                    tile.TileType = (ushort)ModContent.TileType<FluorescentTree>();
+                    tile.TileFrameY = -1;
+                    tile.TileFrameX = 0;
+                    tile.HasTile = true;
+                    continue;
+                }
+                if (g == 2)
+                {
+                    tile.TileType = (ushort)ModContent.TileType<FluorescentTree>();
+                    tile.TileFrameY = 3;
+                    tile.TileFrameX = (short)Main.rand.Next(4);
+                    tile.HasTile = true;
+                    continue;
+                }
+                if (g == Height - 1)
+                {
+                    tile.TileType = (ushort)ModContent.TileType<FluorescentTree>();
+                    tile.TileFrameY = 2;
+                    tile.TileFrameX = 0;
+                    tile.HasTile = true;
+                    continue;
+                }
+                tile.TileType = (ushort)ModContent.TileType<FluorescentTree>();
+                tile.TileFrameY = 1;
+                tile.TileFrameX = (short)Main.rand.Next(12);
+                tile.HasTile = true;
+            }
+        }
+        public static void RandomUpdate(int i,int j,int Type)
+        {
+            if(Main.tile[i,j].TileType != Type || !Main.tile[i, j].HasTile)
+            {
+                return;
+            }
+            if (Main.rand.NextBool(4))
+            {
+                if (Main.tile[i, j].Slope == SlopeType.Solid && Main.tile[i + 1, j].Slope == SlopeType.Solid && Main.tile[i - 1, j].Slope == SlopeType.Solid && Main.tile[i + 2, j].Slope == SlopeType.Solid && Main.tile[i - 2, j].Slope == SlopeType.Solid &&
+                    Main.tile[i, j + 1].Slope == SlopeType.Solid && Main.tile[i + 1, j + 1].Slope == SlopeType.Solid && Main.tile[i - 1, j + 1].Slope == SlopeType.Solid && Main.tile[i + 2, j + 1].Slope == SlopeType.Solid && Main.tile[i - 2, j + 1].Slope == SlopeType.Solid)//树木
+                {
+                    int MaxHeight = 0;
+                    for (int x = -2; x < 3; x++)
+                    {
+                        for (int y = -1; y > -30; y--)
+                        {
+                            if (j + y > 20)
+                            {
+                                if (Main.tile[i + x, j + y].HasTile || Main.tile[i + x, j + y].LiquidAmount > 3)
+                                {
+                                    return;
+                                }
+                            }
+                            MaxHeight = -y;
+                        }
+                    }
+                    if (MaxHeight > 7)
+                    {
+                        BuildFluorescentTree(i, j - 1, MaxHeight);
+                    }
+                }
+            }
+
+            if (!Main.tile[i, j - 1].HasTile && Main.tile[i, j].Slope == SlopeType.Solid && Main.tile[i, j - 1].LiquidAmount > 0)
+            {
+                Tile tile = Main.tile[i, j - 1];
+                tile.TileType = (ushort)(ModContent.TileType<Tiles.LampLotus>());
+                tile.HasTile = true;
+                tile.TileFrameX = (short)(28 * Main.rand.Next(8));
+            }
+            if (Main.rand.NextBool(6))//黑萤藤蔓
+            {
+                Tile t0 = Main.tile[i, j];
+
+                Tile t2 = Main.tile[i, j + 1];
+                if (t0.Slope == SlopeType.Solid && !t2.HasTile)
+                {
+                    t2.TileType = (ushort)ModContent.TileType<Tiles.BlackVine>();
+                    t2.HasTile = true;
+                    t2.TileFrameY = (short)(Main.rand.Next(6, 9) * 18);
+                }
+            }
+            if (!Main.tile[i, j - 1].HasTile && !Main.tile[i + 1, j - 1].HasTile && !Main.tile[i - 1, j - 1].HasTile && Main.tile[i, j].Slope == SlopeType.Solid && Main.tile[i - 1, j].Slope == SlopeType.Solid && Main.tile[i + 1, j].Slope == SlopeType.Solid)//黑萤苣
+            {
+                Tile t1 = Main.tile[i, j - 1];
+                Tile t2 = Main.tile[i, j - 2];
+                Tile t3 = Main.tile[i, j - 3];
+                for (int x = -1; x < 2; x++)
+                {
+                    for (int y = -3; y < 4; y++)
+                    {
+                        if (Main.tile[i + x, j + y].LiquidAmount > 3)
+                        {
+                            return;
+                        }
+                    }
+                }
+                if (Main.rand.NextBool(2))
+                {
+                    switch (Main.rand.Next(1, 10))
+                    {
+                        case 1:
+                            t1.TileType = (ushort)ModContent.TileType<Tiles.BlackStarShrubSmall>();
+                            t2.TileType = (ushort)ModContent.TileType<Tiles.BlackStarShrubSmall>();
+                            t1.HasTile = true;
+                            t2.HasTile = true;
+                            short numa = (short)(Main.rand.Next(0, 6) * 48);
+                            t1.TileFrameX = numa;
+                            t2.TileFrameX = numa;
+                            t1.TileFrameY = 16;
+                            t2.TileFrameY = 0;
+                            break;
+
+                        case 2:
+                            t1.TileType = (ushort)ModContent.TileType<Tiles.BlackStarShrubSmall>();
+                            t2.TileType = (ushort)ModContent.TileType<Tiles.BlackStarShrubSmall>();
+                            t1.HasTile = true;
+                            t2.HasTile = true;
+                            short num = (short)(Main.rand.Next(0, 6) * 48);
+                            t2.TileFrameX = num;
+                            t1.TileFrameX = num;
+                            t1.TileFrameY = 16;
+                            t2.TileFrameY = 0;
+                            break;
+
+                        case 3:
+                            t1.TileType = (ushort)ModContent.TileType<Tiles.BlackStarShrub>();
+                            t2.TileType = (ushort)ModContent.TileType<Tiles.BlackStarShrub>();
+                            t3.TileType = (ushort)ModContent.TileType<Tiles.BlackStarShrub>();
+                            t1.HasTile = true;
+                            t2.HasTile = true;
+                            t3.HasTile = true;
+                            short num1 = (short)(Main.rand.Next(0, 6) * 72);
+                            t3.TileFrameX = num1;
+                            t2.TileFrameX = num1;
+                            t1.TileFrameX = num1;
+                            t1.TileFrameY = 32;
+                            t2.TileFrameY = 16;
+                            t3.TileFrameY = 0;
+                            break;
+
+                        case 4:
+                            t1.TileType = (ushort)ModContent.TileType<Tiles.BlueBlossom>();
+                            t2.TileType = (ushort)ModContent.TileType<Tiles.BlueBlossom>();
+                            t3.TileType = (ushort)ModContent.TileType<Tiles.BlueBlossom>();
+                            t1.HasTile = true;
+                            t2.HasTile = true;
+                            t3.HasTile = true;
+                            short num2 = (short)(Main.rand.Next(0, 12) * 120);
+                            t3.TileFrameX = num2;
+                            t2.TileFrameX = num2;
+                            t1.TileFrameX = num2;
+                            t1.TileFrameY = 32;
+                            t2.TileFrameY = 16;
+                            t3.TileFrameY = 0;
+                            break;
+
+                        case 5:
+                            WorldGen.Place3x2(i - 1, j - 1, (ushort)ModContent.TileType<Tiles.BlackFrenLarge>(), Main.rand.Next(3));
+                            break;
+
+                        case 6:
+                            WorldGen.Place2x2(i - 1, j - 1, (ushort)ModContent.TileType<Tiles.BlackFren>(), Main.rand.Next(3));
+                            break;
+
+                        case 7:
+                            WorldGen.Place3x2(i - 1, j - 1, (ushort)ModContent.TileType<Tiles.BlackFrenLarge>(), Main.rand.Next(3));
+                            break;
+
+                        case 8:
+                            WorldGen.Place2x2(i - 1, j - 1, (ushort)ModContent.TileType<Tiles.BlackFren>(), Main.rand.Next(3));
+                            break;
+
+                        case 9:
+                            WorldGen.Place2x1(i - 1, j - 1, (ushort)ModContent.TileType<Tiles.CocoonRock>(), Main.rand.Next(3));
+                            break;
+
+                        case 10:
+                            WorldGen.Place2x1(i - 1, j - 1, (ushort)ModContent.TileType<Tiles.CocoonRock>(), Main.rand.Next(3));
+                            break;
+                    }
                 }
             }
         }
