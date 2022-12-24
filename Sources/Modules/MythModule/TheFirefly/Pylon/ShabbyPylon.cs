1fc5bd49a744b6b8b61d652d3bf5338d28ff1f75
lyc-Lacewing <lycrlsu01@gmail.com>
2022-12-24 11:43:30 +0800

    null check --> default check

--- Sources/Modules/MythModule/TheFirefly/Pylon/ShabbyPylon.cs
+++ Sources/Modules/MythModule/TheFirefly/Pylon/ShabbyPylon.cs
@@ -122,7 +122,7 @@ internal class ScreenMovePlayer : ModPlayer
         if (PylonSystem.Instance.firstEnableAnimation)
         {
             var firefly = TileEntity.ByPosition.FirstOrDefault(pair => pair.Value is ShabbyPylonTileEntity);
-            if (firefly.Value != null)
+            if (firefly.Value != default)
             {
                 var target = firefly.Key.ToWorldCoordinates() - Main.ScreenSize.ToVector2() / 2;
                 AnimationTimer++;
