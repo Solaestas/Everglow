e22fa5215c0b914498f271650f1464206fdcbcdf
Felixyang777 <1761610526@qq.com>
2022-12-26 09:12:01 +0800

    IIIDModule

---
+++ Sources/Modules/IIIDModule/IIIDModule.cs
@@ -0,0 +1,25 @@
+﻿using Everglow.Sources.Commons.Core.ModuleSystem;
+using Everglow.Sources.Commons.Function.ObjectPool;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using Terraria.Graphics.Effects;
+
+namespace Everglow.Sources.Modules.IIIDModule
+{
+    internal class IIIDModule : IModule
+    {
+        string IModule.Name => "IIID";
+        void IModule.Load()
+        {
+            if (!Main.dedServ)
+            {
+            }
+        }        
+        void IModule.Unload()
+        {
+        }
+    }
+}
