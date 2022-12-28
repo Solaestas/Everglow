74b9384eb0f156b59145362531377fb30a236672
Setnour6 <set.nour6@gmail.com>
2022-12-28 00:28:29 -0500

    Omnielement starts with LilyHarp

---
+++ Sources/Modules/MythModule/StarterInventoryMyth.cs
@@ -0,0 +1,31 @@
+using Everglow.Sources.Modules.MythModule.OmniElementItems;
+using static Steamworks.SteamUser;
+
+namespace Everglow.Sources.Modules.MythModule
+{
+    public class StarterInventoryMyth : ModPlayer
+    {
+        public ulong SteamID64 = GetSteamID().m_SteamID;
+        public override IEnumerable<Item> AddStartingItems(bool mediumCoreDeath)
+        {
+            //if (mediumCoreDeath)
+            //{
+            //    return new[]
+            //    {
+            //        new Item(ItemID.HealingPotion)
+            //    };
+            //}
+            if (SteamID64 is 76561199058565968 /*Omnielement*/)
+            {
+                return new[]
+                {
+                new Item(ModContent.ItemType<LilyHarp>()),
+                };  
+            }
+            return new[]
+            {
+            new Item(ItemID.CopperCoin, 0), // stack of 0 means you don't start with it. This code is needed.
+            };
+        }
+    }
+}
