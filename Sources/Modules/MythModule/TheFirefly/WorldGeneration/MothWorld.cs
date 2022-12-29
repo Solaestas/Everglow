0991da24d40980301ecd659318e4e98030c7375e
Omnielement <877183629@qq.com>
2022-12-29 14:00:20 +0800

    Debug

--- Sources/Modules/MythModule/TheFirefly/WorldGeneration/MothWorld.cs
+++ Sources/Modules/MythModule/TheFirefly/WorldGeneration/MothWorld.cs
@@ -5,11 +5,11 @@ namespace Everglow.Sources.Modules.MythModule.TheFirefly.WorldGeneration
 {
     internal class MothWorld : Subworld
     {
-
         public override int Width { get; } = 800;
         public override int Height { get; } = 600;
         public override List<GenPass> Tasks { get;} = new() { new MothLand.MothLandGenPass() };
         public override bool NormalUpdates => true;
+        public override bool ShouldSave => true;
         public override void OnLoad()
         {
             Main.worldSurface = 20;
