9a49e3ff52105a147dcdbaf427f3999820c24ab9
Omnielement <877183629@qq.com>
2023-03-21 15:42:18 +0800

    一个shader一个拖尾

---
+++ Sources/Modules/Myth/TheFirefly/Effects/Powderlization.fx
@@ -0,0 +1,24 @@
+﻿sampler uImage0 : register(s0);
+texture2D tex0;
+sampler2D uPerlinTex = sampler_state
+{
+    Texture = <tex0>;
+    MinFilter = Linear;
+    MagFilter = Linear;
+    AddressU = Wrap;
+    AddressV = Wrap;
+};
+float4 PixelShaderFunction(float2 coords : TEXCOORD0) : COLOR0
+{
+    float4 color = tex2D(uImage0, coords);
+    if (color.r + color.g + color.b > minr)
+        return color;
+    return float4(0, 0, 0, 0);
+}
+technique Technique1
+{
+    pass Test
+    {
+        PixelShader = compile ps_2_0 PixelShaderFunction();
+    }
+}
--- Sources/Modules/Myth/TheFirefly/Projectiles/CorruptDust.cs
+++ Sources/Modules/Myth/TheFirefly/Projectiles/CorruptDust.cs
@@ -1,3 +1,4 @@
+using Everglow.Myth.Common;
 using Everglow.Myth.TheFirefly.Dusts;
 using Terraria;
 
@@ -23,6 +24,8 @@ public class CorruptDust : ModProjectile
 		Projectile.alpha = 0;
 		Projectile.penetrate = 1;
 		Projectile.scale = 1;
+		ProjectileID.Sets.TrailingMode[Projectile.type] = 0;
+		ProjectileID.Sets.TrailCacheLength[Projectile.type] = 90;
 	}
 
 	public override void AI()
@@ -47,6 +50,69 @@ public class CorruptDust : ModProjectile
 
 	public override void PostDraw(Color lightColor)
 	{
-		base.PostDraw(lightColor);
+		DrawTrail();
+	}
+	private void DrawTrail()
+	{
+		float k1 = 60f;
+		float k0 = (2400 - Projectile.timeLeft) / k1;
+
+		if (Projectile.timeLeft <= 2400 - k1)
+			k0 = 1;
+
+		var c0 = new Color(0, k0 * k0 * 0.4f + 0.2f, k0 * 0.8f + 0.2f, 0);
+		var bars = new List<Vertex2D>();
+
+
+		int TrueL = 0;
+		for (int i = 1; i < Projectile.oldPos.Length; ++i)
+		{
+			if (Projectile.oldPos[i] == Vector2.Zero)
+				break;
+
+			TrueL++;
+		}
+		for (int i = 1; i < Projectile.oldPos.Length; ++i)
+		{
+			float width = 36;
+			if (Projectile.timeLeft <= 40)
+				width = Projectile.timeLeft * 0.9f;
+			if (i < 10)
+				width *= i / 10f;
+			if (Projectile.ai[0] == 3)
+				width *= 0.5f;
+			if (Projectile.oldPos[i] == Vector2.Zero)
+				break;
+
+			var normalDir = Projectile.oldPos[i - 1] - Projectile.oldPos[i];
+			normalDir = Vector2.Normalize(new Vector2(-normalDir.Y, normalDir.X));
+			var factor = i / (float)TrueL;
+			var w = MathHelper.Lerp(1f, 0.05f, factor);
+			float x0 = factor * 0.6f - (float)(Main.timeForVisualEffects / 35d) + 10000;
+			x0 %= 1f;
+			bars.Add(new Vertex2D(Projectile.oldPos[i] + normalDir * -width * (1 - factor) + new Vector2(5f, 5f) - Main.screenPosition, c0, new Vector3(x0, 1, w)));
+			bars.Add(new Vertex2D(Projectile.oldPos[i] + normalDir * width * (1 - factor) + new Vector2(5f, 5f) - Main.screenPosition, c0, new Vector3(x0, 0, w)));
+			var factorII = (i + 1) / (float)TrueL;
+			var x1 = factorII * 0.6f - (float)(Main.timeForVisualEffects / 35d) + 10000;
+			x1 %= 1f;
+			if (x0 > x1)
+			{
+				float DeltaValue = 1 - x0;
+				var factorIII = factorII * x0 + factor * DeltaValue;
+				bars.Add(new Vertex2D(Projectile.oldPos[i] + normalDir * -width * (1 - factorIII) + new Vector2(5f) - Main.screenPosition, c0, new Vector3(1, 1, 0)));
+				bars.Add(new Vertex2D(Projectile.oldPos[i] + normalDir * width * (1 - factorIII) + new Vector2(5f) - Main.screenPosition, c0, new Vector3(1, 0, 0)));
+				bars.Add(new Vertex2D(Projectile.oldPos[i] + normalDir * -width * (1 - factorIII) + new Vector2(5f) - Main.screenPosition, c0, new Vector3(0, 1, 0)));
+				bars.Add(new Vertex2D(Projectile.oldPos[i] + normalDir * width * (1 - factorIII) + new Vector2(5f) - Main.screenPosition, c0, new Vector3(0, 0, 0)));
+			}
+		}
+		Main.spriteBatch.End();
+		Main.spriteBatch.Begin(SpriteSortMode.Deferred, BlendState.AlphaBlend, Main.DefaultSamplerState, DepthStencilState.None, RasterizerState.CullNone, null, Main.GameViewMatrix.TransformationMatrix);
+		Texture2D t = MythContent.QuickTexture("TheFirefly/Projectiles/GoldLine");
+		Main.graphics.GraphicsDevice.Textures[0] = t;
+		if (bars.Count > 3)
+			Main.graphics.GraphicsDevice.DrawUserPrimitives(PrimitiveType.TriangleStrip, bars.ToArray(), 0, bars.Count - 2);
+		Main.spriteBatch.End();
+		Main.spriteBatch.Begin(SpriteSortMode.Deferred, BlendState.AlphaBlend, Main.DefaultSamplerState, DepthStencilState.None, RasterizerState.CullNone, null, Main.GameViewMatrix.TransformationMatrix);
+
 	}
 }
