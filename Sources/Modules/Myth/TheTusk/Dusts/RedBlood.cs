4000a225c6b08078812b4badc1ac407da102f755
Omnielement <877183629@qq.com>
2023-04-10 23:29:31 +0800

    it will dissolve now

--- Sources/Modules/Myth/TheTusk/Dusts/RedBlood.cs
+++ Sources/Modules/Myth/TheTusk/Dusts/RedBlood.cs
@@ -1,4 +1,4 @@
-﻿namespace Everglow.Myth.TheTusk.Dusts;
+namespace Everglow.Myth.TheTusk.Dusts;
 
 public class RedBlood : ModDust
 {
@@ -11,6 +11,8 @@ public class RedBlood : ModDust
 	{
 		dust.scale *= 0.99f;
 		dust.velocity.Y += 0.25f;
+		if (Main.tile[(int)(dust.position.X / 16f), (int)(dust.position.Y / 16f)].LiquidAmount > 0)
+			dust.active = false;
 		if (Collision.SolidCollision(dust.position, 0, 0))
 			dust.active = false;
 		return true;
