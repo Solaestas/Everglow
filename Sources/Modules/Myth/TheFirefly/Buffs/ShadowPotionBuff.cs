f135d85a3ae36e0d32a4a3615b41d86eb2b61523
Setnour6 <set.nour6@gmail.com>
2023-04-29 21:52:47 -0400

    Reduce night vision flickering

--- Sources/Modules/Myth/TheFirefly/Buffs/ShadowPotionBuff.cs
+++ Sources/Modules/Myth/TheFirefly/Buffs/ShadowPotionBuff.cs
@@ -1,7 +1,11 @@
+using Everglow.Commons.FeatureFlags;
+
 namespace Everglow.Myth.TheFirefly.Buffs;
 
 public class ShadowPotionBuff : ModBuff
 {
+	internal int LightTime = 0;
+	Player player = Main.LocalPlayer;
 	public override void SetStaticDefaults()
 	{
 		Main.buffNoSave[Type] = true;
@@ -11,14 +15,28 @@ public class ShadowPotionBuff : ModBuff
 		Color playerLight = Lighting.GetColor((int)(player.Center.X / 16), (int)(player.Center.Y / 16));
 		if(Math.Max(Math.Max(playerLight.R, playerLight.G), playerLight.B) <= 190)
 		{
-			player.nightVision = true;
 			player.statDefense += 15;
 			player.allDamage += 0.2f;
+			LightTime++;
+			if (EverglowConfig.DebugMode && LightTime is not 0 or 20)
+			{
+				CombatText.NewText(player.Hitbox, Color.White, "LightTime++");
+			}
 		}
 		if (Math.Max(Math.Max(playerLight.R, playerLight.G), playerLight.B) >= 230)
 		{
 			player.lifeRegen -= (int)(player.statLifeMax2 * 0.01f);
 			player.lifeRegenTime = 0;
+			LightTime--;
+			if (EverglowConfig.DebugMode && LightTime is not 0 or 20)
+			{
+				CombatText.NewText(player.Hitbox, Color.White, "LightTime++");
+			}
 		}
+		if (LightTime > 4)
+		{
+			player.nightVision = true;
+		}
+		if (LightTime > 20) { LightTime = 20; }
 	}
 }
