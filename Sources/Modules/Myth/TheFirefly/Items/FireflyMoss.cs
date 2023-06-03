402f86fc2d787a0a2906fd57a427f5aa528cf699
Omnielement <877183629@qq.com>
2023-06-03 19:52:05 +0800

    Moss finished

--- Sources/Modules/Myth/TheFirefly/Items/FireflyMoss.cs
+++ Sources/Modules/Myth/TheFirefly/Items/FireflyMoss.cs
@@ -4,8 +4,45 @@ public class FireflyMoss : ModItem
 {
 	public override void SetDefaults()
 	{
-		Item.DefaultToPlaceableTile(ModContent.TileType<Tiles.DarkCocoonMoss>());
 		Item.width = 20;
 		Item.height = 18;
+		Item.useStyle = ItemUseStyleID.Swing;
+		Item.useAnimation = 15;
+		Item.useTime = 10;
+		Item.useTurn = true;
+		Item.autoReuse = true;
+		Item.consumable = true;
+		Item.maxStack = Item.CommonMaxStack;
+	}
+	public override bool? UseItem(Player player)
+	{
+		int i = (int)(Main.MouseWorld.X / 16);
+		int j = (int)(Main.MouseWorld.Y / 16);
+		Tile tile = Main.tile[i, j];
+		if(tile.TileType == ModContent.TileType<Tiles.DarkCocoon>())
+		{
+			tile.TileType = (ushort)ModContent.TileType<Tiles.DarkCocoonMoss>();
+			WorldGen.SquareTileFrame(i, j, true);
+			NetMessage.SendTileSquare(player.whoAmI, i, j, TileChangeType.None);
+			return true;
+		}
+		else
+		{
+			return false;
+		}
+	}
+	public override bool CanUseItem(Player player)
+	{
+		int i = (int)(Main.MouseWorld.X / 16);
+		int j = (int)(Main.MouseWorld.Y / 16);
+		Tile tile = Main.tile[i, j];
+		if (tile.TileType == ModContent.TileType<Tiles.DarkCocoon>())
+		{
+			return true;
+		}
+		else
+		{
+			return false;
+		}
 	}
 }
