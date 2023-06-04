77041a2235c0da56eb10761b30212cf5355a75aa
Omnielement <877183629@qq.com>
2023-06-04 18:38:42 +0800

    hotfix bug

--- Sources/Modules/Ocean/Projectiles/Weapons/RampageShark.cs
+++ Sources/Modules/Ocean/Projectiles/Weapons/RampageShark.cs
@@ -45,7 +45,7 @@ public class RampageShark : ModProjectile
 					SoundEngine.PlaySound(new SoundStyle("Everglow/Ocean/Sounds/SharkGun0").WithVolumeScale(0.4f), Projectile.Center);
 				}
 				Vector2 newvelocity = velocity.RotatedBy(Main.rand.NextFloat(-Power / 244f, Power / 244f));
-				Projectile p = Projectile.NewProjectileDirect(item.GetSource_ItemUse(item), Projectile.Center + offset + velocity * 0.0f + random, newvelocity, rampage.ShootType, item.damage, item.knockBack, player.whoAmI);
+				Projectile p = Projectile.NewProjectileDirect(item.GetSource_ReleaseEntity(), Projectile.Center + offset + velocity * 0.0f + random, newvelocity, rampage.ShootType, item.damage, item.knockBack, player.whoAmI);
 				p.CritChance = (int)(item.crit + player.GetCritChance(DamageClass.Generic));
 
 				Dust d = Dust.NewDustDirect(Projectile.Center + offset + velocity * 0.7f - new Vector2(4, 8) + random, 0, 0, ModContent.DustType<BulletShell>(), velocity.X, velocity.Y, 0, default, 1f);
@@ -56,7 +56,7 @@ public class RampageShark : ModProjectile
 				Dust smog = Dust.NewDustDirect(Projectile.Center + offset + velocity * 1.3f - new Vector2(4, 8) + random, 0, 0, ModContent.DustType<MothSmog>(), 0, -2f, 0, default, 2f);
 				smog.alpha = 180;
 				float rot = newvelocity.ToRotation();
-				Projectile.NewProjectile(item.GetSource_ItemUse(item), Projectile.Center + offset * 1.5f + velocity * 1.3f + random, Vector2.Zero, ModContent.ProjectileType<RampageSharkHit>(), item.damage, item.knockBack, player.whoAmI, 0.06f, rot);
+				Projectile.NewProjectile(item.GetSource_ReleaseEntity(), Projectile.Center + offset * 1.5f + velocity * 1.3f + random, Vector2.Zero, ModContent.ProjectileType<RampageSharkHit>(), item.damage, item.knockBack, player.whoAmI, 0.06f, rot);
 			}
 			else
 			{
@@ -68,7 +68,7 @@ public class RampageShark : ModProjectile
 				for (int i = 0; i < Times; i++)
 				{
 					Vector2 newvelocity = velocity.RotatedBy(Main.rand.NextFloat(-Power / 66f, Power / 66f));
-					Projectile p = Projectile.NewProjectileDirect(item.GetSource_ItemUse(item), Projectile.Center + offset + velocity * 0.0f + random, newvelocity, rampage.ShootType, item.damage, item.knockBack, player.whoAmI);
+					Projectile p = Projectile.NewProjectileDirect(item.GetSource_ReleaseEntity(), Projectile.Center + offset + velocity * 0.0f + random, newvelocity, rampage.ShootType, item.damage, item.knockBack, player.whoAmI);
 					p.CritChance = (int)(item.crit + player.GetCritChance(DamageClass.Generic));
 					Dust d = Dust.NewDustDirect(Projectile.Center + offset + velocity * 0.7f - new Vector2(4, 8) + random, 0, 0, ModContent.DustType<BulletShell>(), velocity.X, velocity.Y, 0, default, 1f);
 					d.velocity = velocity.RotatedBy(-1.57 * player.direction - Main.rand.NextFloat(1.25f, 1.75f) * player.direction) * 0.4f * (24 + Power) / 54f;
@@ -77,7 +77,7 @@ public class RampageShark : ModProjectile
 					Dust smog = Dust.NewDustDirect(Projectile.Center + offset + velocity * 1.3f - new Vector2(4, 8) + random, 0, 0, ModContent.DustType<MothSmog>(), velocity.X * 0.15f, -2f, 0, default, 2.5f);
 					smog.alpha = 180;
 					float rot = newvelocity.ToRotation();
-					Projectile.NewProjectile(item.GetSource_ItemUse(item), Projectile.Center + offset * 1.5f + velocity * 1.3f + random, Vector2.Zero, ModContent.ProjectileType<RampageSharkHit>(), item.damage, item.knockBack, player.whoAmI, 0.12f, rot);
+					Projectile.NewProjectile(item.GetSource_ReleaseEntity(), Projectile.Center + offset * 1.5f + velocity * 1.3f + random, Vector2.Zero, ModContent.ProjectileType<RampageSharkHit>(), item.damage, item.knockBack, player.whoAmI, 0.12f, rot);
 				}
 
 			}
--- Sources/Modules/Ocean/Projectiles/Weapons/TsunamiShark.cs
+++ Sources/Modules/Ocean/Projectiles/Weapons/TsunamiShark.cs
@@ -55,7 +55,7 @@ public class TsunamiShark : ModProjectile
 			SoundEngine.PlaySound(new SoundStyle("Everglow/Ocean/Sounds/WaterGun").WithVolumeScale(0.4f), Projectile.Center);
 
 
-			Projectile p = Projectile.NewProjectileDirect(item.GetSource_ItemUse(item),
+			Projectile p = Projectile.NewProjectileDirect(item.GetSource_ReleaseEntity(),
                 Projectile.Center + toMuzzle + random, 
 				velocity,
 				ModContent.ProjectileType<TsunamiShark_bullet>(),
@@ -66,7 +66,7 @@ public class TsunamiShark : ModProjectile
 
 			float rot = velocity.ToRotation();
 			//TODO:子弹伤害校正，要求和被消耗的弹药种类挂钩
-			Projectile.NewProjectile(item.GetSource_ItemUse(item),
+			Projectile.NewProjectile(item.GetSource_ReleaseEntity(),
 				Projectile.Center + toMuzzle * 1.5f + velocity * 2.2f + random,
 				Vector2.Zero,
 				ModContent.ProjectileType<TsunamiShark_flame>(),
@@ -81,7 +81,7 @@ public class TsunamiShark : ModProjectile
 		{
 			if (player.ownedProjectileCounts[ModContent.ProjectileType<TsunamiShark_missile>()] < 20)
 			{
-				Projectile.NewProjectileDirect(item.GetSource_ItemUse(item), Projectile.Center + toMuzzle, velocity.RotatedBy(-Main.rand.NextFloat(-0.2f, 0.4f) * player.direction) * 2.4f, ModContent.ProjectileType<TsunamiShark_missile>(), (int)((overridedamage == -1 ? item.damage : overridedamage) * 3.64f), item.knockBack, player.whoAmI, Main.rand.NextFloat(0.6f, 1.4f));
+				Projectile.NewProjectileDirect(item.GetSource_ReleaseEntity(), Projectile.Center + toMuzzle, velocity.RotatedBy(-Main.rand.NextFloat(-0.2f, 0.4f) * player.direction) * 2.4f, ModContent.ProjectileType<TsunamiShark_missile>(), (int)((overridedamage == -1 ? item.damage : overridedamage) * 3.64f), item.knockBack, player.whoAmI, Main.rand.NextFloat(0.6f, 1.4f));
 			}
 			UseCount = 0;
 		}
