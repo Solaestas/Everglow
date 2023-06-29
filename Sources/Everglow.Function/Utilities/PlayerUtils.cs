49232be5429139f565796df090dbc76db716fb35
Setnour6 <set.nour6@gmail.com>
2023-06-29 13:06:48 -0400

    Developer Name Secret System
    Uses player names to check for secrets. Let me know if any of you want me to add/remove/change any listed player names
    
    `EvilPack.cs` example:
    ```cs
    string localPlayerName = Main.LocalPlayer.name;
          if (localPlayerName is "Cataclysmic Armageddon" || PlayerUtils.GetDevPlayerNames().Contains(localPlayerName, StringComparer.OrdinalIgnoreCase))
          {
           Main.NewText("Cataclysmic Armageddon's Long Lost Older Cousin Calamatious Annihilation the Corrupted Moth " + $"{Language.GetTextValue(Language.GetTextValue("Mods.Everglow.Common.Message.HasAwoken"))}", 175, 75, 255);
          }
          else
          {
           Main.NewText($"{Language.GetTextValue("Mods.Everglow.NPCName.CorruptMoth")} {Language.GetTextValue("Mods.Everglow.Common.Message.HasAwoken")}", 175, 75, 255);
          }
    ```

--- Sources/Everglow.Function/Utilities/PlayerUtils.cs
+++ Sources/Everglow.Function/Utilities/PlayerUtils.cs
@@ -1,5 +1,53 @@
+using BepInEx.AssemblyPublicizer;
+
 namespace Everglow.Commons.Utilities;
 
 public static class PlayerUtils
 {
+	public static List<string> GetDevPlayerNames()
+	{
+		List<string> devPlayerNames = new List<string>(){
+		
+		/*Mr. Skirt | DXTST*/
+		"Mr. Skirt","Mr Skirt","DXTsT",
+		/*Dirty Octopus | 脏渔歌*/
+		"Dirty Octopus","脏渔歌",
+		/*u_silver | 新萌の绿草*/
+		"u_silver", "usilver", "新萌の绿草",
+		/*ju_zhang | 太阳照常升起*/
+		"ju_zhang","太阳照常升起","ju zhang",
+		/*ye_you | 夜谷紫幽*/
+		"ye_you","ye you", "dsfgasdfg","Solaestas", "夜谷紫幽",
+		/*Slime1024 | 凝胶 | TheGelatum*/
+		"Slime1024","Slime_1024","凝胶","TheGelatum",
+		/*Omnielement | 万象元素*/
+		"Omnielement","Element Of All", "万象元素",
+		/*yiyang223*/
+		"yiyang223","yiyang",
+		/*Lacewing*/
+		"Lacewing","lyc-lacewing",
+		/*JSDA Ling*/
+		"JSDA Ling","JDSA Ling",
+		/*Jack Lyh*/
+		"Jack Lyh",
+		/*Cyrillya | crapsky223*/
+		"Cyril","Cyrillya","crapsky223",
+		/*FelixYang777*/
+		"FelixYang777","Felix Yang",
+		/*Colin Weiss*/
+		"Colin Weiss", "ColinWeiss",
+		/*SilverMoon | 942293328*/
+		"SilverMoon","942293328","Silverymoon",
+		/*Setnour6*/
+		"Setnour6",
+		/*Nomis*/
+		"Nomis",
+		/*DomesticFoxcy*/
+		"DomesticFoxcy","DomesticFoxy",
+		/*Plantare*/
+		"Plantare", "世纪小花"
+		};
+
+		return devPlayerNames;
+	}
 }
