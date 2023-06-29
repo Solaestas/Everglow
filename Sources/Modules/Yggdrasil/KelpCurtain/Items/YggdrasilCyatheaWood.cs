8ee934260773dfe02ed3c4fde735291f6dcd25f1
Omnielement <877183629@qq.com>
2023-06-30 00:05:03 +0800

    天穹桫椤

---
+++ Sources/Modules/Yggdrasil/KelpCurtain/Items/YggdrasilCyatheaWood.cs
@@ -0,0 +1,23 @@
+namespace Everglow.Yggdrasil.KelpCurtain.Items;
+
+public class YggdrasilCyatheaWood : ModItem
+{
+	public override void SetDefaults()
+	{
+		Item.width = 24;
+		Item.height = 22;
+		Item.rare = ItemRarityID.White;
+		Item.scale = 1f;
+		Item.createTile = ModContent.TileType<Tiles.DragonScaleWood>();
+		Item.useStyle = ItemUseStyleID.Swing;
+		Item.useTurn = true;
+		Item.useAnimation = 15;
+		Item.useTime = 10;
+		Item.autoReuse = true;
+		Item.consumable = true;
+		Item.width = 16;
+		Item.height = 16;
+		Item.maxStack = 999;
+		Item.value = 1000;
+	}
+}
