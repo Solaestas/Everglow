a2b3fcc83905284d3ab8ec289ae8a72c0e1dc11e
Setnour6 <set.nour6@gmail.com>
2023-07-08 15:12:45 -0400

    nevermind, the dust itself is better to use

--- Sources/Modules/Myth/TheFirefly/Buffs/ShadowPotionBuff.cs
+++ Sources/Modules/Myth/TheFirefly/Buffs/ShadowPotionBuff.cs
@@ -1,5 +1,4 @@
-using Everglow.Commons.FeatureFlags;
-using Terraria.DataStructures;
+using Everglow.Myth.TheFirefly.Dusts;
 using Terraria.ID;
 
 namespace Everglow.Myth.TheFirefly.Buffs;
@@ -20,12 +19,28 @@ public class ShadowPotionBuff : ModBuff
 			player.statDefense += 15;
 			player.allDamage += 0.2f;
 			LightTime++;
+
+			if (Main.rand.NextBool(30))
+			{
+				Dust dust = Dust.NewDustDirect(player.position, player.width, player.height, ModContent.DustType<FluorescentTreeDust>(), newColor: new(30, 0, 60), Alpha: 155);
+				dust.noGravity = true;
+				dust.velocity *= 0.5f;
+				dust.velocity.Y -= 1f;
+			}
 		}
 		if (Math.Max(Math.Max(playerLight.R, playerLight.G), playerLight.B) >= 230)
 		{
 			player.lifeRegen -= (int)(player.statLifeMax2 * 0.01f);
 			player.lifeRegenTime = 0;
 			LightTime--;
+
+			if (Main.rand.NextBool(10))
+			{
+				Dust dust = Dust.NewDustDirect(player.position, player.width, player.height, ModContent.DustType<FluorescentTreeDust>(), newColor: new(30, 0, 60), Alpha: 155);
+				dust.noGravity = true;
+				dust.velocity *= 0.5f;
+				dust.velocity.Y -= 1f;
+			}
 		}
 		if (LightTime > 4)
 		{
@@ -33,30 +48,5 @@ public class ShadowPotionBuff : ModBuff
 		}
 		if (LightTime > 20) { LightTime = 20; }
 
-	}
-}
-public class ShadowPotionPlayer : ModPlayer
-{
-	internal static int alpha = 0;
-	public override void DrawEffects(PlayerDrawSet drawInfo, ref float r, ref float g, ref float b, ref float a, ref bool fullBright)
-	{
-		if (Player.HasBuff(ModContent.BuffType<ShadowPotionBuff>()))
-		{
-			if (alpha < 155)
-				alpha++;
-			else
-				alpha--;
-
-			Dust dust = Dust.NewDustDirect(Player.position, Player.width, Player.height, DustID.Shadowflame, 0f, 0f, 155, default, 1.5f);
-			dust.noGravity = true;
-			dust.velocity *= 0.5f;
-			dust.velocity.Y -= 1f;
-
-			Color shadowSkinColor = new(80, 0, 120, alpha);
-
-			drawInfo.colorBodySkin = shadowSkinColor;
-			Player.skinColor = shadowSkinColor;
-		}
-		base.DrawEffects(drawInfo, ref r, ref g, ref b, ref a, ref fullBright);
 	}
 }
