00f552fb42e78927e3f7398a0a19755960086576
Setnour6 <set.nour6@gmail.com>
2023-07-24 12:32:12 -0400

    Add Underground Background

--- Sources/Modules/TwilightForest/Biomes/TwilightForestUndergroundBackgroundStyle.cs
+++ Sources/Modules/TwilightForest/Biomes/TwilightForestUndergroundBackgroundStyle.cs
@@ -6,10 +6,11 @@ namespace Everglow.TwilightForest.Biomes
 	{
 		public override void FillTextureArray(int[] textureSlots) 
 		{
-			textureSlots[0] = BackgroundTextureLoader.GetBackgroundSlot(Mod, "TwilightForest/Backgrounds/TwilightForestFar");
-			textureSlots[1] = BackgroundTextureLoader.GetBackgroundSlot(Mod, "TwilightForest/Backgrounds/TwilightForestFar");
-			textureSlots[2] = BackgroundTextureLoader.GetBackgroundSlot(Mod, "TwilightForest/Backgrounds/TwilightForestFar");
-			textureSlots[3] = BackgroundTextureLoader.GetBackgroundSlot(Mod, "TwilightForest/Backgrounds/TwilightForestFar");
+			string Path = "TwilightForest/Backgrounds/TwilightForestUnderground";
+			textureSlots[0] = BackgroundTextureLoader.GetBackgroundSlot(Mod, Path + "0");
+			textureSlots[1] = BackgroundTextureLoader.GetBackgroundSlot(Mod, Path + "0");
+			textureSlots[2] = BackgroundTextureLoader.GetBackgroundSlot(Mod, Path + "0");
+			textureSlots[3] = BackgroundTextureLoader.GetBackgroundSlot(Mod, Path + "0");
 		}
 	}
 }
