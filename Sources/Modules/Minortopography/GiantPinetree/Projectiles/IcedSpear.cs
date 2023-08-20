efd0e0be5cc4c8b3f46de8b5dce86bc077ff8eee
Setnour6 <set.nour6@gmail.com>
2023-08-20 12:15:33 -0400

    undo the undo of tile collision changes

--- Sources/Modules/Minortopography/GiantPinetree/Projectiles/IcedSpear.cs
+++ Sources/Modules/Minortopography/GiantPinetree/Projectiles/IcedSpear.cs
@@ -11,7 +11,7 @@ public class IcedSpear : ModProjectile
 		Projectile.hostile = false;
 		Projectile.width = 20;
 		Projectile.height = 20;
-		Projectile.tileCollide = true;
+		Projectile.tileCollide = false;
 		Projectile.timeLeft = 240;
 		Projectile.aiStyle = -1;
 		Projectile.penetrate = -1;
@@ -58,6 +58,7 @@ public class IcedSpear : ModProjectile
 			}
 			else
 			{
+				Projectile.tileCollide = true;
 				if (Collide(Projectile.Center))
 				{
 					Projectile.damage = (int)(Projectile.damage * 0.1f);
@@ -69,7 +70,6 @@ public class IcedSpear : ModProjectile
 				}
 				else if (!Collision.SolidCollision(Projectile.Center, 0, 0))
 				{
-					Projectile.tileCollide = true;
 					Projectile.velocity.Y += 0.25f;
 					Projectile.velocity *= 0.995f;
 					Projectile.rotation = (float)(Math.Atan2(Projectile.velocity.Y, Projectile.velocity.X) + Math.PI * 0.25);
