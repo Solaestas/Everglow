3d099effde7e5881c0183ecdd318c211defb8676
yiyang233 <1787058089@qq.com>
2023-08-29 20:31:01 +0800

    RapierMasterBoss

---
+++ Sources/Modules/EternalResolve/Bosses/Projectiles/CrutchRapier_Hostile.cs
@@ -0,0 +1,25 @@
+using Everglow.Commons.Weapons.StabbingSwords;
+using Everglow.EternalResolve.Items.Weapons.StabbingSwords;
+
+namespace Everglow.EternalResolve.Bosses.Projectiles
+{
+    public class CrutchRapier_Hostile : Rapier_Hostile
+    {
+        public override void SetDefaults()
+        {
+            Color = new Color(155, 162, 164);
+            base.SetDefaults();
+			TradeLength = 4;
+			TradeShade = 0.3f;
+			Shade = 0.2f;
+			FadeTradeShade = 0.64f;
+			FadeScale = 1;
+			TradeLightColorValue = 1f;
+			FadeLightColorValue = 0.4f;
+			MaxLength = 1.05f;
+			DrawWidth = 0.4f;
+			itemType = ModContent.ItemType<CrutchBayonet>();
+		}
+
+	}
+}
