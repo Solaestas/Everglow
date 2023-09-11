65ab5b6f6e71c1547722f229c19679c0d92f5111
Setnour6 <set.nour6@gmail.com>
2023-09-11 15:05:17 -0400

    Reimplement Old BlackStarFruit Light Draw Logic
    Also organized the content. PreDraw needs to be tested more to hopefully display the fruit in the intended position.

--- Sources/Modules/Myth/TheFirefly/NPCs/BlackStarFruit.cs
+++ Sources/Modules/Myth/TheFirefly/NPCs/BlackStarFruit.cs
@@ -1,12 +1,18 @@
 using Everglow.Myth.TheFirefly.Dusts;
 using Everglow.Myth.TheFirefly.Projectiles;
-using Terraria.Audio;
 using Terraria.DataStructures;
 
 namespace Everglow.Myth.TheFirefly.NPCs;
 
 public class BlackStarFruit : ModNPC
 {
+	/// <summary>
+	/// The time of the light from the entity. Used with <see cref="vectorRotationPosition"/> and <see cref="flo"/>
+	/// </summary>
+	private float EntityLightTime = 0;
+	private Vector2[] vectorRotationPosition = new Vector2[12];
+	private float[] floatLightPosition = new float[12];
+	//public override string Texture => "Everglow/" + ModAsset.FruitBombPath; // Best way I found to avoid path errors, and FruitBomb is empty anyways
 	public override void SetDefaults()
 	{
 		NPC.damage = 0;
@@ -42,11 +48,21 @@ public class BlackStarFruit : ModNPC
 	{
 		NPC.dontTakeDamage = false;
 		NPC.velocity = new Vector2(0, (float)Math.Sin(Main.time / 50f + NPC.Center.X + NPC.whoAmI) * 0.25f);
+		InitFruitLightEffect();
 		Lighting.AddLight(NPC.Center, 0, 0.1f, 0.8f);
 	}
 	public override void HitEffect(NPC.HitInfo hit)
 	{
 		Projectile.NewProjectile(NPC.GetSource_FromAI(), NPC.Center, Vector2.zeroVector, ModContent.ProjectileType<FruitBomb>(), 50, 3);
+	}
+	public override bool PreDraw(SpriteBatch spriteBatch, Vector2 screenPos, Color drawColor)
+	{
+		SpriteEffects effects = SpriteEffects.None;
+		if (NPC.spriteDirection == 1)
+			effects = SpriteEffects.FlipHorizontally;
+		Texture2D fruit = ModAsset.BlackStarFruit.Value;
+		Main.spriteBatch.Draw(fruit, NPC.Center - Main.screenPosition, null, new Color(10, 83, 110, 0), NPC.rotation, fruit.Size() / 2f, 0.025f * (float)(4 + Math.Sin(Main.time / 15d + NPC.position.X / 36d)), effects, 0f);
+		return false;
 	}
 	public override void PostDraw(SpriteBatch spriteBatch, Vector2 screenPos, Color drawColor)
 	{
@@ -54,15 +70,41 @@ public class BlackStarFruit : ModNPC
 		if (NPC.spriteDirection == 1)
 			effects = SpriteEffects.FlipHorizontally;
 		Texture2D lightShell = ModAsset.Lightball.Value;
+		Texture2D lightFruit = ModAsset.BlackStarFruitLight.Value;
 		var color = new Color(10, 83, 110, 0);
 		Main.spriteBatch.Draw(lightShell, NPC.Center - Main.screenPosition, null, color, NPC.rotation, lightShell.Size() / 2f, 0.025f * (float)(4 + Math.Sin(Main.time / 15d + NPC.position.X / 36d)), effects, 0f);
+		for (int j = 0; j < 12; j++)
+		{
+			Main.spriteBatch.Draw(lightFruit, NPC.Center - Main.screenPosition + vectorRotationPosition[j] * floatLightPosition[j] * EntityLightTime, null, new Color(1 - floatLightPosition[j], 1 - floatLightPosition[j], 1 - floatLightPosition[j], 0), NPC.rotation, new Vector2(5, 5), 1.2f * EntityLightTime * (1 - floatLightPosition[j]), effects, 0f);
+		}
 	}
 	public override bool? CanBeHitByProjectile(Projectile projectile)
 	{
-		if(projectile.type == ModContent.ProjectileType<FruitBomb>())
+		if (projectile.type == ModContent.ProjectileType<FruitBomb>())
 		{
 			return false;
 		}
 		return base.CanBeHitByProjectile(projectile);
+	}
+	private void InitFruitLightEffect()
+	{
+		if (EntityLightTime < 1)
+		{
+			EntityLightTime += 0.01f;
+		}
+		for (int j = 0; j < 12; j++)
+		{
+			floatLightPosition[j] *= 0.98f;
+			if (EntityLightTime <= 0.2f)
+			{
+				vectorRotationPosition[j] = new Vector2(0, 24).RotatedByRandom(3.1415926535 * 2);
+				floatLightPosition[j] = Main.rand.NextFloat(0f, 1f);
+			}
+			if (floatLightPosition[j] < 0.07f)
+			{
+				floatLightPosition[j] = 1;
+				vectorRotationPosition[j] = new Vector2(0, 20).RotatedByRandom(3.1415926535 * 2);
+			}
+		}
 	}
 }
