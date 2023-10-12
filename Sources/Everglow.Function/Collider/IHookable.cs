b2675a36f28c4f22ab18e78b0a6244294380d63c
夜幽 <28271655@qq.com>
2023-10-12 19:11:37 +0800

    hook and laser

---
+++ Sources/Everglow.Function/Collider/IHookable.cs
@@ -0,0 +1,11 @@
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Everglow.Commons.Collider;
+public interface IHookable
+{
+	public void SetHookPosition(Projectile hook);
+}
