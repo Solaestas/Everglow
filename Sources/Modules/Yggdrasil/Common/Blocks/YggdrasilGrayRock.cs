7c866dd80f54978c8c0ebb7b1f962e4ff2ccad35
元素·万象 <877183629@qq.com>
2023-12-14 10:42:26 +0800

    new blocks update

---
+++ Sources/Modules/Yggdrasil/Common/Blocks/YggdrasilGrayRock.cs
@@ -0,0 +1,25 @@
+namespace Everglow.Yggdrasil.Common.Blocks;
+
+public class YggdrasilGrayRock : ModTile
+{
+	public override void PostSetDefaults()
+	{
+		Main.tileSolid[Type] = true;
+		Main.tileMerge[Type][(ushort)ModContent.TileType<YggdrasilTown.Tiles.StoneScaleWood>()] = true;
+		Main.tileMerge[(ushort)ModContent.TileType<YggdrasilTown.Tiles.StoneScaleWood>()][Type] = true;
+
+		Main.tileMerge[Type][(ushort)ModContent.TileType<YggdrasilTown.Tiles.LampWood.DarkForestSoil>()] = true;
+		Main.tileMerge[(ushort)ModContent.TileType<YggdrasilTown.Tiles.LampWood.DarkForestSoil>()][Type] = true;
+
+		Main.tileMerge[Type][(ushort)ModContent.TileType<YggdrasilTown.Tiles.LampWood.DarkForestGrass>()] = true;
+		Main.tileMerge[(ushort)ModContent.TileType<YggdrasilTown.Tiles.LampWood.DarkForestGrass>()][Type] = true;
+		Main.tileBlockLight[Type] = true;
+
+		DustType = DustID.BorealWood;
+		MinPick = 180;
+		HitSound = SoundID.Dig;
+		AddMapEntry(new Color(96, 97, 99));
+
+	}
+
+}
