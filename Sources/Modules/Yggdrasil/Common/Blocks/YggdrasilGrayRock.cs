59187211ccb72ee9dceb51ede2adbfdf71c2303c
元素·万象 <877183629@qq.com>
2023-12-19 11:54:28 +0800

    try to merge YggdrailGrayRock with DarkForestSoil ,failed.

--- Sources/Modules/Yggdrasil/Common/Blocks/YggdrasilGrayRock.cs
+++ Sources/Modules/Yggdrasil/Common/Blocks/YggdrasilGrayRock.cs
@@ -13,13 +13,17 @@ public class YggdrasilGrayRock : ModTile
 
 		Main.tileMerge[Type][(ushort)ModContent.TileType<YggdrasilTown.Tiles.LampWood.DarkForestGrass>()] = true;
 		Main.tileMerge[(ushort)ModContent.TileType<YggdrasilTown.Tiles.LampWood.DarkForestGrass>()][Type] = true;
+
+		Main.tileMergeDirt[Type] = true;
 		Main.tileBlockLight[Type] = true;
 
 		DustType = DustID.BorealWood;
 		MinPick = 180;
 		HitSound = SoundID.Dig;
 		AddMapEntry(new Color(96, 97, 99));
-
 	}
-
+	public override void PostSetupTileMerge()
+	{
+		base.PostSetupTileMerge();
+	}
 }
