08f7bd8a77203db0308b1b889ecc2f21f28d46f7
元素·万象 <877183629@qq.com>
2023-12-25 18:39:26 +0800

    third..

--- Sources/Modules/Myth/TheFirefly/NPCs/Bosses/CorruptMoth.cs
+++ Sources/Modules/Myth/TheFirefly/NPCs/Bosses/CorruptMoth.cs
@@ -1084,7 +1084,6 @@ public class CorruptMoth : ModNPC
 		}//拳
 		if (NPC.ai[0] == 13)//
 		{
-			ShieldHealthValue = 0;
 			if (Timer == 0)
 			{
 				foreach (NPC butterfly in Main.npc)
@@ -1124,6 +1123,7 @@ public class CorruptMoth : ModNPC
 			}
 			else
 			{
+				ShieldHealthValue = 0;
 				NPC.ai[0] = 1;
 				Timer = 150;
 			}
