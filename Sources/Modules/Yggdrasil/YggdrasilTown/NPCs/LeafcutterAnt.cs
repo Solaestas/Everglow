3008f184afa941b1b9ffa568ab864defe0ab510a
yang felix <1761610526@qq.com>
2024-03-18 20:55:47 +0800

    切叶蚁

---
+++ Sources/Modules/Yggdrasil/YggdrasilTown/NPCs/LeafcutterAnt.cs
@@ -0,0 +1,57 @@
+using Everglow.Yggdrasil.YggdrasilTown.Dusts;
+using Terraria;
+using Terraria.DataStructures;
+using Terraria.GameContent.ItemDropRules;
+
+namespace Everglow.Yggdrasil.YggdrasilTown.NPCs;
+
+public class LeafcutterAnt : ModNPC
+{
+	public override void SetStaticDefaults()
+	{
+		Main.npcFrameCount[NPC.type] = 6;
+		YggdrasilTownNPC.RegisterYggdrasilTownNPC(Type);
+	}
+	public override void SetDefaults()
+	{
+
+		NPC.width = 54;
+		NPC.height = 42;
+		NPC.lifeMax = 80;
+		NPC.damage = 25;
+		NPC.defense = 10;
+		NPC.friendly = false;
+		NPC.aiStyle = 3;
+		NPC.knockBackResist = 0.5f;
+		NPC.value = 100;
+		NPC.HitSound = SoundID.NPCHit4;
+		NPC.DeathSound = SoundID.NPCDeath4;
+		AIType = NPCID.WalkingAntlion;
+		AnimationType = NPCID.WalkingAntlion;
+
+	}
+	public override void OnHitPlayer(Player target, Player.HurtInfo hurtInfo)
+	{
+		target.AddBuff(BuffID.Poisoned,600);
+	}
+	public override float SpawnChance(NPCSpawnInfo spawnInfo)
+	{
+		YggdrasilTownBiome YggdrasilTownBiome = ModContent.GetInstance<YggdrasilTownBiome>();
+		if (!YggdrasilTownBiome.IsBiomeActive(Main.LocalPlayer))
+			return 0f;
+		return 3f;
+	}
+
+	public override void OnSpawn(IEntitySource source)
+	{
+	}
+
+	public override void OnKill()
+	{
+		
+	}
+	public override void ModifyNPCLoot(NPCLoot npcLoot)
+	{
+//TODO 掉落物
+	}
+}
