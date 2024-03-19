b547013228f25b8ee8d025dde97f554acf15749c
yang felix <1761610526@qq.com>
2024-03-19 21:44:16 +0800

    WhisperingGhost

--- Sources/Modules/Yggdrasil/YggdrasilTown/NPCs/WhisperingGhost.cs
+++ Sources/Modules/Yggdrasil/YggdrasilTown/NPCs/WhisperingGhost.cs
@@ -3,6 +3,7 @@ using Everglow.Yggdrasil.YggdrasilTown.Dusts;
 using Terraria;
 using Terraria.DataStructures;
 using Terraria.GameContent.ItemDropRules;
+using Terraria.WorldBuilding;
 
 namespace Everglow.Yggdrasil.YggdrasilTown.NPCs;
 
@@ -28,7 +29,7 @@ public class WhisperingGhost : ModNPC
 		NPC.HitSound = SoundID.NPCHit4;
 		NPC.DeathSound = SoundID.NPCDeath4;
 		NPC.noGravity = true;
-		NPC.noTileCollide = true;
+		NPC.noTileCollide = false;
 
 
 	}
@@ -53,31 +54,41 @@ public class WhisperingGhost : ModNPC
 		Vector2 aimTarget = target.Center;
 		if (Collision.CanHit(NPC.Center, 1, 1, target.Center, 1, 1))
 		{
-            toAim = aimTarget - NPC.Center + new Vector2(0, MathF.Sin(timeValue) * 100);
-			NPC.velocity = Vector2.Normalize(toAim) * NPC.velocity.Length()*1.2f;
-			if (NPC.velocity.Length() >= 1.75f)
+			toAim = aimTarget - NPC.Center + new Vector2(0, MathF.Sin(timeValue) * 150);
+			NPC.velocity = Vector2.Normalize(toAim) * NPC.velocity.Length() * 1.2f;
+			if (NPC.velocity.Length() >= 2.5f)
 			{
-				NPC.velocity = Vector2.Normalize(NPC.velocity) * 1.75f;
+				NPC.velocity = Vector2.Normalize(NPC.velocity) * 2.5f;
 			}
 		}
 		else
 		{
-			toAim = new Vector2(MathF.Cos(0.5f* timeValue), MathF.Sin(0.75f* timeValue));
+			toAim = new Vector2(MathF.Cos(0.5f * timeValue), MathF.Sin(0.75f * timeValue));
 			NPC.velocity = Vector2.Normalize(toAim) * NPC.velocity.Length() * 0.9f;
 			if (NPC.velocity.Length() <= 0.75f)
 			{
-				NPC.velocity = Vector2.Normalize(NPC.velocity) *0.75f;
+				NPC.velocity = Vector2.Normalize(NPC.velocity) * 0.75f;
 			}
 		}
-		if(Collision.SolidTiles(NPC.position, NPC.width, NPC.height))
+		if (WorldUtils.Find(NPC.Center.ToTileCoordinates(), Searches.Chain(new Searches.Down(5), _cachedConditions_notNull, _cachedConditions_solid), out var _))
 		{
-			NPC.velocity += new Vector2(0, -0.1f);
+			float length = NPC.velocity.Length();
+			NPC.velocity += new Vector2(0, -0.25f);
+			NPC.velocity = Vector2.Normalize(NPC.velocity) * length;
+		}
+		else
+		{
+			float length = NPC.velocity.Length();
+			NPC.velocity += new Vector2(0, 0.125f);
+			NPC.velocity = Vector2.Normalize(NPC.velocity) * length;
 		}
 
-		
+
 
 
 	}
+	private static Terraria.WorldBuilding.Conditions.NotNull _cachedConditions_notNull = new Terraria.WorldBuilding.Conditions.NotNull();
+	private static Terraria.WorldBuilding.Conditions.IsSolid _cachedConditions_solid = new Terraria.WorldBuilding.Conditions.IsSolid();
 	public override void OnSpawn(IEntitySource source)
 	{
 	}
@@ -88,6 +99,6 @@ public class WhisperingGhost : ModNPC
 	}
 	public override void ModifyNPCLoot(NPCLoot npcLoot)
 	{
-		npcLoot.Add(ItemDropRule.Common(ItemID.Megaphone,50));
+		npcLoot.Add(ItemDropRule.Common(ItemID.Megaphone, 50));
 	}
 }
