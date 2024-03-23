310faa6fa1540a49471ef7256a5b7279b3c9d524
元素·万象 <877183629@qq.com>
2024-03-23 23:06:11 +0800

    new texture

--- Sources/Modules/Yggdrasil/YggdrasilTown/NPCs/BarkSpicyCaterpillar.cs
+++ Sources/Modules/Yggdrasil/YggdrasilTown/NPCs/BarkSpicyCaterpillar.cs
@@ -7,11 +7,33 @@ public class BarkSpicyCaterpillar : Caterpillar
 {
 	public override void SetDefaults()
 	{
+		
+		base.SetDefaults();
+
+		
 		SegmentBehavioralSize = 10;
 		SegmentHitBoxSize = 30;
 		SegmentCount = 10;
 		AnimationSpeed = 2;
-		base.SetDefaults();
+		NPC.knockBackResist = -0.12f;
+		NPC.lifeMax = 16;
+		NPC.damage = 6;
+		NPC.value = 3;
+		if(Main.expertMode)
+		{
+			NPC.knockBackResist = -0.08f;
+			NPC.lifeMax = 30;
+			NPC.damage = 8;
+			NPC.value = 7;
+		}
+		if(Main.masterMode)
+		{
+			NPC.knockBackResist = -0.04f;
+			NPC.lifeMax = 42;
+			NPC.damage = 11;
+			NPC.value = 10;
+		}
+
 	}
 	public override void OnSpawn(IEntitySource source)
 	{
@@ -51,22 +73,16 @@ public class BarkSpicyCaterpillar : Caterpillar
 		for (int j = 0; j < Segments.Count; j++)
 		{
 			Vector2 v0 = new Vector2(0, Main.rand.NextFloat(0, 6f)).RotatedByRandom(MathHelper.TwoPi);
-			int type = ModContent.Find<ModGore>("Everglow/BrownCaterpillar_gore1").Type;
+			int type = ModContent.Find<ModGore>("Everglow/BarkSpicyCaterpillar_gore1").Type;
 			if (j == 0)
 			{
-				type = ModContent.Find<ModGore>("Everglow/BrownCaterpillar_gore0").Type;
+				type = ModContent.Find<ModGore>("Everglow/BarkSpicyCaterpillar_gore0").Type;
 			}
 			if (j == Segments.Count - 1)
 			{
-				type = ModContent.Find<ModGore>("Everglow/BrownCaterpillar_gore2").Type;
+				type = ModContent.Find<ModGore>("Everglow/BarkSpicyCaterpillar_gore2").Type;
 			}
 			Gore.NewGore(NPC.GetSource_Death(), NPC.Center + Segments[j].SelfPosition, v0, type, NPC.scale);
-			if (Main.rand.NextBool(2))
-			{
-				v0 = new Vector2(0, Main.rand.NextFloat(0, 6f)).RotatedByRandom(MathHelper.TwoPi);
-				type = ModContent.Find<ModGore>("Everglow/BrownCaterpillar_gore3").Type;
-				Gore.NewGore(NPC.GetSource_Death(), NPC.Center + Segments[j].SelfPosition, v0, type, NPC.scale);
-			}
 		}
 		for (int i = 0; i < 4; i++)
 		{
