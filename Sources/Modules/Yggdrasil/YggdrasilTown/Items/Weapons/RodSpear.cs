1ddf47542413f52ed5dbd42dd950a562899ceab5
Cloudea <2025140062@qq.com>
2024-06-15 15:29:53 +0800

    Add Weapon: Rod Spear

---
+++ Sources/Modules/Yggdrasil/YggdrasilTown/Items/Weapons/RodSpear.cs
@@ -0,0 +1,47 @@
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using Terraria.Enums;
+
+namespace Everglow.Yggdrasil.YggdrasilTown.Items.Weapons;
+
+internal class RodSpear : ModItem
+{
+	public override void SetDefaults()
+	{
+		Item.width = 50;
+		Item.height = 54;
+
+		Item.useStyle = ItemUseStyleID.Swing;
+		Item.useAnimation = 45;
+		Item.useTime = 45;
+		Item.UseSound = SoundID.Item1;
+		Item.autoReuse = true;
+		Item.noUseGraphic = true;
+		Item.noMelee = true;
+
+		Item.damage = 18;
+		Item.DamageType = DamageClass.Melee;
+		Item.crit = 4;
+		Item.knockBack = 4f;
+
+		Item.SetShopValues(
+			ItemRarityColor.Green2,
+			Item.buyPrice(silver: 20));
+
+		Item.channel = false;
+
+		Item.shoot = ModContent.ProjectileType<Projectiles.RodSpear>();
+	}
+
+	public override void AddRecipes()
+	{
+		CreateRecipe(8)
+			.AddIngredient(ItemID.BorealWood, 2)
+			.AddIngredient(ItemID.IceBlock, 1)
+			.AddTile(TileID.IceBlock)
+			.Register();
+	}
+}
