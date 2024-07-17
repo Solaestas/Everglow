d0125f46af416b26489e1d362f4d36f66c41e6e4
元素·万象 <877183629@qq.com>
2024-07-17 14:57:29 +0800

    slingshot update

--- Sources/Modules/Yggdrasil/YggdrasilTown/Projectiles/BrittleRockSlingshotProj.cs
+++ Sources/Modules/Yggdrasil/YggdrasilTown/Projectiles/BrittleRockSlingshotProj.cs
@@ -9,8 +9,8 @@ public class BrittleRockSlingshotProj : SlingshotProjectile
 	public override void SetDef()
 	{
 		ShootProjType = ModContent.ProjectileType<BrittleRockSlingshotStone>();
-		SlingshotLength = 12;
-		MaxPower = 60;
+		SlingshotLength = 11;
+		MaxPower = 130;
 		SplitBranchDis = 10;
 	}
 
@@ -59,7 +59,7 @@ public class BrittleRockSlingshotProj : SlingshotProjectile
 			{
 				SoundEngine.PlaySound(new SoundStyle("Everglow/Myth/Misc/Sounds/SlingshotShoot2"), Projectile.Center);
 			}
-			Projectile.NewProjectileDirect(Projectile.GetSource_FromAI(), Projectile.Center + SlingshotStringHead, -Vector2.Normalize(MinusShootDir) * (float)(Power / 6f + 8f), ShootProjType, (int)(Projectile.damage * (0.5 + Power / 120)), Projectile.knockBack, player.whoAmI, Power / 450f);
+			Projectile.NewProjectileDirect(Projectile.GetSource_FromAI(), Projectile.Center + SlingshotStringHead, -Vector2.Normalize(MinusShootDir) * (float)(Power / 6f + 8f), ShootProjType, (int)(Projectile.damage * (0.5 + Power / 120)), Projectile.knockBack, player.whoAmI, 0, 0, Power / 130f);
 
 			Projectile.timeLeft = 5;
 			Release = false;
@@ -73,7 +73,8 @@ public class BrittleRockSlingshotProj : SlingshotProjectile
 	public override void DrawString()
 	{
 		Player player = Main.player[Projectile.owner];
-		Color drawColor = new Color(0.6f, 0.2f, 0.9f, 0.7f);
+		Color drawColor = new Color(0.6f, 0.2f, 0.9f, 0.1f) * (Power / 100f);
+		Color lightColor = Lighting.GetColor(Projectile.Center.ToTileCoordinates());
 		float DrawRot = Projectile.rotation - MathF.PI / 4f;
 		Vector2 HeadCenter = new Vector2(SlingshotLength, -SlingshotLength).RotatedBy(DrawRot);
 		if (player.direction == -1)
@@ -83,11 +84,11 @@ public class BrittleRockSlingshotProj : SlingshotProjectile
 
 		HeadCenter += Projectile.Center - Main.screenPosition;
 		Vector2 SlingshotStringHead = new Vector2(SlingshotLength, -SlingshotLength).RotatedBy(DrawRot) + Projectile.Center - Main.MouseWorld;
-		Vector2 SlingshotStringTail = new Vector2(SlingshotLength, -SlingshotLength).RotatedBy(DrawRot) + Vector2.Normalize(SlingshotStringHead) * Power * 0.2625f;
+		Vector2 SlingshotStringTail = new Vector2(SlingshotLength, -SlingshotLength).RotatedBy(DrawRot) + Vector2.Normalize(SlingshotStringHead) * Power * 0.4565f;
 		if (player.direction == -1)
 		{
 			SlingshotStringHead = new Vector2(SlingshotLength, -SlingshotLength).RotatedBy(DrawRot + Math.PI / 2d) + Projectile.Center - Main.MouseWorld;
-			SlingshotStringTail = new Vector2(SlingshotLength, -SlingshotLength).RotatedBy(DrawRot + Math.PI / 2d) + Vector2.Normalize(SlingshotStringHead) * Power * 0.2625f;
+			SlingshotStringTail = new Vector2(SlingshotLength, -SlingshotLength).RotatedBy(DrawRot + Math.PI / 2d) + Vector2.Normalize(SlingshotStringHead) * Power * 0.4565f;
 		}
 		SlingshotStringTail += Projectile.Center - Main.screenPosition;
 		Vector2 Head1 = HeadCenter + HeadCenter.RotatedBy(Math.PI / 8 + DrawRot).SafeNormalize(Vector2.Zero) * SplitBranchDis;
@@ -100,8 +101,24 @@ public class BrittleRockSlingshotProj : SlingshotProjectile
 		SpriteBatchState spriteBatchState = GraphicsUtils.GetState(Main.spriteBatch).Value;
 		Main.spriteBatch.End();
 		Main.spriteBatch.Begin(SpriteSortMode.Immediate, BlendState.AlphaBlend, SamplerState.PointClamp, DepthStencilState.Default, RasterizerState.CullNone, null, Main.GameViewMatrix.TransformationMatrix);
+		for (int i = 0; i < Power; i += 20)
+		{
+			DrawTexLine(Head1, SlingshotStringTail, 1, drawColor, Commons.ModAsset.String.Value);
+		}
 		DrawTexLine(Head1, SlingshotStringTail, 1, drawColor, Commons.ModAsset.String.Value);
-		DrawTexLine(Head2, SlingshotStringTail, 1, drawColor, Commons.ModAsset.String.Value);
+		Texture2D stone = ModAsset.BrittleRockSlingshotStone.Value;
+		Main.spriteBatch.Draw(stone, SlingshotStringTail, null, lightColor, 0, stone.Size() * 0.5f, MathF.Pow(Power / 130f, 0.3f), SpriteEffects.None, 0);
+		Texture2D stoneGlow = ModAsset.BrittleRockSlingshotStone_glow.Value;
+		for (int i = 0; i < Power; i += 10)
+		{
+			float wave = Power / 130f;
+			Color drawColor2 = new Color(0.6f + Main.rand.NextFloat(-wave,wave), 0.2f, 0.9f + Main.rand.NextFloat(-wave, wave), 0.1f) * (Power / 100f);
+			Main.spriteBatch.Draw(stoneGlow, SlingshotStringTail + new Vector2(0, Main.rand.NextFloat(Power / 40f)).RotatedByRandom(MathHelper.TwoPi), null, drawColor2, 0, stone.Size() * 0.5f, MathF.Pow(Power / 130f, 0.3f), SpriteEffects.None, 0);
+		}
+		for (int i = 0; i < Power; i += 20)
+		{
+			DrawTexLine(Head2, SlingshotStringTail, 1, drawColor, Commons.ModAsset.String.Value);
+		}
 		Main.spriteBatch.End();
 		Main.spriteBatch.Begin(spriteBatchState);
 	}
