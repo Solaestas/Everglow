1c8eff86d19fd60499cf91b34b9894afe0af95c8
元素·万象 <877183629@qq.com>
2024-08-24 15:05:36 +0800

    Add right click of sickle.

--- Sources/Modules/Yggdrasil/CorruptWormHive/Items/Weapons/TrueDeathSickle.cs
+++ Sources/Modules/Yggdrasil/CorruptWormHive/Items/Weapons/TrueDeathSickle.cs
@@ -27,27 +27,36 @@ public class TrueDeathSickle : ModItem
 	{
 		if (base.CanUseItem(player))
 		{
-			if (Main.myPlayer == player.whoAmI && player.itemTime == 0 && player.itemAnimation == 0)
+			if (Main.myPlayer == player.whoAmI && player.itemTime == 0 && player.itemAnimation == 0 && player.ownedProjectileCounts[ModContent.ProjectileType<TrueDeathSickleManager>()] + player.ownedProjectileCounts[ModContent.ProjectileType<TrueDeathSickle_RightClick>()] <= 0)
 			{
-				if (player.altFunctionUse != 2 && player.ownedProjectileCounts[ModContent.ProjectileType<TrueDeathSickleManager>()] <= 0)
+				if (player.altFunctionUse != 2)
 				{
 					player.itemTime = (int)(Item.useTime / player.meleeSpeed);
 					player.itemAnimation = (int)(Item.useAnimation / player.meleeSpeed);
 					Projectile.NewProjectile(player.GetSource_ItemUse(Item), player.Center, Vector2.Zero, ModContent.ProjectileType<TrueDeathSickleManager>(), player.GetWeaponDamage(Item), Item.knockBack, player.whoAmI);
 				}
-
-				// else//右键
-				// {
-				//    Projectile.NewProjectile(player.GetSource_ItemUse(Item), player.Center, Vector2.Zero, ModContent.ProjectileType<Projectiles.TrueDeathSickle_Super>(), player.GetWeaponDamage(Item), Item.knockBack, player.whoAmI);
-				// }
+				else// 右键
+				{
+					player.itemTime = (int)(Item.useTime / player.meleeSpeed);
+					player.itemAnimation = (int)(Item.useAnimation / player.meleeSpeed);
+					if (Main.MouseWorld.X > player.Center.X)
+					{
+						player.direction = 1;
+					}
+					else
+					{
+						player.direction = -1;
+					}
+					Projectile.NewProjectile(player.GetSource_ItemUse(Item), player.Center, Vector2.Zero, ModContent.ProjectileType<TrueDeathSickle_RightClick>(), player.GetWeaponDamage(Item), Item.knockBack, player.whoAmI, 250, 0, 0.5f * player.direction);
+				}
 			}
 			return false;
 		}
 		return base.CanUseItem(player);
 	}
 
-	// public override bool AltFunctionUse(Player player)
-	// {
-	//    return true;
-	// }
+	public override bool AltFunctionUse(Player player)
+	{
+		return true;
+	}
 }
