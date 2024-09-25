affa51e3e5b860a72b76551144f4496501b524b0
Cloudea <cloudeasoft@qq.com>
2024-09-25 10:11:07 +0800

    feat: complete functional codes of Ring Of Matter, add sprite for 1 projectile in YggdrasilTown

--- Sources/Modules/Yggdrasil/YggdrasilTown/Items/Accessories/RingOfMatter.cs
+++ Sources/Modules/Yggdrasil/YggdrasilTown/Items/Accessories/RingOfMatter.cs
@@ -4,27 +4,47 @@ namespace Everglow.Yggdrasil.YggdrasilTown.Items.Accessories;
 
 public class RingOfMatter : ModItem
 {
+	private float Bonus { get; set; } = 0;
+
 	public override void SetDefaults()
 	{
 		Item.width = 20;
 		Item.height = 12;
 		Item.accessory = true;
-		Item.SetShopValues(ItemRarityColor.Orange3, 100000);
+		Item.SetShopValues(ItemRarityColor.Orange3, Item.sellPrice(gold: 10));
 	}
 
 	public override void UpdateAccessory(Player player, bool hideVisual)
 	{
-		player.GetCritChance(DamageClass.Generic) += 4;
+		if (player.GetModPlayer<RingOfMatterPlayer>().HasNotEquipped)
+		{
+			int goldCoinNum = player.CountItem(ItemID.GoldCoin);
+			int platinumCoinNum = player.CountItem(ItemID.PlatinumCoin);
+			Bonus = (goldCoinNum + platinumCoinNum * 100) / 50;
+			if (Bonus > 10)
+			{
+				Bonus = 10;
+			}
+			Bonus /= 100;
+		}
+
+		player.GetDamage(DamageClass.Generic) += Bonus;
+		player.statDefense *= 1 + Bonus;
+		player.statLifeMax2 -= (int)(player.statLifeMax2 * (2 * Bonus));
+
+		player.GetModPlayer<RingOfMatterPlayer>().HasRingOfMatter = true;
 	}
 }
 
 public class RingOfMatterPlayer : ModPlayer
 {
-	public bool HasRingOfMatter = false;
-	public float CoinValue = 0;
+	public bool HasNotEquipped { get; private set; } = false;
+
+	public bool HasRingOfMatter { get; set; } = false;
 
 	public override void ResetEffects()
 	{
+		HasNotEquipped = HasRingOfMatter ? false : true;
 		HasRingOfMatter = false;
 	}
 }
