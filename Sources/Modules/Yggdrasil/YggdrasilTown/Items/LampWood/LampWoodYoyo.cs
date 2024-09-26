30d055d1139dafe78879756aaa79502042341c1a
Cloudea <cloudeasoft@qq.com>
2024-09-26 09:21:30 +0800

    feat: add 2 weapons in YggdrasilTown/LampWood

---
+++ Sources/Modules/Yggdrasil/YggdrasilTown/Items/LampWood/LampWoodYoyo.cs
@@ -0,0 +1,27 @@
+namespace Everglow.Yggdrasil.YggdrasilTown.Items.LampWood;
+
+public class LampWoodYoyo : ModItem
+{
+	public override void SetDefaults()
+	{
+		Item.width = 24;
+		Item.height = 24;
+
+		Item.useStyle = ItemUseStyleID.Shoot;
+		Item.useTime = 25;
+		Item.useAnimation = 25;
+		Item.noMelee = true;
+		Item.noUseGraphic = true;
+		Item.UseSound = SoundID.Item1;
+
+		Item.damage = 9;
+		Item.DamageType = DamageClass.MeleeNoSpeed;
+		Item.knockBack = 2.5f;
+		Item.channel = true;
+		Item.rare = ItemRarityID.White;
+		Item.value = Item.buyPrice(silver: 5);
+
+		Item.shoot = ModContent.ProjectileType<Projectiles.LampWoodYoyo>();
+		Item.shootSpeed = 16f;
+	}
+}
