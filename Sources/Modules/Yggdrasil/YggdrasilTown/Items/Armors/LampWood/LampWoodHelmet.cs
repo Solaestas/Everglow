9d1d3c64fc0b2066c2737f66f0fe37acf8378caa
Cloudea <cloudeasoft@qq.com>
2024-09-27 10:05:13 +0800

    feat: update Lamp Wood Armor set

--- Sources/Modules/Yggdrasil/YggdrasilTown/Items/Armors/LampWood/LampWoodHelmet.cs
+++ Sources/Modules/Yggdrasil/YggdrasilTown/Items/Armors/LampWood/LampWoodHelmet.cs
@@ -2,9 +2,15 @@ using Everglow.Yggdrasil.YggdrasilTown.Items.LampWood;
 using Terraria.GameContent.Creative;
 
 namespace Everglow.Yggdrasil.YggdrasilTown.Items.Armors.LampWood;
+
 [AutoloadEquip(EquipType.Head)]
 public class LampWoodHelmet : ModItem
 {
+	private const int PickSpeedBonus = 10;
+	private const int ArmorSetPickSpeedBonus = 10;
+	private const int ArmorSetLifeRegenBonus = 1;
+	private const int ArmorSetDefenseBonus = 2;
+
 	public override void SetStaticDefaults()
 	{
 		CreativeItemSacrificesCatalog.Instance.SacrificeCountNeededByItemId[Type] = 1;
@@ -14,22 +20,29 @@ public class LampWoodHelmet : ModItem
 	{
 		Item.width = 38;
 		Item.height = 24;
-		Item.value = 1340;
-		Item.rare = ItemRarityID.White;
-		Item.defense = 5;
+		Item.value = Item.sellPrice(silver: 20);
+		Item.rare = ItemRarityID.Green;
+		Item.defense = 2;
 	}
+
+	public override void UpdateEquip(Player player)
+	{
+		player.pickSpeed -= PickSpeedBonus / 100f;
+	}
+
 	public override bool IsArmorSet(Item head, Item body, Item legs)
 	{
 		return body.type == ModContent.ItemType<LampWoodBreastplate>() && legs.type == ModContent.ItemType<LampWoodLeggings>();
 	}
+
 	public override void UpdateArmorSet(Player player)
 	{
-		player.setBonus = "Increases defense damage by 6";
-		player.statDefense += 6;
+		player.statDefense += ArmorSetDefenseBonus;
+		player.lifeRegen += ArmorSetLifeRegenBonus * 2;
+		player.pickSpeed -= ArmorSetPickSpeedBonus / 100f;
+		Lighting.AddLight(player.Center, 0.5f, 0.5f, 0f);
 	}
-	public override void UpdateEquip(Player player)
-	{
-	}
+
 	public override void AddRecipes()
 	{
 		Recipe recipe = CreateRecipe();
--- Sources/Modules/Yggdrasil/YggdrasilTown/Items/Armors/LampWood/LampWoodLeggings.cs
+++ Sources/Modules/Yggdrasil/YggdrasilTown/Items/Armors/LampWood/LampWoodLeggings.cs
@@ -1,35 +1,37 @@
 using Everglow.Yggdrasil.YggdrasilTown.Items.LampWood;
 using Terraria.GameContent.Creative;
 
-namespace Everglow.Yggdrasil.YggdrasilTown.Items.Armors.LampWood
+namespace Everglow.Yggdrasil.YggdrasilTown.Items.Armors.LampWood;
+
+[AutoloadEquip(EquipType.Legs)]
+public class LampWoodLeggings : ModItem
 {
-	[AutoloadEquip(EquipType.Legs)]
-	public class LampWoodLeggings : ModItem
+	private const int MoveSpeedBonus = 5;
+
+	public override void SetStaticDefaults()
 	{
-		public override void SetStaticDefaults()
-		{
-			CreativeItemSacrificesCatalog.Instance.SacrificeCountNeededByItemId[Type] = 1;
-		}
+		CreativeItemSacrificesCatalog.Instance.SacrificeCountNeededByItemId[Type] = 1;
+	}
 
-		public override void SetDefaults()
-		{
-			Item.width = 20;
-			Item.height = 16;
-			Item.value = 1057;
-			Item.rare = ItemRarityID.White;
-			Item.defense = 3;
-		}
+	public override void SetDefaults()
+	{
+		Item.width = 20;
+		Item.height = 16;
+		Item.value = Item.sellPrice(silver: 20);
+		Item.rare = ItemRarityID.Green;
+		Item.defense = 2;
+	}
 
-		public override void UpdateEquip(Player player)
-		{
-			player.moveSpeed += 0.07f;
-		}
-		public override void AddRecipes()
-		{
-			Recipe recipe = CreateRecipe();
-			recipe.AddIngredient<LampWood_Wood>(40);
-			recipe.AddTile(TileID.WorkBenches);
-			recipe.Register();
-		}
+	public override void UpdateEquip(Player player)
+	{
+		player.moveSpeed += MoveSpeedBonus / 100f;
+	}
+
+	public override void AddRecipes()
+	{
+		Recipe recipe = CreateRecipe();
+		recipe.AddIngredient<LampWood_Wood>(40);
+		recipe.AddTile(TileID.WorkBenches);
+		recipe.Register();
 	}
 }
