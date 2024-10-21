9e4f1e68ed74733ed565ea0050159ea082aa00cb
Cloudea <cloudeasoft@qq.com>
2024-10-21 17:33:31 +0800

    feat: add new sprite Molten Core

--- Sources/Modules/Yggdrasil/YggdrasilTown/Items/Accessories/Furnace/MoltenCore.cs
+++ Sources/Modules/Yggdrasil/YggdrasilTown/Items/Accessories/Furnace/MoltenCore.cs
@@ -12,14 +12,17 @@ public class MoltenCore : ModItem
 	private const int MoveSpeedBonus = 5;
 	private const int JumpSpeedBonus = 5;
 
+	private const int ItemFrames = 4;
+
+	private int FrameCounter { get; set; } = 0;
+
 	public override void SetDefaults()
 	{
 		Item.width = 44;
 		Item.height = 46;
 		Item.accessory = true;
 
-		// TODO: Add new rarity color: lime
-		Item.SetShopValues(ItemRarityColor.Cyan9, Item.buyPrice(gold: 10));
+		Item.SetShopValues(ItemRarityColor.Lime7, Item.buyPrice(gold: 10));
 	}
 
 	public override void UpdateAccessory(Player player, bool hideVisual)
@@ -37,5 +40,48 @@ public class MoltenCore : ModItem
 			player.moveSpeed += MoveSpeedBonus / 100f;
 			player.jumpSpeedBoost += JumpSpeedBonus / 100f;
 		}
+	}
+
+	public override bool PreDrawInInventory(SpriteBatch spriteBatch, Vector2 position, Rectangle frame, Color drawColor, Color itemColor, Vector2 origin, float scale)
+	{
+		Texture2D texture = ModAsset.MoltenCore.Value;
+		int frameWidth = frame.Width;
+		int frameHeight = frame.Height / ItemFrames;
+		int frameOffsetY = frameHeight * (((int)Main.time / 5) % 4);
+		spriteBatch.Draw(
+			texture,
+			position,
+			new Rectangle(0, frameOffsetY, frameWidth, frameHeight),
+			drawColor,
+			0,
+			new Vector2(frameWidth, frameHeight) / 2,
+			1,
+			SpriteEffects.None,
+			0);
+
+		FrameCounter = (FrameCounter + 1) % ItemFrames;
+		return false;
+	}
+
+	public override bool PreDrawInWorld(SpriteBatch spriteBatch, Color lightColor, Color alphaColor, ref float rotation, ref float scale, int whoAmI)
+	{
+		Texture2D texture = ModAsset.MoltenCore.Value;
+		int frameWidth = texture.Width;
+		int frameHeight = texture.Height / ItemFrames;
+		int frameOffsetY = frameHeight * (((int)Main.time / 5) % 4);
+
+		spriteBatch.Draw(
+			texture,
+			Item.Center - Main.screenPosition,
+			new Rectangle(0, frameOffsetY, frameWidth, frameHeight),
+			lightColor,
+			0,
+			new Vector2(frameWidth, frameHeight / 2) / 2,
+			1,
+			SpriteEffects.None,
+			0);
+
+		FrameCounter = (FrameCounter + 1) % ItemFrames;
+		return false;
 	}
 }
