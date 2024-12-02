55c7a20356d3afd700ae7cc0af245567a251099e
元素·万象 <877183629@qq.com>
2024-12-02 17:02:53 +0800

    Yggdrasil relic fixed.

--- Sources/Modules/Yggdrasil/WorldGeneration/MainWorldGeneration_Yggdrasil.cs
+++ Sources/Modules/Yggdrasil/WorldGeneration/MainWorldGeneration_Yggdrasil.cs
@@ -41,7 +41,7 @@ public class MainWorldGeneratioin_Yggdrasil
 		int pointX = (int)(WorldGen.genRand.Next(80, 160) * (WorldGen.genRand.Next(2) - 0.5f) * 2 - 20 + Main.maxTilesX / 2);
 		int pointY = 160;
 
-		while (!IsTileSmooth(new Point(pointX, pointY)))
+		while (!IsTileSmooth(new Point(pointX, pointY)) || !AreaNoChest(pointX, pointX + 16, pointY - 13, pointY + 3))
 		{
 			pointX = (int)(WorldGen.genRand.Next(80, 240) * (WorldGen.genRand.Next(2) - 0.5f) * 2 - 20 + Main.maxTilesX / 2);
 			for (int y = 160; y < Main.maxTilesY / 3; y++)
@@ -58,6 +58,22 @@ public class MainWorldGeneratioin_Yggdrasil
 			}
 		}
 		return new Point16(pointX, pointY);
+	}
+
+	public static bool AreaNoChest(int x0, int x1, int y0, int y1)
+	{
+		for (int i = x0; i < x1; i++)
+		{
+			for (int j = y0; j < y1; j++)
+			{
+				Tile tile = SafeGetTile(i, j);
+				if (tile.TileType == 21 || tile.TileType == 467 || TileID.Sets.BasicChest[tile.TileType])
+				{
+					return false;
+				}
+			}
+		}
+		return true;
 	}
 
 	/// <summary>
@@ -79,11 +95,11 @@ public class MainWorldGeneratioin_Yggdrasil
 		var rightTile = SafeGetTile(x + width, y);
 		var leftTileUp = SafeGetTile(x, y - 1);
 		var rightTileUp = SafeGetTile(x + width, y - 1);
-		if (!leftTileUp.HasTile && !rightTileUp.HasTile)
+		if ((!leftTileUp.HasTile || leftTileUp.TileType == 3) && (!rightTileUp.HasTile || rightTileUp.TileType == 3))
 		{
 			if (leftTile.HasTile && rightTile.HasTile)
 			{
-				if((leftTile.TileType == TileID.Grass || leftTile.TileType == TileID.Dirt) && (rightTile.TileType == TileID.Grass || rightTile.TileType == TileID.Dirt))
+				if ((leftTile.TileType == TileID.Grass || leftTile.TileType == TileID.Dirt) && (rightTile.TileType == TileID.Grass || rightTile.TileType == TileID.Dirt))
 				{
 					return true;
 				}
