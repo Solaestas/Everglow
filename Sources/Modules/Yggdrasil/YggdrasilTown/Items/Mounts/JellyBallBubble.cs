b6fd55933c654bf73f59d03f08553d41850f0b4b
Cloudea <cloudeasoft@qq.com>
2024-12-05 09:43:12 +0800

    feat(Yggdrasil.YggdrasilTown): Jelly Bubble behavior adjustment

--- Sources/Modules/Yggdrasil/YggdrasilTown/Items/Mounts/JellyBallBubble.cs
+++ Sources/Modules/Yggdrasil/YggdrasilTown/Items/Mounts/JellyBallBubble.cs
@@ -10,29 +10,31 @@ public class JellyBallBubble : ModMount
 	public override void SetStaticDefaults()
 	{
 		// Movement
-		MountData.heightBoost = 20; // Height between the mount and the ground
-		MountData.flightTimeMax = int.MaxValue - 1; // The amount of time in frames a mount can be in the state of flying.
+		MountData.heightBoost = 20;
+		MountData.flightTimeMax = int.MaxValue - 1;
 		MountData.fatigueMax = int.MaxValue - 1;
 		MountData.fallDamage = 0f;
 		MountData.usesHover = true;
-		MountData.runSpeed = 11f; // The speed of the mount
-		MountData.dashSpeed = 8f; // The speed the mount moves when in the state of dashing.
-		MountData.acceleration = 0.19f; // The rate at which the mount speeds up.
-		MountData.jumpHeight = 5; // How high the mount can jump.
-		MountData.jumpSpeed = 4f; // The rate at which the player and mount ascend towards (negative y velocity) the jump height when the jump button is pressed.
-		MountData.blockExtraJumps = true; // Determines whether or not you can use a double jump (like cloud in a bottle) while in the mount.
-		MountData.constantJump = false; // Allows you to hold the jump button down.
+
+		MountData.runSpeed = 11f;
+		MountData.dashSpeed = 8f;
+		MountData.acceleration = 0.19f;
+
+		MountData.jumpHeight = 5;
+		MountData.jumpSpeed = 4f;
+		MountData.blockExtraJumps = true;
+		MountData.constantJump = false;
 
 		// Misc
-		MountData.buff = ModContent.BuffType<Buffs.JellyBallBubbleMount>(); // The ID number of the buff assigned to the mount.
+		MountData.buff = ModContent.BuffType<Buffs.JellyBallBubbleMount>();
 
 		// Visual Effects
-		MountData.spawnDust = ModContent.DustType<Dusts.JellyBallSpark>(); // The ID of the dust spawned when mounted or dismounted.
+		MountData.spawnDust = ModContent.DustType<Dusts.JellyBallSpark>();
 		MountData.spawnDustNoGravity = true;
 
 		// Frame data and player offsets
-		MountData.totalFrames = 4; // Amount of animation frames for the mount
-		MountData.playerYOffsets = Enumerable.Repeat(20, MountData.totalFrames).ToArray(); // Fills an array with values for less repeating code
+		MountData.totalFrames = 4;
+		MountData.playerYOffsets = Enumerable.Repeat(20, MountData.totalFrames).ToArray();
 		MountData.xOffset = 13;
 		MountData.yOffset = -12;
 		MountData.playerHeadOffset = 22;
@@ -78,31 +80,30 @@ public class JellyBallBubble : ModMount
 
 	public override void UpdateEffects(Player player)
 	{
-		bool playerIsNotMoving = !(player.controlLeft
-			|| player.controlRight
-			|| player.controlUp
-			|| player.controlDown
-			|| player.controlHook);
+		// Update the max speed of mount to player's max speed
+		MountData.runSpeed = player.maxRunSpeed;
+		MountData.dashSpeed = player.maxRunSpeed;
+		MountData.acceleration = 0.19f;
 
-		// TODO: Update the maxSpeed on X axis to player's maxSpeed
-
-		var position = FindSentryRestingSpot(player, player.MountedCenter, out int xPosition, out int yPosition);
+		// Buoyancy simulation
+		var bottomTilePosition = FindSentryRestingSpot(player, player.MountedCenter, out int xPosition, out int yPosition);
+		var playerIsNotMoving = !(player.controlLeft || player.controlRight || player.controlUp || player.controlDown);
 		if (playerIsNotMoving)
 		{
-			float distance = position.Y - player.MountedCenter.Y;
-
-			var offset = 0.5f;
-			var min = (Distance - offset) * 16f;
-			var max = (Distance + offset) * 16f;
-			var buoyancyFact = 1 - distance.SmoothStep(min, max);
-
-			var acceleration = GravityAcceleration - BuoyancyAcceleration * buoyancyFact;
-			player.velocity.Y += acceleration * 0.1f;
+			MountData.acceleration = 0.05f;
+			var distanceToBottomTile = bottomTilePosition.Y - player.MountedCenter.Y;
+			var offset = MathF.Abs(MountData.heightBoost) / (16f * 2f);
+			var buoyancyFact = 1 - distanceToBottomTile.SmoothStep((Distance - offset) * 16f, (Distance + offset) * 16f);
+			var acceleration = (GravityAcceleration - BuoyancyAcceleration * buoyancyFact) * 0.1f;
+			player.velocity.Y += acceleration;
 		}
 
-		// TODO: Delete development testing code
-		// Make bottom tile position visualizable
-		Dust.NewDust(position, 1, 1, DustID.Torch, 0, 0, 1);
+		// Make positions visualizable
+		if (Main.timeForVisualEffects % 3 == 0)
+		{
+			Dust.NewDust(player.MountedCenter, 2, 2, DustID.Cloud, 0, 0, 1);
+			Dust.NewDust(bottomTilePosition, 1, 1, DustID.Torch, 0, 0, 1);
+		}
 	}
 
 	public static Vector2 FindSentryRestingSpot(Player player, Vector2 position, out int worldX, out int worldY)
