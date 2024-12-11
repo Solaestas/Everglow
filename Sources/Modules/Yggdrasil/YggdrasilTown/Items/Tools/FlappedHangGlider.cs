671984b024596878023527adbc1aab659f880c3f
Cloudea <cloudeasoft@qq.com>
2024-12-11 16:30:23 +0800

    feat(Yggdrasil.YggdrasilTown): Add new item Flapped Hang Glider

---
+++ Sources/Modules/Yggdrasil/YggdrasilTown/Items/Tools/FlappedHangGlider.cs
@@ -0,0 +1,59 @@
+namespace Everglow.Yggdrasil.YggdrasilTown.Items.Tools;
+
+public class FlappedHangGlider : ModItem
+{
+	public const float VelocityXMAX = 10f;
+	public const float VelocityYMAX = 2f;
+
+	public override void SetDefaults()
+	{
+		Item.width = 54;
+		Item.height = 28;
+		Item.scale = 1.4f;
+
+		Item.holdStyle = ItemHoldStyleID.HoldUp;
+
+		Item.value = 0;
+		Item.rare = ItemRarityID.Gray;
+	}
+
+	public override void HoldItem(Player player)
+	{
+		if (!player.pulley && !player.controlDown)
+		{
+			// Manage velocity X
+			if (MathF.Abs(player.velocity.Y) > 0)
+			{
+				var targetVelocityX = player.velocity.X + player.direction * 0.2f;
+				if (MathF.Sign(targetVelocityX) != player.direction || MathF.Abs(targetVelocityX) <= VelocityXMAX)
+				{
+					player.velocity.X = targetVelocityX;
+				}
+			}
+
+			// Manage velocity Y
+			if (player.gravDir == -1f)
+			{
+				if (player.velocity.Y < -VelocityYMAX)
+				{
+					player.velocity.Y = -VelocityYMAX;
+				}
+			}
+			else if (player.velocity.Y > VelocityYMAX)
+			{
+				player.velocity.Y = VelocityYMAX;
+			}
+		}
+	}
+
+	public override void HoldStyle(Player player, Rectangle heldItemFrame)
+	{
+		if (!player.pulley)
+		{
+			player.itemRotation = player.direction * 0;
+			player.itemLocation.X = player.Center.X - 30 * player.direction;
+			player.itemLocation.Y = player.Center.Y + 0f;
+			player.fallStart = (int)(player.position.Y / 16f);
+		}
+	}
+}
