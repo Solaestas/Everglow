705fd309208912963cd72ab99b28a9841d6610ad
元素·万象 <877183629@qq.com>
2024-12-15 11:32:51 +0800

    New dust

--- Sources/Modules/Yggdrasil/YggdrasilTown/Dusts/YggdrasilAmberMoon.cs
+++ Sources/Modules/Yggdrasil/YggdrasilTown/Dusts/YggdrasilAmberMoon.cs
@@ -6,23 +6,25 @@ public class YggdrasilAmberMoon : ModDust
 	{
 		base.SetStaticDefaults();
 	}
+
 	public override Color? GetAlpha(Dust dust, Color lightColor)
 	{
 		Color c = lightColor;
 		c.A = 120;
 		return c;
 	}
+
 	public override void OnSpawn(Dust dust)
 	{
 		dust.color.A = 255;
 		base.OnSpawn(dust);
 	}
+
 	public override bool Update(Dust dust)
 	{
-		
 		dust.color.A -= 15;
 		dust.frame = new Rectangle(0, 0, 8, 10);
-		if(dust.color.A < 200)
+		if (dust.color.A < 200)
 		{
 			dust.frame.Y = 10;
 		}
@@ -34,7 +36,7 @@ public class YggdrasilAmberMoon : ModDust
 		{
 			dust.active = false;
 		}
-		dust.rotation = MathF.Atan2(dust.velocity.Y ,dust.velocity.X);
+		dust.rotation = MathF.Atan2(dust.velocity.Y, dust.velocity.X);
 		dust.position += dust.velocity;
 		dust.velocity *= 0.9f;
 		return false;
