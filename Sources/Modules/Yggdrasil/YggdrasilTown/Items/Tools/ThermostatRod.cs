98b48164fb36208947225e94ee3fa9d0138da0b7
Cloudea <cloudeasoft@qq.com>
2024-12-16 09:16:06 +0800

    fix(Yggdrasil.YggdrasilTown): Update the appearance of Thermostat Rod to match its texture

--- Sources/Modules/Yggdrasil/YggdrasilTown/Items/Tools/ThermostatRod.cs
+++ Sources/Modules/Yggdrasil/YggdrasilTown/Items/Tools/ThermostatRod.cs
@@ -1,6 +1,5 @@
 using Everglow.Yggdrasil.YggdrasilTown.Projectiles;
 using Terraria.DataStructures;
-using Terraria.Enums;
 
 namespace Everglow.Yggdrasil.YggdrasilTown.Items.Tools;
 
@@ -14,7 +13,9 @@ public class ThermostatRod : ModItem
 	public override void SetDefaults()
 	{
 		Item.CloneDefaults(ItemID.WoodFishingPole);
-		Item.SetShopValues(ItemRarityColor.White0, Item.buyPrice(silver: 10));
+
+		Item.value = Item.buyPrice(silver: 10);
+		Item.rare = ItemRarityID.White;
 
 		Item.fishingPole = 10;
 		Item.shootSpeed = 12f;
@@ -28,35 +29,13 @@ public class ThermostatRod : ModItem
 
 	public override bool Shoot(Player player, EntitySource_ItemUse_WithAmmo source, Vector2 position, Vector2 velocity, int type, int damage, float knockback)
 	{
-		int bobberAmount = Main.rand.Next(3, 6); // 3 to 5 bobbers
-		float spreadAmount = 75f; // how much the different bobbers are spread out.
-
-		for (int index = 0; index < bobberAmount; ++index)
-		{
-			Vector2 bobberSpeed = velocity + new Vector2(Main.rand.NextFloat(-spreadAmount, spreadAmount) * 0.05f, Main.rand.NextFloat(-spreadAmount, spreadAmount) * 0.05f);
-
-			// Generate new bobbers
-			Projectile.NewProjectile(source, position, bobberSpeed, type, 0, 0f, player.whoAmI);
-		}
+		Projectile.NewProjectile(source, position, velocity, type, 0, 0f, player.whoAmI);
 		return false;
 	}
 
 	public override void ModifyFishingLine(Projectile bobber, ref Vector2 lineOriginOffset, ref Color lineColor)
 	{
-		// Change these two values in order to change the origin of where the line is being drawn.
-		// This will make it draw 43 pixels right and 30 pixels up from the player's center, while they are looking right and in normal gravity.
-		lineOriginOffset = new Vector2(43, -30);
-
-		// Sets the fishing line's color. Note that this will be overridden by the colored string accessories.
-		if (bobber.ModProjectile is ThermostatBobber exampleBobber)
-		{
-			// ExampleBobber has custom code to decide on a line color.
-			lineColor = exampleBobber.FishingLineColor;
-		}
-		else
-		{
-			// If the bobber isn't ExampleBobber, a Fishing Bobber accessory is in effect and we use DiscoColor instead.
-			lineColor = Main.DiscoColor;
-		}
+		lineOriginOffset = new Vector2(36, -20);
+		lineColor = ThermostatBobber.FishingLineColor;
 	}
 }
