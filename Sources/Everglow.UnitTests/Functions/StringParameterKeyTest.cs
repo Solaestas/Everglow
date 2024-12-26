de07da13ce1a65adb33debeb910fb70276cc74d8
Cloudea <cloudeasoft@qq.com>
2024-12-26 14:40:36 +0800

    refactor: Introduce Strong Type Key for StringParameters and Corresponding Unit Tests

---
+++ Sources/Everglow.UnitTests/Functions/StringParameterKeyTest.cs
@@ -0,0 +1,54 @@
+using Everglow.Commons.UI.StringDrawerSystem;
+
+namespace Everglow.UnitTests.Functions;
+
+[TestClass]
+public class StringParameterKeyTest
+{
+	[TestMethod]
+	public void SameValuesAreEqual()
+	{
+		var key = "key";
+		var keyObj1 = new StringParameterKey(key);
+		var keyObj2 = new StringParameterKey(key);
+
+		Assert.AreEqual(keyObj1, keyObj2);
+	}
+
+	[TestMethod]
+	public void DifferentValuesAreUnequal()
+	{
+		var key1 = "key1";
+		var keyObj1 = new StringParameterKey(key1);
+
+		var key2 = "key2";
+		var keyObj2 = new StringParameterKey(key2);
+
+		Assert.AreNotEqual(keyObj1, keyObj2);
+	}
+
+	[TestMethod]
+	public void DifferentTypesAreUnequal()
+	{
+		var key = "TestKey";
+		var StringParameterKey = new StringParameterKey(key);
+
+		Assert.AreNotEqual(key, StringParameterKey);
+	}
+
+	[TestMethod]
+	public void OperatorsWorkCorrectly()
+	{
+		var key1 = "Test1";
+		var same1 = new StringParameterKey(key1);
+		var same2 = new StringParameterKey(key1);
+
+		var key2 = "Test2";
+		var different = new StringParameterKey(key2);
+
+		Assert.IsTrue(same1 == same2);
+		Assert.IsTrue(same1 != different);
+		Assert.IsFalse(same1 == different);
+		Assert.IsFalse(same1 != same2);
+	}
+}
