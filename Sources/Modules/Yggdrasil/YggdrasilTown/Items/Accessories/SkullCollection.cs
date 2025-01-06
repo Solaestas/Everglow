309571269bf47d23273fd1b7f28da9c2d95769c6
Cloudea <2025140062@qq.com>
2025-01-06 21:17:04 +0800

    feat(Yggdrasil): New textures

--- Sources/Modules/Yggdrasil/YggdrasilTown/Items/Accessories/SkullCollection.cs
+++ Sources/Modules/Yggdrasil/YggdrasilTown/Items/Accessories/SkullCollection.cs
@@ -1,7 +1,11 @@
+using Terraria.DataStructures;
+
 namespace Everglow.Yggdrasil.YggdrasilTown.Items.Accessories;
 
 public class SkullCollection : ModItem
 {
+	public const int ItemFrames = 7;
+
 	public const int DetectRange = 96;
 	public const int BuffDuration = 180;
 	public const int BuffCooldownMax = 120;
@@ -9,11 +13,16 @@ public class SkullCollection : ModItem
 
 	public int BuffCooldown { get; set; }
 
+	public override void SetStaticDefaults()
+	{
+		Main.RegisterItemAnimation(Item.type, new DrawAnimationVertical(10, ItemFrames));
+	}
+
 	public override void SetDefaults()
 	{
 		Item.accessory = true;
-		Item.width = 28;
-		Item.height = 20;
+		Item.width = 30;
+		Item.height = 35;
 
 		Item.rare = ItemRarityID.Orange;
 		Item.value = Item.buyPrice(platinum: 0, gold: 2);
@@ -21,7 +30,6 @@ public class SkullCollection : ModItem
 
 	public override void UpdateAccessory(Player player, bool hideVisual)
 	{
-
 		if (--BuffCooldown < 0)
 		{
 			BuffCooldown = 0;
@@ -35,8 +43,7 @@ public class SkullCollection : ModItem
 			}
 
 			// If there's any enemy within 6 blocks / 96 pixels around the player
-			var distance = Vector2.Distance(npc.Center, player.Center);
-			if (distance <= DetectRange)
+			if (Vector2.Distance(npc.Center, player.Center) <= DetectRange)
 			{
 				// 1. Add a 3s Mana Regeneration buff to the caster, then this effect goes to a 2s cooldown
 				// ========================================================================================
@@ -52,5 +59,14 @@ public class SkullCollection : ModItem
 				break;
 			}
 		}
+	}
+
+	public override bool PreDrawInWorld(SpriteBatch spriteBatch, Color lightColor, Color alphaColor, ref float rotation, ref float scale, int whoAmI)
+	{
+		Texture2D texture = ModContent.Request<Texture2D>(Texture).Value;
+		var frame = texture.Frame(verticalFrames: ItemFrames, frameY: ((int)Main.time / 10) % ItemFrames);
+		spriteBatch.Draw(texture, Item.Center - Main.screenPosition, frame, lightColor, 0, new Vector2(texture.Width, texture.Height / ItemFrames) / 2, 1f, SpriteEffects.None, 0);
+
+		return false;
 	}
 }
