01b0e721e3f31954bd4bc46316aadafbc11f1494
Cloudea <cloudeasoft@qq.com>
2025-01-13 17:08:31 +0800

    fix(Food System): Mark FoodRecipes as an abstract class to avoid invalid reference

--- Sources/Modules/Food/FoodRecipes/FoodRecipes.cs
+++ Sources/Modules/Food/FoodRecipes/FoodRecipes.cs
@@ -1,15 +1,6 @@
-using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Text;
-using System.Threading.Tasks;
-using Everglow.Food.Items.Ingredients;
-using Everglow.Food.Items.ModFood;
-using Terraria.ID;
-
 namespace Everglow.Food.FoodRecipes;
 
-public class FoodRecipes : ModSystem
+public abstract class FoodRecipes : ModSystem
 {
 
 	public List<CookingUnitWithOrder> CookingUnitWithOrderMenu;
