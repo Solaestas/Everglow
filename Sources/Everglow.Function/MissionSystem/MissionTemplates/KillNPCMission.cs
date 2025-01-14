8eec31255fc2dbad6aacdf457455a04ff5c62f64
Cloudea <cloudeasoft@qq.com>
2025-01-14 16:34:32 +0800

    fix(Mission): Add npc individual kill counter persistence code to KillNPCMission

--- Sources/Everglow.Function/MissionSystem/MissionTemplates/KillNPCMission.cs
+++ Sources/Everglow.Function/MissionSystem/MissionTemplates/KillNPCMission.cs
@@ -76,6 +76,18 @@ public abstract class KillNPCMission : MissionBase
 	{
 		base.LoadData(tag);
 
+		tag.TryGet<List<KillNPCRequirement>>(nameof(DemandNPCs), out var demandNPCs);
+		if (demandNPCs != null && demandNPCs.Count != 0)
+		{
+			foreach (var demand in DemandNPCs.Where(d => d.EnableIndividualCounter))
+			{
+				demand.Count(
+					demandNPCs
+						.Where(d => d.EnableIndividualCounter && d.NPCs.Intersect(demand.NPCs).Any())
+						.Sum(x => x.Counter));
+			}
+		}
+
 		LoadVanillaNPCTextures(DemandNPCs.SelectMany(x => x.NPCs));
 		LoadVanillaItemTextures(RewardItems.Select(x => x.type));
 	}
@@ -83,5 +95,7 @@ public abstract class KillNPCMission : MissionBase
 	public override void SaveData(TagCompound tag)
 	{
 		base.SaveData(tag);
+
+		tag.Add(nameof(DemandNPCs), DemandNPCs);
 	}
 }
