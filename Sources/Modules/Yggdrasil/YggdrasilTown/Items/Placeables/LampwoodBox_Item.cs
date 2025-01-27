ac975019ebdbff397a5110855b4c91ad1002ce8d
Felixyang777 <1761610526@qq.com>
2025-01-27 22:19:04 +0800

    feat(Yggdrasil): Add new items and tiles for three fish box

---
+++ Sources/Modules/Yggdrasil/YggdrasilTown/Items/Placeables/LampwoodBox_Item.cs
@@ -0,0 +1,94 @@
+using Terraria;
+using Terraria.GameContent.ItemDropRules;
+using Terraria.ID;
+using Terraria.ModLoader;
+using Everglow.Yggdrasil.YggdrasilTown.Tiles.LampWood.Furniture;
+
+namespace ExampleMod.Content.Items.Consumables
+{
+
+	public class LampwoodBox_Item : ModItem
+	{
+		public override void SetStaticDefaults()
+		{
+			Item.ResearchUnlockCount = 10;
+		}
+
+		public override void SetDefaults()
+		{
+			Item.DefaultToPlaceableTile(ModContent.TileType<LampwoodBox>());
+			Item.width = 12;
+			Item.height = 12;
+			Item.rare = ItemRarityID.Orange;
+			Item.value = Item.sellPrice(0, 0, 10);
+		}
+
+		public override void ModifyResearchSorting(ref ContentSamples.CreativeHelper.ItemGroup itemGroup)
+		{
+			itemGroup = ContentSamples.CreativeHelper.ItemGroup.Crates;
+		}
+
+		public override bool CanRightClick()
+		{
+			return true;
+		}
+
+		public override void ModifyItemLoot(ItemLoot itemLoot)
+		{
+			int[] themedDrops = new int[] {
+			};
+			itemLoot.Add(ItemDropRule.OneFromOptionsNotScalingWithLuck(1, themedDrops));
+			itemLoot.Add(ItemDropRule.Common(ItemID.GoldCoin, 4, 5, 13));
+			IItemDropRule[] oreTypes = new IItemDropRule[] {
+				ItemDropRule.Common(ItemID.CopperOre, 1, 30, 50),
+				ItemDropRule.Common(ItemID.TinOre, 1, 30, 50),
+				ItemDropRule.Common(ItemID.IronOre, 1, 30, 50),
+				ItemDropRule.Common(ItemID.LeadOre, 1, 30, 50),
+				ItemDropRule.Common(ItemID.SilverOre, 1, 30, 50),
+				ItemDropRule.Common(ItemID.TungstenOre, 1, 30, 50),
+				ItemDropRule.Common(ItemID.GoldOre, 1, 30, 50),
+				ItemDropRule.Common(ItemID.PlatinumOre, 1, 30, 50),
+
+			};
+			itemLoot.Add(new OneFromRulesRule(7, oreTypes));
+
+			// Drop pre-hm bars (except copper/tin), with the addition of one from ExampleMod
+			IItemDropRule[] oreBars = new IItemDropRule[] {
+				ItemDropRule.Common(ItemID.IronBar, 1, 10, 21),
+				ItemDropRule.Common(ItemID.LeadBar, 1, 10, 21),
+				ItemDropRule.Common(ItemID.SilverBar, 1, 10, 21),
+				ItemDropRule.Common(ItemID.TungstenBar, 1, 10, 21),
+				ItemDropRule.Common(ItemID.GoldBar, 1, 10, 21),
+				ItemDropRule.Common(ItemID.PlatinumBar, 1, 10, 21),
+
+			};
+			itemLoot.Add(new OneFromRulesRule(4, oreBars));
+
+			// Drop an "exploration utility" potion, with the addition of one from ExampleMod
+			IItemDropRule[] explorationPotions = new IItemDropRule[] {
+				ItemDropRule.Common(ItemID.ObsidianSkinPotion, 1, 2, 5),
+				ItemDropRule.Common(ItemID.SpelunkerPotion, 1, 2, 5),
+				ItemDropRule.Common(ItemID.HunterPotion, 1, 2, 5),
+				ItemDropRule.Common(ItemID.GravitationPotion, 1, 2, 5),
+				ItemDropRule.Common(ItemID.MiningPotion, 1, 2, 5),
+				ItemDropRule.Common(ItemID.HeartreachPotion, 1, 2, 5),
+
+			};
+			itemLoot.Add(new OneFromRulesRule(4, explorationPotions));
+
+			// Drop (pre-hm) resource potion
+			IItemDropRule[] resourcePotions = new IItemDropRule[] {
+				ItemDropRule.Common(ItemID.HealingPotion, 1, 5, 18),
+				ItemDropRule.Common(ItemID.ManaPotion, 1, 5, 18),
+			};
+			itemLoot.Add(new OneFromRulesRule(2, resourcePotions));
+
+			// Drop (high-end) bait
+			IItemDropRule[] highendBait = new IItemDropRule[] {
+				ItemDropRule.Common(ItemID.JourneymanBait, 1, 2, 7),
+				ItemDropRule.Common(ItemID.MasterBait, 1, 2, 7),
+			};
+			itemLoot.Add(new OneFromRulesRule(2, highendBait));
+		}
+	}
+}
---
+++ Sources/Modules/Yggdrasil/YggdrasilTown/Tiles/FurnaceTiles/UrbanBox.cs
@@ -0,0 +1,33 @@
+using Everglow.Yggdrasil.YggdrasilTown.Dusts;
+using Everglow.Yggdrasil.YggdrasilTown.TwilightForest.Dusts;
+using Terraria.DataStructures;
+using Terraria.Localization;
+using Terraria.ObjectData;
+
+namespace Everglow.Yggdrasil.YggdrasilTown.Tiles.FurnaceTiles;
+
+public class UrbanBox : ModTile
+{
+	public override void SetStaticDefaults()
+	{
+		// Properties
+		Main.tileFrameImportant[Type] = true;
+		Main.tileSolidTop[Type] = true;
+		Main.tileTable[Type] = true;
+
+		// Placement
+		TileObjectData.newTile.CopyFrom(TileObjectData.Style2x2);
+		TileObjectData.newTile.CoordinateHeights = new int[2] { 16, 18 };
+		TileObjectData.newTile.StyleHorizontal = true; // Optional, if you add more placeStyles for the item 
+		TileObjectData.addTile(Type);
+
+		// Etc
+		LocalizedText name = CreateMapEntryName();
+		AddMapEntry(new Color(200, 200, 200), name);
+	}
+
+	public override bool CreateDust(int i, int j, ref int type)
+	{
+		return false;
+	}
+}
