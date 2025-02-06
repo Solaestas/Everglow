1e20d34116bc05af5720d3d42ec69ef24d52f105
Cloudea <cloudeasoft@qq.com>
2025-02-06 11:28:15 +0800

    feat(Yggdrasil): Add 4 new weapons of twilight forest

---
+++ Sources/Modules/Yggdrasil/YggdrasilTown/Items/Weapons/TwilightForest/TwilightEucalyptusBoomerangKnift.cs
@@ -0,0 +1,29 @@
+using Everglow.Yggdrasil.YggdrasilTown.Projectiles;
+
+namespace Everglow.Yggdrasil.YggdrasilTown.Items.Weapons.TwilightForest;
+
+public class TwilightEucalyptusBoomerangKnift : ModItem
+{
+	public override void SetDefaults()
+	{
+		Item.width = 50;
+		Item.height = 40;
+
+		Item.DamageType = DamageClass.Melee;
+		Item.damage = 30;
+		Item.knockBack = 0.4f;
+		Item.crit = 4;
+
+		Item.useTime = Item.useAnimation = 10;
+		Item.useStyle = ItemUseStyleID.Swing;
+		Item.UseSound = SoundID.Item1;
+		Item.autoReuse = true;
+		Item.noMelee = true;
+		Item.noUseGraphic = true;
+
+		Item.shoot = ModContent.ProjectileType<TwilightEucalyptusBoomerangKnift_Projectile>();
+		Item.shootSpeed = 16;
+	}
+
+	public override bool CanUseItem(Player player) => player.ownedProjectileCounts[Item.shoot] < 3;
+}
