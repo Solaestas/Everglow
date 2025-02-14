761094f58a933e45dba3a0332d7b3b8b969a089c
元素·万象 <877183629@qq.com>
2025-02-14 20:17:13 +0800

    Union office door.

--- Sources/Modules/Yggdrasil/YggdrasilTown/Tiles/UnionOfficeDoorClosed.cs
+++ Sources/Modules/Yggdrasil/YggdrasilTown/Tiles/UnionOfficeDoorClosed.cs
@@ -1,6 +1,8 @@
+using Everglow.Yggdrasil.WorldGeneration;
 using Everglow.Yggdrasil.YggdrasilTown.Dusts;
-using Everglow.Yggdrasil.YggdrasilTown.Items.Placeables.Furniture.LampWood;
-using Everglow.Yggdrasil.YggdrasilTown.Tiles.LampWood.Furniture;
+using Everglow.Yggdrasil.YggdrasilTown.Items.Placeables;
+using Terraria.DataStructures;
+using Terraria.Enums;
 using Terraria.GameContent.ObjectInteractions;
 using Terraria.ObjectData;
 
@@ -20,21 +22,37 @@ namespace Everglow.Yggdrasil.YggdrasilTown.Tiles
 			TileID.Sets.DrawsWalls[Type] = true;
 			TileID.Sets.HasOutlines[Type] = true;
 			TileID.Sets.DisableSmartCursor[Type] = true;
-			TileID.Sets.OpenDoorID[Type] = ModContent.TileType<LampWoodDoorOpen>();
 
+			// TileID.Sets.OpenDoorID[Type] = ModContent.TileType<UnionOfficeDoorOpen>();
 			AddToArray(ref TileID.Sets.RoomNeeds.CountsAsDoor);
 
-			DustType = ModContent.DustType<LampWood_Dust>();
+			DustType = ModContent.DustType<UnionMarblePost_Dust_Khaki>();
 
 			AdjTiles = new int[] { TileID.ClosedDoor };
 
 			// Names
-			// AddMapEntry(new Color(200, 200, 200), Language.GetText("MapObject.Door"));
+			AddMapEntry(new Color(200, 200, 200));
 
 			// Placement
-			TileObjectData.newTile.CopyFrom(TileObjectData.GetTileData(TileID.ClosedDoor, 0));
 			TileObjectData.newTile.Width = 2;
 			TileObjectData.newTile.Height = 6;
+			TileObjectData.newTile.Origin = new Point16(0, 0);
+			TileObjectData.newTile.AnchorTop = new AnchorData(AnchorType.SolidTile, TileObjectData.newTile.Width, 0);
+			TileObjectData.newTile.AnchorBottom = new AnchorData(AnchorType.SolidTile, TileObjectData.newTile.Width, 0);
+			TileObjectData.newTile.UsesCustomCanPlace = true;
+			TileObjectData.newTile.LavaDeath = true;
+			TileObjectData.newTile.CoordinateHeights = new[] { 16, 16, 16, 16, 16, 16 };
+			TileObjectData.newTile.CoordinateWidth = 16;
+			TileObjectData.newTile.CoordinatePadding = 2;
+			TileObjectData.newTile.StyleHorizontal = false;
+			TileObjectData.newTile.StyleWrapLimit = 36;
+			TileObjectData.newTile.StyleLineSkip = 1; // When a door closes, each tile randomize between 3 different options. StyleLineSkip ensures that those tiles are interpreted as the correct style.
+			TileObjectData.newAlternate.CopyFrom(TileObjectData.newTile);
+			TileObjectData.newAlternate.Origin = new Point16(0, 1);
+			TileObjectData.addAlternate(0);
+			TileObjectData.newAlternate.CopyFrom(TileObjectData.newTile);
+			TileObjectData.newAlternate.Origin = new Point16(0, 2);
+			TileObjectData.addAlternate(0);
 			TileObjectData.addTile(Type);
 		}
 
@@ -53,7 +71,143 @@ namespace Everglow.Yggdrasil.YggdrasilTown.Tiles
 			Player player = Main.LocalPlayer;
 			player.noThrow = 2;
 			player.cursorItemIconEnabled = true;
-			player.cursorItemIconID = ModContent.ItemType<LampWoodDoor>();
+			player.cursorItemIconID = ModContent.ItemType<UnionOfficeDoor_Item>();
+		}
+
+		public override bool RightClick(int i, int j)
+		{
+			return base.RightClick(i, j);
+		}
+
+		public override void NearbyEffects(int i, int j, bool closer)
+		{
+			Player player = Main.LocalPlayer;
+			Tile tile = Main.tile[i, j];
+			int tileCoordX = i - tile.TileFrameX / 18;
+			int tileCootdY = j - tile.TileFrameY / 18;
+			Rectangle tileRectangle = new Rectangle(tileCoordX * 16, tileCootdY * 16, 32, 96);
+			Rectangle playerMoveBox = player.Hitbox;
+
+			// Test Code
+			// for (int t = 0; t < 20; t++)
+			// {
+			// Dust dust = Dust.NewDustDirect(new Vector2(tileCoordX * 16, tileCootdY * 16), 32, 96, DustID.ShimmerSpark);
+			// dust.velocity *= 0;
+			// dust.noGravity = true;
+			// }
+			playerMoveBox.X += (int)(player.velocity.X * 3) - 1;
+			playerMoveBox.Y += (int)(player.velocity.Y * 3);
+			playerMoveBox.Width += 2;
+			if (tileRectangle.Intersects(playerMoveBox))
+			{
+				int direction = 1;
+				if (player.Center.X > i * 16 + 8)
+				{
+					direction = -1;
+				}
+				if(CanOpenDoor(i, j, direction))
+				{
+					OpenOfficeDoor(i, j, direction);
+				}
+				else if (CanOpenDoor(i, j, -direction))
+				{
+					OpenOfficeDoor(i, j, -direction);
+				}
+			}
+		}
+
+		public bool CanOpenDoor(int i, int j, int direction)
+		{
+			Tile tile = Main.tile[i, j];
+			int originalTileFrameX = tile.TileFrameX;
+			int originalTileFrameY = tile.TileFrameY;
+			if (direction == 1)
+			{
+				for (int x = 0; x < 3; x++)
+				{
+					for (int y = 0; y < 6; y++)
+					{
+						Tile checkTile = YggdrasilWorldGeneration.SafeGetTile(i - originalTileFrameX / 18 + x + 2, j - originalTileFrameY / 18 + y);
+						if(checkTile.HasTile)
+						{
+							if (!Main.tileCut[checkTile.TileType])
+							{
+								return false;
+							}
+						}
+					}
+				}
+			}
+
+			if (direction == -1)
+			{
+				for (int x = 0; x < 3; x++)
+				{
+					for (int y = 0; y < 6; y++)
+					{
+						Tile checkTile = YggdrasilWorldGeneration.SafeGetTile(i - originalTileFrameX / 18 + x - 3, j - originalTileFrameY / 18 + y);
+						if (checkTile.HasTile)
+						{
+							if (!Main.tileCut[checkTile.TileType])
+							{
+								return false;
+							}
+						}
+					}
+				}
+			}
+			return true;
+		}
+
+		public void OpenOfficeDoor(int i, int j, int direction)
+		{
+			Tile tile = Main.tile[i, j];
+			int originalTileFrameX = tile.TileFrameX;
+			int originalTileFrameY = tile.TileFrameY;
+			var firstTile = YggdrasilWorldGeneration.SafeGetTile(i - originalTileFrameX / 18, j - originalTileFrameY / 18);
+			if (firstTile.TileType == Type)
+			{
+				for (int x = 0; x < 2; x++)
+				{
+					for (int y = 0; y < 6; y++)
+					{
+						Tile checkTile = YggdrasilWorldGeneration.SafeGetTile(i - originalTileFrameX / 18 + x, j - originalTileFrameY / 18 + y);
+						if (checkTile.TileType == Type)
+						{
+							checkTile.HasTile = false;
+						}
+					}
+				}
+			}
+			if (direction == 1)
+			{
+				for (int x = 0; x < 4; x++)
+				{
+					for (int y = 0; y < 6; y++)
+					{
+						Tile checkTile = YggdrasilWorldGeneration.SafeGetTile(i - originalTileFrameX / 18 + x + 1, j - originalTileFrameY / 18 + y);
+						checkTile.TileType = (ushort)ModContent.TileType<UnionOfficeDoorOpen>();
+						checkTile.TileFrameX = (short)(x * 18 + 72);
+						checkTile.TileFrameY = (short)(y * 18);
+						checkTile.HasTile = true;
+					}
+				}
+			}
+
+			if (direction == -1)
+			{
+				for (int x = 0; x < 4; x++)
+				{
+					for (int y = 0; y < 6; y++)
+					{
+						Tile checkTile = YggdrasilWorldGeneration.SafeGetTile(i - originalTileFrameX / 18 + x - 3, j - originalTileFrameY / 18 + y);
+						checkTile.TileType = (ushort)ModContent.TileType<UnionOfficeDoorOpen>();
+						checkTile.TileFrameX = (short)(x * 18);
+						checkTile.TileFrameY = (short)(y * 18);
+						checkTile.HasTile = true;
+					}
+				}
+			}
 		}
 	}
 }
