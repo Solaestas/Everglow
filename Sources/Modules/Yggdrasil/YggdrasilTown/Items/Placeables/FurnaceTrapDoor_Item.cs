b732276c7f591a4b61522152f9d06d7aa5dc1ed1
元素·万象 <877183629@qq.com>
2025-02-17 13:17:07 +0800

    Trap door

---
+++ Sources/Modules/Yggdrasil/YggdrasilTown/Items/Placeables/FurnaceTrapDoor_Item.cs
@@ -0,0 +1,13 @@
+using Everglow.Yggdrasil.YggdrasilTown.Tiles.FurnaceTiles;
+
+namespace Everglow.Yggdrasil.YggdrasilTown.Items.Placeables;
+
+public class FurnaceTrapDoor_Item : ModItem
+{
+	public override void SetDefaults()
+	{
+		Item.DefaultToPlaceableTile(ModContent.TileType<FurnaceTrapDoor>());
+		Item.width = 16;
+		Item.height = 16;
+	}
+}
