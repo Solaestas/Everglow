f8bd67c54d95e8fdbb5b95613aea4fffd317b277
Cloudea <cloudeasoft@qq.com>
2025-03-07 14:36:06 +0800

    feat(Mission): Enhance mission filter
    - Fix rotation issues
    - Add click-to-select functionality
    - Remove unused overrides

--- Sources/Everglow.Function/Mechanics/MissionSystem/UI/UIElements/UIMissionFilter.cs
+++ Sources/Everglow.Function/Mechanics/MissionSystem/UI/UIElements/UIMissionFilter.cs
@@ -6,47 +6,141 @@ namespace Everglow.Commons.Mechanics.MissionSystem.UI.UIElements;
 
 public class UIMissionFilter : BaseElement
 {
-	private float outerRotation = 0;
+	private const int MouseHoldFramesLimitForAutoRotation = 8;
+	private const float RotationSnapThreshold = 0.01f;
 
-	private float innerRotation = 0;
+	// Outer ring rotation state
+	private float _outerRotation;
+	private Vector2? _outerMouseDownPosition;
+	private float _outerMouseClickRotation;
+	private bool _outerHeld;
+	private int _outerClickTimer;
+	private float? _outerClickTargetRotation;
 
-	/// <summary>
-	/// Outer click position
-	/// </summary>
-	private Vector2? outerMouseClickPosition;
-
-	/// <summary>
-	/// Inner click position
-	/// </summary>
-	private Vector2? innerMouseClickPosition;
-
-	/// <summary>
-	/// The initial rotation of outer ring
-	/// </summary>
-	private float outerMouseClickRotation = 0;
-
-	/// <summary>
-	/// The initial rotation of inner ring
-	/// </summary>
-	private float innerMouseClickRotation = 0;
+	// Inner ring rotation state
+	private float _innerRotation;
+	private Vector2? _innerMouseDownPosition;
+	private float _innerMouseClickRotation;
+	private bool _innerHeld;
+	private int _innerClickTimer;
+	private float? _innerClickTargetRotation;
 
 	/// <summary>
-	/// If the outer ring is held
+	/// Available pool type selections (null represents "All" option)
 	/// </summary>
-	private bool outerHolding = false;
+	private static List<PoolType?> PoolTypeList { get; } = [null, .. Enum.GetValues<PoolType>()];
 
 	/// <summary>
-	/// If the inner ring is held
+	/// Available mission type selections (null represents "All" option)
 	/// </summary>
-	private bool innerHolding = false;
+	private static List<MissionType?> MissionTypeList { get; } = [null, .. Enum.GetValues<MissionType>()];
 
-	public static List<PoolType?> PoolTypeList => [null, .. Enum.GetValues<PoolType>()];
-
-	public static List<MissionType?> MissionTypeList => [null, .. Enum.GetValues<MissionType>()];
-
+	/// <summary>
+	/// Currently selected pool type filter
+	/// </summary>
 	public PoolType? PoolTypeValue { get; private set; }
 
+	/// <summary>
+	/// Currently selected mission type filter
+	/// </summary>
 	public MissionType? MissionTypeValue { get; private set; }
+
+	private float MouseRotation => HitBox.Center.ToVector2().AngleTo(Main.MouseScreen);
+
+	#region Statics
+
+	private static MissionType? RotationToMissionType(float rotation)
+	{
+		var unit = MathHelper.TwoPi / MissionTypeList.Count;
+		var standard = ((rotation % MathHelper.TwoPi) + MathHelper.TwoPi) % MathHelper.TwoPi;
+		var index = (int)Math.Round(standard / unit) % MissionTypeList.Count;
+		return MissionTypeList[index];
+	}
+
+	private static PoolType? RotationToPoolType(float rotation)
+	{
+		var unit = MathHelper.TwoPi / PoolTypeList.Count;
+		var standard = ((rotation % MathHelper.TwoPi) + MathHelper.TwoPi) % MathHelper.TwoPi;
+		var index = (int)Math.Round(standard / unit) % PoolTypeList.Count;
+		return PoolTypeList[index];
+	}
+
+	private static float MissionTypeToRotation(MissionType? type) => MissionTypeList.IndexOf(type) * MathHelper.TwoPi / MissionTypeList.Count;
+
+	private static float PoolTypeToRotation(PoolType? type) => PoolTypeList.IndexOf(type) * MathHelper.TwoPi / PoolTypeList.Count;
+
+	private static bool DistanceWithinInnerRing(float distance)
+	{
+		var innerRadius1 = 229 / 2f;
+		innerRadius1 *= MissionContainer.Scale;
+		var innerRadius2 = 229 / 2f - 42;
+		innerRadius2 *= MissionContainer.Scale;
+		return distance < innerRadius1 && distance > innerRadius2;
+	}
+
+	private static bool DistanceWithinOuterRing(float distance)
+	{
+		var outerRadius1 = 337 / 2f;
+		outerRadius1 *= MissionContainer.Scale;
+		var outerRadius2 = 337 / 2f - 48;
+		outerRadius2 *= MissionContainer.Scale;
+		return distance < outerRadius1 && distance > outerRadius2;
+	}
+
+	private static float GetNaturalRotation(float targetRotation, float originRotation)
+	{
+		var rotationDiff = (targetRotation - originRotation) % MathHelper.TwoPi;
+		if (MathF.Abs(rotationDiff) > MathF.PI)
+		{
+			if (rotationDiff > 0)
+			{
+				rotationDiff -= MathHelper.TwoPi;
+			}
+			else if (rotationDiff < 0)
+			{
+				rotationDiff += MathHelper.TwoPi;
+			}
+		}
+
+		return rotationDiff;
+	}
+
+	private static float CalculateNearestSnapRotation(float currentRotation, float optionCount)
+	{
+		if (optionCount <= 0)
+		{
+			throw new InvalidDataException();
+		}
+
+		var unit = MathHelper.TwoPi / optionCount;
+		var diff = currentRotation % MathHelper.TwoPi % unit;
+
+		if (diff > 0)
+		{
+			if (diff > unit / 2)
+			{
+				diff = unit - diff;
+			}
+			else
+			{
+				diff = -diff;
+			}
+		}
+		else if (diff < 0)
+		{
+			if (diff < -unit / 2)
+			{
+				diff = -unit - diff;
+			}
+			else
+			{
+				diff = -diff;
+			}
+		}
+		return currentRotation + diff;
+	}
+
+	#endregion
 
 	public override void OnInitialization()
 	{
@@ -64,8 +158,8 @@ public class UIMissionFilter : BaseElement
 	{
 		if (!(Main.mouseLeft && !Main.mouseLeftRelease))
 		{
-			innerHolding = false;
-			outerHolding = false;
+			_innerHeld = false;
+			_outerHeld = false;
 		}
 
 		var scale = MissionContainer.Scale;
@@ -73,7 +167,7 @@ public class UIMissionFilter : BaseElement
 		var mouseClicking = Main.mouseLeft && !Main.mouseLeftRelease;
 		var distanceToCenter = Main.MouseScreen.Distance(HitBox.Center.ToVector2());
 
-		if (((DistanceWithinOuterRing(distanceToCenter) && mouseClicking) || outerHolding) && !innerHolding)
+		if (((DistanceWithinOuterRing(distanceToCenter) && mouseClicking) || _outerHeld) && !_innerHeld)
 		{
 			OuterOnHover();
 		}
@@ -82,7 +176,7 @@ public class UIMissionFilter : BaseElement
 			OuterOnOut();
 		}
 
-		if (((DistanceWithinInnerRing(distanceToCenter) && mouseClicking) || innerHolding) && !outerHolding)
+		if (((DistanceWithinInnerRing(distanceToCenter) && mouseClicking) || _innerHeld) && !_outerHeld)
 		{
 			InnerOnHover();
 		}
@@ -92,66 +186,74 @@ public class UIMissionFilter : BaseElement
 		}
 	}
 
-	private static bool DistanceWithinInnerRing(float distance)
-	{
-		var innerRaidus1 = 229 / 2f;
-		innerRaidus1 *= MissionContainer.Scale;
-		var innerRadius2 = 229 / 2f - 42;
-		innerRadius2 *= MissionContainer.Scale;
-		return distance < innerRaidus1 && distance > innerRadius2;
-	}
-
-	private static bool DistanceWithinOuterRing(float distance)
-	{
-		var outerRadius1 = 337 / 2f;
-		outerRadius1 *= MissionContainer.Scale;
-		var outerRadius2 = 337 / 2f - 48;
-		outerRadius2 *= MissionContainer.Scale;
-		return distance < outerRadius1 && distance > outerRadius2;
-	}
-
 	private void OuterOnOut()
 	{
-		outerMouseClickPosition = null;
-	}
-
-	private void OuterOnHover()
-	{
-		if (outerMouseClickPosition == null)
-		{
-			outerMouseClickPosition = Main.MouseScreen;
-			outerMouseClickRotation = outerRotation;
-			outerHolding = true;
-		}
-
-		var enterAngle = HitBox.Center.ToVector2().AngleTo(outerMouseClickPosition.Value);
-		var currentAngle = HitBox.Center.ToVector2().AngleTo(Main.MouseScreen);
-		outerRotation = outerMouseClickRotation + currentAngle - enterAngle;
+		_outerMouseDownPosition = null;
 	}
 
 	private void InnerOnOut()
 	{
-		innerMouseClickPosition = null;
+		_innerMouseDownPosition = null;
+	}
+
+	private void OuterOnHover()
+	{
+		if (_outerMouseDownPosition == null)
+		{
+			_outerMouseDownPosition = Main.MouseScreen;
+			_outerMouseClickRotation = _outerRotation;
+			_outerHeld = true;
+		}
+
+		var enterAngle = HitBox.Center.ToVector2().AngleTo(_outerMouseDownPosition.Value);
+		_outerRotation = _outerMouseClickRotation + MouseRotation - enterAngle;
 	}
 
 	private void InnerOnHover()
 	{
-		if (innerMouseClickPosition == null)
+		if (_innerMouseDownPosition == null)
 		{
-			innerMouseClickPosition = Main.MouseScreen;
-			innerMouseClickRotation = innerRotation;
-			innerHolding = true;
+			_innerMouseDownPosition = Main.MouseScreen;
+			_innerMouseClickRotation = _innerRotation;
+			_innerHeld = true;
 		}
 
-		var enterAngle = HitBox.Center.ToVector2().AngleTo(innerMouseClickPosition.Value);
-		var currentAngle = HitBox.Center.ToVector2().AngleTo(Main.MouseScreen);
-		innerRotation = innerMouseClickRotation + currentAngle - enterAngle;
+		var enterAngle = HitBox.Center.ToVector2().AngleTo(_innerMouseDownPosition.Value);
+		_innerRotation = _innerMouseClickRotation + MouseRotation - enterAngle;
+	}
+
+	private void OuterRotatedTo(float rotation)
+	{
+		var rotationDiff = GetNaturalRotation(rotation, _outerRotation);
+
+		if (MathF.Abs(rotationDiff) > RotationSnapThreshold)
+		{
+			_outerRotation += rotationDiff / 10;
+		}
+		else
+		{
+			_outerRotation += rotationDiff;
+		}
+	}
+
+	private void InnerRotatedTo(float rotation)
+	{
+		var rotationDiff = GetNaturalRotation(rotation, _innerRotation);
+
+		if (MathF.Abs(rotationDiff) > RotationSnapThreshold)
+		{
+			_innerRotation += rotationDiff / 10;
+		}
+		else
+		{
+			_innerRotation += rotationDiff;
+		}
 	}
 
 	public override void Update(GameTime gt)
 	{
 		// Update mission type
-		var missionType = RotationToMissionType(outerRotation);
+		var missionType = RotationToMissionType(_outerRotation);
 		if (MissionTypeValue != missionType)
 		{
 			MissionTypeValue = missionType;
@@ -159,110 +261,61 @@ public class UIMissionFilter : BaseElement
 		}
 
 		// Update pool type
-		var poolType = RotationToPoolType(innerRotation);
+		var poolType = RotationToPoolType(_innerRotation);
 		if (PoolTypeValue != poolType)
 		{
 			PoolTypeValue = poolType;
 			MissionManager.NeedRefresh = true;
 		}
 
-		if (!innerHolding)
-		{
-			var num = PoolTypeList.Count;
-			var unit = MathHelper.TwoPi / num;
-			var exceed = innerRotation % unit;
-			if (exceed > unit / 2)
-			{
-				var delta = unit - exceed;
-				if (delta > 0.01f)
-				{
-					innerRotation += delta / 10;
-				}
-				else
-				{
-					innerRotation += delta;
-				}
-			}
-			else
-			{
-				var delta = exceed;
-				if (delta > 0.01f)
-				{
-					innerRotation -= delta / 10;
-				}
-				else
-				{
-					innerRotation -= delta;
-				}
-			}
-		}
-
-		if (!outerHolding)
-		{
-			var num = MissionTypeList.Count;
-			var unit = MathHelper.TwoPi / num;
-			var exceed = outerRotation % unit;
-			if (exceed > unit / 2)
-			{
-				var delta = unit - exceed;
-				if (delta > 0.01f)
-				{
-					outerRotation += delta / 10;
-				}
-				else
-				{
-					outerRotation += delta;
-				}
-			}
-			else
-			{
-				var delta = exceed;
-				if (delta > 0.01f)
-				{
-					outerRotation -= delta / 10;
-				}
-				else
-				{
-					outerRotation -= delta;
-				}
-			}
-		}
+		ManageAutoRotation();
 	}
 
-	public static MissionType? RotationToMissionType(float rotation)
+	private void ManageAutoRotation()
 	{
-		var unit = MathHelper.TwoPi / MissionTypeList.Count;
-		var standard = ((rotation % MathHelper.TwoPi) + MathHelper.TwoPi) % MathHelper.TwoPi;
-		var index = Math.Round(standard / unit) % MissionTypeList.Count;
-		return index switch
+		// Inner spin
+		if (_innerHeld)
 		{
-			0 => null,
-			1 => MissionType.None,
-			2 => MissionType.MainStory,
-			3 => MissionType.SideStory,
-			4 => MissionType.Legendary,
-			5 => MissionType.Achievement,
-			6 => MissionType.Daily,
-			7 => MissionType.Challenge,
-			_ => null,
-		};
-	}
+			_innerClickTimer++;
+			_innerClickTargetRotation = null;
+		}
+		else
+		{
+			if (_innerClickTimer != 0 && _innerClickTimer < MouseHoldFramesLimitForAutoRotation)
+			{
+				var clickedPoolType = RotationToPoolType(MathHelper.Pi - MouseRotation + _innerRotation);
+				_innerClickTargetRotation = PoolTypeToRotation(clickedPoolType);
+			}
 
-	public static PoolType? RotationToPoolType(float rotation)
-	{
-		var unit = MathHelper.TwoPi / PoolTypeList.Count;
-		var standard = ((rotation % MathHelper.TwoPi) + MathHelper.TwoPi) % MathHelper.TwoPi;
-		var index = Math.Round(standard / unit) % PoolTypeList.Count;
-		return index switch
+			_innerClickTimer = 0;
+
+			// If there's no click target rotation, then fix the rotation to nearest snap.
+			_innerClickTargetRotation ??= CalculateNearestSnapRotation(_innerRotation, PoolTypeList.Count);
+
+			InnerRotatedTo(_innerClickTargetRotation.Value);
+		}
+
+		// Outer spin
+		if (_outerHeld)
 		{
-			0 => null,
-			1 => PoolType.Accepted,
-			2 => PoolType.Available,
-			3 => PoolType.Failed,
-			4 => PoolType.Overdue,
-			5 => PoolType.Completed,
-			_ => null,
-		};
+			_outerClickTimer++;
+			_outerClickTargetRotation = null;
+		}
+		else
+		{
+			if (_outerClickTimer != 0 && _outerClickTimer < MouseHoldFramesLimitForAutoRotation)
+			{
+				var clickedMissionType = RotationToMissionType(MathHelper.Pi - MouseRotation + _outerRotation);
+				_outerClickTargetRotation = MissionTypeToRotation(clickedMissionType);
+			}
+
+			_outerClickTimer = 0;
+
+			// If there's no click target rotation, then fix the rotation to nearest snap.
+			_outerClickTargetRotation ??= CalculateNearestSnapRotation(_outerRotation, MissionTypeList.Count);
+
+			OuterRotatedTo(_outerClickTargetRotation.Value);
+		}
 	}
 
 	public override void Draw(SpriteBatch sb)
@@ -273,22 +326,12 @@ public class UIMissionFilter : BaseElement
 		var scale = MissionContainer.Scale;
 
 		var typeTexture = ModAsset.MissionClassificationMarbleRing.Value;
-		sb.Draw(typeTexture, drawPos, null, Color.White, outerRotation, typeTexture.Size() / 2, scale, SpriteEffects.None, 0);
+		sb.Draw(typeTexture, drawPos, null, Color.White, _outerRotation, typeTexture.Size() / 2, scale, SpriteEffects.None, 0);
 
 		var typeGlow = ModAsset.GoldRingTexture.Value;
-		sb.Draw(typeGlow, drawPos, null, Color.White, outerRotation, typeGlow.Size() / 2, scale, SpriteEffects.None, 0);
+		sb.Draw(typeGlow, drawPos, null, Color.White, _outerRotation, typeGlow.Size() / 2, scale, SpriteEffects.None, 0);
 
 		var statusFilter = ModAsset.MissionDurationMarbleRing.Value;
-		sb.Draw(statusFilter, drawPos, null, Color.White, innerRotation, statusFilter.Size() / 2, scale, SpriteEffects.None, 0);
-	}
-
-	protected override void DrawChildren(SpriteBatch sb)
-	{
-		base.DrawChildren(sb);
-	}
-
-	protected override void DrawSelf(SpriteBatch sb)
-	{
-		base.DrawSelf(sb);
+		sb.Draw(statusFilter, drawPos, null, Color.White, _innerRotation, statusFilter.Size() / 2, scale, SpriteEffects.None, 0);
 	}
 }
