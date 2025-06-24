dad1b4374d6ec43b0ab83a6427cc6d9ecff40428
元素·万象 <877183629@qq.com>
2025-06-25 00:50:14 +0800

    visual effect opt.

--- Sources/Modules/Yggdrasil/KelpCurtain/Projectiles/Summon/WoodlandWraithStaff_SporeZone.cs
+++ Sources/Modules/Yggdrasil/KelpCurtain/Projectiles/Summon/WoodlandWraithStaff_SporeZone.cs
@@ -43,25 +43,59 @@ public class WoodlandWraithStaff_SporeZone : ModProjectile
 
 	public override bool PreDraw(ref Color lightColor)
 	{
+		float timeValue = (float)(Main.time * 0.0002f);
 		Vector2 drawPos = Projectile.Center - Main.screenPosition;
 		List<Vertex2D> bars = new List<Vertex2D>();
+		List<Vertex2D> barsCenter = new List<Vertex2D>();
 		var drawColor = new Color(220, 220, 239, 0);
-		if (Projectile.timeLeft < 60)
+
+		for (int i = 0; i <= 100; i++)
 		{
-			drawColor = drawColor * (Projectile.timeLeft / 60f);
+			var colorTile = Lighting.GetColor((drawPos + new Vector2(Range + 40, 0).RotatedBy(i / 100f * MathHelper.TwoPi) + Main.screenPosition).ToTileCoordinates());
+			var ringColor = Color.Lerp(drawColor, colorTile, 0.95f);
+			if (Projectile.timeLeft < 60)
+			{
+				ringColor *= Projectile.timeLeft / 60f;
+			}
+			ringColor.A = 0;
+			bars.Add(drawPos + new Vector2(Range + 40, 0).RotatedBy(i / 100f * MathHelper.TwoPi), ringColor * 0, new Vector3(i / 100f * 4f, timeValue, 0));
+			bars.Add(drawPos + new Vector2(Range + 20, 0).RotatedBy(i / 100f * MathHelper.TwoPi), ringColor, new Vector3(i / 100f * 4f, 0.05f + timeValue, 0));
 		}
 		for (int i = 0; i <= 100; i++)
 		{
-			bars.Add(drawPos + new Vector2(Range + 20, 0).RotatedBy(i / 100f * MathHelper.TwoPi), drawColor, Vector3.zero);
-			bars.Add(drawPos + new Vector2(Range, 0).RotatedBy(i / 100f * MathHelper.TwoPi), drawColor, Vector3.zero);
+			var colorTile = Lighting.GetColor((drawPos + new Vector2(Range + 20, 0).RotatedBy(i / 100f * MathHelper.TwoPi) + Main.screenPosition).ToTileCoordinates());
+			var ringColor = Color.Lerp(drawColor, colorTile, 0.95f);
+			if (Projectile.timeLeft < 60)
+			{
+				ringColor *= Projectile.timeLeft / 60f;
+			}
+			ringColor.A = 0;
+			bars.Add(drawPos + new Vector2(Range + 20, 0).RotatedBy(i / 100f * MathHelper.TwoPi), ringColor, new Vector3(i / 100f * 4f, 0.05f + timeValue, 0));
+			bars.Add(drawPos + new Vector2(Range, 0).RotatedBy(i / 100f * MathHelper.TwoPi), ringColor * 0, new Vector3(i / 100f * 4f, timeValue, 0));
+		}
+
+		for (int i = 0; i <= 100; i++)
+		{
+			var colorTile = Lighting.GetColor((drawPos + new Vector2(Range + 30, 0).RotatedBy(i / 100f * MathHelper.TwoPi) + Main.screenPosition).ToTileCoordinates());
+			var ringColor = Color.Lerp(drawColor, colorTile, 0.8f);
+			if (Projectile.timeLeft < 60)
+			{
+				ringColor *= Projectile.timeLeft / 60f;
+			}
+			ringColor.A = 0;
+			barsCenter.Add(drawPos + new Vector2(Range + 30, 0).RotatedBy(i / 100f * MathHelper.TwoPi), ringColor, new Vector3(i / 100f * 4f, 0, 0));
+			barsCenter.Add(drawPos + new Vector2(Range + 10, 0).RotatedBy(i / 100f * MathHelper.TwoPi), ringColor, new Vector3(i / 100f * 4f, 1, 0));
 		}
 		if (bars.Count > 0)
 		{
 			SpriteBatchState sBS = GraphicsUtils.GetState(Main.spriteBatch).Value;
 			Main.spriteBatch.End();
 			Main.spriteBatch.Begin(SpriteSortMode.Immediate, BlendState.AlphaBlend, SamplerState.PointWrap, DepthStencilState.None, RasterizerState.CullNone, null, Main.GameViewMatrix.TransformationMatrix);
-			Main.graphics.GraphicsDevice.Textures[0] = Commons.ModAsset.White.Value;
+			Main.graphics.GraphicsDevice.Textures[0] = Commons.ModAsset.Noise_forceField_medium.Value;
 			Main.graphics.GraphicsDevice.DrawUserPrimitives(PrimitiveType.TriangleStrip, bars.ToArray(), 0, bars.Count - 2);
+
+			Main.graphics.GraphicsDevice.Textures[0] = Commons.ModAsset.Trail_17.Value;
+			Main.graphics.GraphicsDevice.DrawUserPrimitives(PrimitiveType.TriangleStrip, barsCenter.ToArray(), 0, barsCenter.Count - 2);
 			Main.spriteBatch.End();
 			Main.spriteBatch.Begin(sBS);
 		}
