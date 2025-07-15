9a267bc6c65b6c01c5df64e719c20e9a32ebc57a
Cloudea <2025140062@qq.com>
2025-07-15 22:56:07 +0800

    fix(KelpCurtain): Fix bugs of witherbark set appearance

--- Sources/Modules/Yggdrasil/KelpCurtain/Items/Armors/Witherbark/WitherbarkBreastPlate.cs
+++ Sources/Modules/Yggdrasil/KelpCurtain/Items/Armors/Witherbark/WitherbarkBreastPlate.cs
@@ -1,98 +1,73 @@
 using Terraria.DataStructures;
 using Terraria.GameContent.Creative;
 
-namespace Everglow.Yggdrasil.KelpCurtain.Items.Armors.Witherbark
+namespace Everglow.Yggdrasil.KelpCurtain.Items.Armors.Witherbark;
+
+[AutoloadEquip(EquipType.Body)]
+public class WitherbarkBreastPlate : ModItem
 {
-	[AutoloadEquip(EquipType.Body)]
-	public class WitherbarkBreastPlate : ModItem
+	public override void SetStaticDefaults()
 	{
-		public override void SetStaticDefaults()
+		CreativeItemSacrificesCatalog.Instance.SacrificeCountNeededByItemId[Type] = 1;
+	}
+
+	public override void SetDefaults()
+	{
+		Item.width = 26;
+		Item.height = 28;
+
+		Item.rare = ItemRarityID.Green;
+		Item.value = Item.buyPrice(0, 0, 60, 0);
+
+		Item.defense = 2;
+	}
+
+	public override void UpdateEquip(Player player)
+	{
+		player.whipRangeMultiplier += 0.2f;
+	}
+
+	public abstract class WitherbarkBreasCloakLayer : PlayerDrawLayer
+	{
+		public virtual int XOffsetValue => 10;
+
+		public abstract int YOffsetValue { get; }
+
+		public abstract Texture2D CloakTexture { get; }
+
+		public sealed override bool GetDefaultVisibility(PlayerDrawSet drawInfo) => drawInfo.drawPlayer.body == EquipLoader.GetEquipSlot(ModIns.Mod, nameof(WitherbarkBreastPlate), EquipType.Body);
+
+		public override void Draw(ref PlayerDrawSet drawInfo)
 		{
-			CreativeItemSacrificesCatalog.Instance.SacrificeCountNeededByItemId[Type] = 1;
-		}
+			// Direction and gravity adaption.
+			float xOffset = -XOffsetValue * drawInfo.drawPlayer.direction;
+			float yOffset = -YOffsetValue * drawInfo.drawPlayer.gravDir;
+			float rotation = drawInfo.drawPlayer.gravDir > 0 ? 0f : MathHelper.Pi;
+			SpriteEffects effect = drawInfo.drawPlayer.direction * drawInfo.drawPlayer.gravDir > 0 ? SpriteEffects.None : SpriteEffects.FlipHorizontally;
 
-		public override void SetDefaults()
-		{
-			Item.width = 26;
-			Item.height = 28;
-
-			Item.rare = ItemRarityID.Green;
-			Item.value = Item.buyPrice(0, 0, 60, 0);
-
-			Item.defense = 2;
-		}
-
-		public override void UpdateEquip(Player player)
-		{
-			player.whipRangeMultiplier += 0.2f;
+			var position = drawInfo.Center + new Vector2(xOffset, yOffset) - Main.screenPosition;
+			position = new Vector2((int)position.X, (int)position.Y);
+			Rectangle frame = drawInfo.drawPlayer.legFrame;
+			var drawData = new DrawData(CloakTexture, position, frame, drawInfo.colorArmorBody, rotation, new Vector2(frame.Width * 0.5f, 0), 1f, effect, 0);
+			drawInfo.DrawDataCache.Add(drawData);
 		}
 	}
 
-	public class WitherbarkBreasCloakLayer_Front : PlayerDrawLayer
+	public class WitherbarkBreasCloakLayer_Front : WitherbarkBreasCloakLayer
 	{
-		public override bool GetDefaultVisibility(PlayerDrawSet drawInfo)
-		{
-			return drawInfo.drawPlayer.armor[1] != null && drawInfo.drawPlayer.armor[1].type == ModContent.ItemType<WitherbarkBreastPlate>();
-		}
+		public override int YOffsetValue => 25;
+
+		public override Texture2D CloakTexture => ModAsset.WitherbarkBreastPlate_CloakFront.Value;
 
 		public override Position GetDefaultPosition() => new AfterParent(PlayerDrawLayers.ArmOverItem);
-
-		public override void Draw(ref PlayerDrawSet drawInfo)
-		{
-			Texture2D cloak = ModAsset.WitherbarkBreastPlate_CloakFront.Value;
-
-			var position = drawInfo.Center + new Vector2(0f, -7f) - Main.screenPosition;
-			position = new Vector2((int)position.X, (int)position.Y); // You'll sometimes want to do this, to avoid quivering.
-			Rectangle frame = drawInfo.drawPlayer.legFrame;
-			Color drawColor = drawInfo.colorArmorBody;
-			drawInfo.DrawDataCache.Add(new DrawData(
-				cloak,
-				position,
-				frame,
-				drawColor,
-				0f,
-				frame.Size() * 0.5f,
-				1f,
-				SpriteEffects.None,
-				0));
-		}
 	}
 
-	public class WitherbarkBreasCloakLayer_Back : PlayerDrawLayer
+	public class WitherbarkBreasCloakLayer_Back : WitherbarkBreasCloakLayer
 	{
-		public override bool GetDefaultVisibility(PlayerDrawSet drawInfo)
-		{
-			return drawInfo.drawPlayer.armor[1] != null && drawInfo.drawPlayer.armor[1].type == ModContent.ItemType<WitherbarkBreastPlate>();
-		}
+		public override int YOffsetValue => 29;
+
+		public override Texture2D CloakTexture => ModAsset.WitherbarkBreastPlate_CloakBack.Value;
 
 		public override Position GetDefaultPosition() => new BeforeParent(PlayerDrawLayers.Leggings);
-
-		public override void Draw(ref PlayerDrawSet drawInfo)
-		{
-			Texture2D cloak = ModAsset.WitherbarkBreastPlate_CloakBack.Value;
-
-			var position = drawInfo.Center + new Vector2(0f, -5f) - Main.screenPosition;
-			position = new Vector2((int)position.X, (int)position.Y); // You'll sometimes want to do this, to avoid quivering.
-			Rectangle frame = drawInfo.drawPlayer.legFrame;
-			Color drawColor = drawInfo.colorArmorBody;
-			drawInfo.DrawDataCache.Add(new DrawData(
-				cloak,
-				position,
-				frame,
-				drawColor,
-				0f,
-				frame.Size() * 0.5f,
-				1f,
-				SpriteEffects.None,
-				0));
-		}
-	}
-
-	public class WitherbarkLeggingsPlayer : ModPlayer
-	{
-		public override void ModifyDrawInfo(ref PlayerDrawSet drawInfo)
-		{
-			base.ModifyDrawInfo(ref drawInfo);
-		}
 	}
 }
