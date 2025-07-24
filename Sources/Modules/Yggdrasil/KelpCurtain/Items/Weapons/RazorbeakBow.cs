0f4489cf606f403e06f079ed7b67426d47fe7c1f
Cloudea <2025140062@qq.com>
2025-07-24 20:29:30 +0800

    fix(KelpCurtain): Adjust details of razorbeak bow

--- Sources/Modules/Yggdrasil/KelpCurtain/Items/Weapons/RazorbeakBow.cs
+++ Sources/Modules/Yggdrasil/KelpCurtain/Items/Weapons/RazorbeakBow.cs
@@ -1,5 +1,6 @@
 using Everglow.Commons.Graphics;
 using ReLogic.Graphics;
+using Terraria.Audio;
 using Terraria.DataStructures;
 using Terraria.GameContent;
 using Terraria.UI.Chat;
@@ -20,8 +21,8 @@ public class RazorbeakBow : ModItem
 		Item.damage = 11;
 		Item.crit = 4;
 		Item.knockBack = 3f;
+		Item.scale = 0.9f;
 
-		Item.UseSound = SoundID.Item5;
 		Item.useStyle = ItemUseStyleID.Shoot;
 		Item.autoReuse = true;
 		Item.noMelee = true;
@@ -43,81 +44,84 @@ public class RazorbeakBow : ModItem
 	public override float UseTimeMultiplier(Player player)
 	{
 		int stack = player.GetModPlayer<RazorbeakBowPlayer>().RazorbeakBowEffectStack;
-		var mul = 2 / (1 + stack * RazorbeakBowPlayer.RazorbeakBowAttackSpeedBonusPerStack);
-		return stack > 0 ? mul : 2f;
+		return stack > 0 ? 1f / (1 + stack * RazorbeakBowPlayer.RazorbeakBowAttackSpeedBonusPerStack) : 1f;
 	}
 
 	public override float UseAnimationMultiplier(Player player) => UseTimeMultiplier(player);
 
-	public override void HoldItem(Player player)
-	{
-		Item.reuseDelay = (int)(BaseDelay * UseTimeMultiplier(player));
-	}
+	public override void HoldItem(Player player) => Item.reuseDelay = (int)(BaseDelay * UseTimeMultiplier(player));
+
+	public override Vector2? HoldoutOffset() => new Vector2(-8, -4);
 
 	public override bool Shoot(Player player, EntitySource_ItemUse_WithAmmo source, Vector2 position, Vector2 velocity, int type, int damage, float knockback)
 	{
 		var modPlayer = player.GetModPlayer<RazorbeakBowPlayer>();
 
+		// Initialize group info before the first shot in group.
 		if (player.ItemAnimationJustStarted)
 		{
 			modPlayer.GroupIndex++;
-			modPlayer.RazorbeakHitInfo.Add(modPlayer.GroupIndex, (0, false));
+			modPlayer.RazorbeakHitInfo.TryAdd(modPlayer.GroupIndex, (0, false));
 		}
 
-		int useTime = CombinedHooks.TotalUseTime(Item.useTime, player, Item);
-		if (player.itemAnimation <= useTime // not enough time to shoot again
-			|| (Item.useLimitPerAnimation != null && player.ItemUsesThisAnimation == Item.useLimitPerAnimation)) // this shot hits the limit
+		// Mark the group as done before the last shot in group.
+		bool isFinalShot = player.itemAnimation <= CombinedHooks.TotalUseTime(Item.useTime, player, Item) // not enough time to shoot again
+			|| (Item.useLimitPerAnimation != null && player.ItemUsesThisAnimation == Item.useLimitPerAnimation); // this shot hits the limit
+		if (isFinalShot && modPlayer.RazorbeakHitInfo.TryGetValue(modPlayer.GroupIndex, out var value))
 		{
-			if (modPlayer.RazorbeakHitInfo.TryGetValue(modPlayer.GroupIndex, out var value))
-			{
-				modPlayer.RazorbeakHitInfo[modPlayer.GroupIndex] = (value.Count, true); // Mark the group as done
-			}
+			modPlayer.RazorbeakHitInfo[modPlayer.GroupIndex] = (value.Count, true); // Mark the group as done
 		}
 
 		var proj = Projectile.NewProjectileDirect(source, position, velocity, type, damage, knockback, player.whoAmI);
-		proj.GetGlobalProjectile<RazorbeakGlobalProjectile>().ShotByRazorbeakBow = true;
-		proj.GetGlobalProjectile<RazorbeakGlobalProjectile>().GroupIndex = modPlayer.GroupIndex;
+		SoundEngine.PlaySound(Item.UseSound = SoundID.Item5, position);
+
+		// Set global projectile properties
+		var gProj = proj.GetGlobalProjectile<RazorbeakGlobalProjectile>();
+		gProj.ShotByRazorbeakBow = true;
+		gProj.GroupIndex = modPlayer.GroupIndex;
+
 		return false;
 	}
 
 	public override void PostDrawInInventory(SpriteBatch spriteBatch, Vector2 position, Rectangle frame, Color drawColor, Color itemColor, Vector2 origin, float scale)
 	{
-		if (Main.LocalPlayer.HeldItem.ModItem == this)
+		if (Main.LocalPlayer.HeldItem.ModItem != this)
 		{
-			// Draw text
-			var modPlayer = Main.LocalPlayer.GetModPlayer<RazorbeakBowPlayer>();
-			var text = $"{1 + modPlayer.RazorbeakBowEffectStack * RazorbeakBowPlayer.RazorbeakBowAttackSpeedBonusPerStack}x";
-			var progress = modPlayer.RazorbeakBowEffectStack / (float)RazorbeakBowPlayer.RazorbeakBowEffectMaxStack;
+			return;
+		}
 
-			var stringSize = ChatManager.GetStringSize(FontAssets.MouseText.Value, text, Vector2.One);
-			var drawPos = Main.LocalPlayer.Bottom - Main.screenPosition;
-			var textColor = new Color(
-				Color.LimeGreen.G / 255f * (0.9f + progress * 0.2f),
-				MathHelper.Lerp(1f, Color.LimeGreen.R / 255f * (0.7f + progress * 0.3f), progress),
-				Color.LimeGreen.B / 255f)
-				* (0.9f + 0.1f * MathF.Sin((float)Main.timeForVisualEffects * 0.04f));
-			var textScale = 1f + 0.05f * modPlayer.RazorbeakBowEffectStack;
+		// Draw text
+		var modPlayer = Main.LocalPlayer.GetModPlayer<RazorbeakBowPlayer>();
+		var progress = modPlayer.RazorbeakBowEffectStack / (float)RazorbeakBowPlayer.RazorbeakBowEffectMaxStack;
 
-			var sBS = GraphicsUtils.GetState(spriteBatch).Value;
-			spriteBatch.End();
-			spriteBatch.Begin(sBS);
+		var text = $"{1 + modPlayer.RazorbeakBowEffectStack * RazorbeakBowPlayer.RazorbeakBowAttackSpeedBonusPerStack}x";
+		var textSize = ChatManager.GetStringSize(FontAssets.MouseText.Value, text, Vector2.One);
+		var textPos = Main.LocalPlayer.Bottom - Main.screenPosition;
+		var textColor = new Color(
+			Color.LimeGreen.G / 255f * (0.9f + progress * 0.2f),
+			MathHelper.Lerp(1f, Color.LimeGreen.R / 255f * (0.7f + progress * 0.3f), progress),
+			Color.LimeGreen.B / 255f)
+			* (0.9f + 0.1f * MathF.Sin((float)Main.timeForVisualEffects * 0.04f));
+		var textScale = 1f + 0.05f * modPlayer.RazorbeakBowEffectStack;
 
-			spriteBatch.transformMatrix = Main.GameViewMatrix.ZoomMatrix;
-			spriteBatch.DrawString(FontAssets.MouseText.Value, text, drawPos, textColor, 0, new Vector2(stringSize.X * 0.5f, -stringSize.Y * 0.5f), textScale, SpriteEffects.None, 0);
+		var sBS = GraphicsUtils.GetState(spriteBatch).Value;
+		spriteBatch.End();
+		spriteBatch.Begin(sBS);
 
+		spriteBatch.transformMatrix = Main.GameViewMatrix.ZoomMatrix;
+		spriteBatch.DrawString(FontAssets.MouseText.Value, text, textPos, textColor, 0, new Vector2(textSize.X * 0.5f, -textSize.Y * 0.5f), textScale, SpriteEffects.None, 0);
+
+		// Draw timer
+		float timer = modPlayer.RazorbeakBowTimer / (float)RazorbeakBowPlayer.RazorbeakBowEffectDuration;
+		if (timer > 0)
+		{
 			spriteBatch.End();
 			spriteBatch.Begin(SpriteSortMode.Immediate, BlendState.AlphaBlend, SamplerState.PointClamp, default, RasterizerState.CullNone, null, Main.GameViewMatrix.ZoomMatrix);
-
-			// Draw timer
-			float timer = modPlayer.RazorbeakBowTimer / (float)RazorbeakBowPlayer.RazorbeakBowEffectDuration;
-			if (timer > 0)
-			{
-				ValueBarHelper.DrawCircleValueBar(spriteBatch, drawPos + new Vector2(0, 50f * textScale), 1 - timer, Color.Black, Color.White, 0.08f);
-			}
-
-			spriteBatch.End();
-			spriteBatch.Begin(sBS);
+			ValueBarHelper.DrawCircleValueBar(spriteBatch, textPos + new Vector2(0, 50f * textScale), 1 - timer, Color.Black, Color.White, 0.08f);
 		}
+
+		spriteBatch.End();
+		spriteBatch.Begin(sBS);
 	}
 
 	public class RazorbeakBowPlayer : ModPlayer
@@ -155,14 +159,15 @@ public class RazorbeakBow : ModItem
 
 		public bool ShotByRazorbeakBow { get; set; } = false;
 
+		public bool RazorbeakBowEffectTriggered { get; set; } = false;
+
 		public int GroupIndex { get; set; }
 
 		public override void OnHitNPC(Projectile projectile, NPC target, NPC.HitInfo hit, int damageDone)
 		{
-			if (ShotByRazorbeakBow)
+			if (ShotByRazorbeakBow && !RazorbeakBowEffectTriggered)
 			{
-				var owner = Main.player[projectile.owner];
-				var modPlayer = owner.GetModPlayer<RazorbeakBowPlayer>();
+				var modPlayer = Main.player[projectile.owner].GetModPlayer<RazorbeakBowPlayer>();
 
 				// Reset hit effect timer.
 				modPlayer.RazorbeakBowTimer = RazorbeakBowPlayer.RazorbeakBowEffectDuration;
@@ -170,6 +175,8 @@ public class RazorbeakBow : ModItem
 				// Process effect counter.
 				if (modPlayer.RazorbeakHitInfo.TryGetValue(GroupIndex, out var value))
 				{
+					RazorbeakBowEffectTriggered = true;
+
 					var count = value.Count + 1;
 					if (count < RazorbeakBowPlayer.RazorbeakBowEffectHitRequirement)
 					{
@@ -188,16 +195,19 @@ public class RazorbeakBow : ModItem
 		{
 			if (ShotByRazorbeakBow)
 			{
-				// Remove the group index from the player's RazorbeakHitInfo if no other arrows of the same group are active.
+				// Remove useless group index from the player's RazorbeakHitInfo。
 				var modPlayer = Main.player[projectile.owner].GetModPlayer<RazorbeakBowPlayer>();
-				if (!Main.projectile.Where(proj =>
-						proj.active
-						&& proj.owner == projectile.owner
-						&& proj.type == projectile.type
-						&& proj.whoAmI != projectile.whoAmI)
-					.Any(p => p.GetGlobalProjectile<RazorbeakGlobalProjectile>().GroupIndex == GroupIndex)
-						&& modPlayer.RazorbeakHitInfo.TryGetValue(GroupIndex, out var hitInfo)
-						&& hitInfo.Done)
+				if (!modPlayer.RazorbeakHitInfo.TryGetValue(GroupIndex, out var hitInfo) || !hitInfo.Done)
+				{
+					return;
+				}
+
+				if (!Main.projectile.Any(proj =>
+					proj.active
+					&& proj.owner == projectile.owner
+					&& proj.type == projectile.type
+					&& proj.whoAmI != projectile.whoAmI
+					&& proj.GetGlobalProjectile<RazorbeakGlobalProjectile>().GroupIndex == GroupIndex))
 				{
 					modPlayer.RazorbeakHitInfo.Remove(GroupIndex);
 				}
