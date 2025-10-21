f052ceb1fcc8635f9b730d1b27a230cb477ee774
元素·万象 <877183629@qq.com>
2025-10-22 00:03:43 +0800

    fix some path name.

--- Sources/Modules/SpellAndSkull/Dusts/LightDust.cs
+++ Sources/Modules/SpellAndSkull/Dusts/LightDust.cs
@@ -2,13 +2,15 @@ using Everglow.Commons.VFX;
 using Everglow.Commons.VFX.Pipelines;
 using Everglow.Commons.VFX.Visuals;
 
-namespace Everglow.SpellAndSkull.Projectiles.BlackHole.Dust;
+namespace Everglow.SpellAndSkull.Dusts;
 
 [Pipeline(typeof(WCSPipeline))]
-internal class DarkDust : Particle
+internal class LightDust : Particle
 {
 	public int time;
 	public int time_max;
+	public Color drawColor;
+
 	public override void AI()
 	{
 		time++;
@@ -18,19 +20,24 @@ internal class DarkDust : Particle
 			scale *= 0.9f;
 		}
 		if (time > time_max)
+		{
 			Kill();
+		}
 		else
 		{
 			velocity = velocity.RotatedBy(0.05f);
 		}
 	}
+
 	public override void Draw()
 	{
-		Texture2D tex = ModAsset.BlackDust.Value;
-		for (int i = 0; i < 5; i++)
+		Texture2D tex = ModAsset.BlackHole_lightSpot.Value;
+		for (int i = 0; i < 8; i++)
 		{
-			float a = (5 - i) / 5f;
-			Ins.Batch.Draw(tex, position - velocity * i * 0.5f, null, Color.White * a, velocity.ToRotation(), tex.Size() / 2, new Vector2(1.2f, 0.8f) * scale, 0);
+			Color c = drawColor;
+			c.A = 0;
+			float a = (8 - i) / 8f;
+			Ins.Batch.Draw(tex, position - velocity * i * 0.4f, null, c * 1.2f * a, velocity.ToRotation(), tex.Size() / 2, new Vector2(1.2f, 0.8f) * scale * i * 0.5f, 0);
 		}
 	}
 }
