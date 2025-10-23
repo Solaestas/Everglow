63294647dfc0fae0880f53ae77a196af9f998da6
Cloudea <cloudeasoft@qq.com>
2025-10-23 18:55:17 +0800

    fix(Function): Adjust allowed prefixes of yoyos

--- Sources/Everglow.Function/Templates/Weapons/Yoyos/YoyoItem.cs
+++ Sources/Everglow.Function/Templates/Weapons/Yoyos/YoyoItem.cs
@@ -1,4 +1,5 @@
 using Terraria.GameContent.Creative;
+using Terraria.GameContent.Prefixes;
 
 namespace Everglow.Commons.Templates.Weapons.Yoyos;
 
@@ -8,7 +9,6 @@ public abstract class YoyoItem : ModItem
 
 	public override void SetStaticDefaults()
 	{
-		// These are all related to gamepad controls and don't seem to affect anything else
 		ItemID.Sets.Yoyo[Item.type] = true; // Used to increase the gamepad range when using Strings.
 		ItemID.Sets.GamepadExtraRange[Item.type] = 15; // Increases the gamepad range. Some vanilla values: 4 (Wood), 10 (Valor), 13 (Yelets), 18 (The Eye of Cthulhu), 21 (Terrarian).
 		ItemID.Sets.GamepadSmartQuickReach[Item.type] = true; // Unused, but weapons that require aiming on the screen are in this set.
@@ -59,27 +59,12 @@ public abstract class YoyoItem : ModItem
 	{
 	}
 
-	// Here is an example of blacklisting certain modifiers. Remove this section for standard vanilla behavior.
-	// In this example, we are blacklisting the ones that reduce damage of a melee weapon.
-	// Make sure that your item can even receive these prefixes (check the vanilla wiki on prefixes).
-	private static readonly int[] unwantedPrefixes = new int[] { PrefixID.Terrible, PrefixID.Dull, PrefixID.Shameful, PrefixID.Annoying, PrefixID.Broken, PrefixID.Damaged, PrefixID.Shoddy };
-
-	public override bool AllowPrefix(int pre)
-	{
-		// return false to make the game reroll the prefix.
-
-		// DON'T DO THIS BY ITSELF:
-		// return false;
-		// This will get the game stuck because it will try to reroll every time. Instead, make it have a chance to return true.
-		if (Array.IndexOf(unwantedPrefixes, pre) > -1)
-		{
-			// IndexOf returns a positive index of the element you search for. If not found, it's less than 0.
-			// Here we check if the selected prefix is positive (it was found).
-			// If so, we found a prefix that we don't want. Reroll.
-			return false;
-		}
-
-		// Don't reroll
-		return true;
-	}
+	/// <summary>
+	/// Only allows prefixes that are appropriate for yoyos.
+	/// <br/>Most of yoyos: <see cref="PrefixLegacy.Prefixes.PrefixesForBoomeransAndChakrums"/>
+	/// <br/>Terrarian: <see cref="PrefixLegacy.Prefixes.PrefixesForBoomeransAndChakrums_TerrarianYoyo"/>
+	/// </summary>
+	/// <param name="pre"></param>
+	/// <returns></returns>
+	public override bool AllowPrefix(int pre) => PrefixLegacy.Prefixes.PrefixesForBoomeransAndChakrums.Any(p => p == pre);
 }
