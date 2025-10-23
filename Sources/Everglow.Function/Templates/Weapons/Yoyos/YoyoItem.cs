7b2d70aaec5824ac6f4b9d647b2474efa17a3070
Cloudea <cloudeasoft@qq.com>
2025-10-23 19:45:30 +0800

    refactor: optimize yoyo template

--- Sources/Everglow.Function/Templates/Weapons/Yoyos/YoyoItem.cs
+++ Sources/Everglow.Function/Templates/Weapons/Yoyos/YoyoItem.cs
@@ -15,46 +15,38 @@ public abstract class YoyoItem : ModItem
 		CreativeItemSacrificesCatalog.Instance.SacrificeCountNeededByItemId[Type] = 1;
 	}
 
-	public override void SetDefaults()
+	public sealed override void SetDefaults()
 	{
 		Item.width = 24;
 		Item.height = 24;
+
+		Item.DamageType = DamageClass.MeleeNoSpeed;
+		Item.knockBack = 2.5f;
+
+		Item.useStyle = ItemUseStyleID.Shoot;
 		Item.useAnimation = 25;
 		Item.useTime = 25;
+		Item.UseSound = SoundID.Item1;
 		Item.noMelee = true;
 		Item.noUseGraphic = true;
 		Item.channel = true;
+
 		Item.shootSpeed = 16f;
-		Item.knockBack = 2.5f;
-		Item.DamageType = DamageClass.MeleeNoSpeed;
-		Item.useStyle = ItemUseStyleID.Shoot;
-		Item.UseSound = SoundID.Item1;
-		Item.DamageType = DamageClass.Melee;
+
 		SetCustomDefaults();
 	}
 
 	/// <summary>
-	/// Default Values:
-	/// ItemID.Sets.Yoyo[Item.type] = true;<br></br>
-	/// ItemID.Sets.GamepadExtraRange[Item.type] = 15;<br></br>
-	/// ItemID.Sets.GamepadSmartQuickReach[Item.type] = true;<br></br>
-	/// CreativeItemSacrificesCatalog.Instance.SacrificeCountNeededByItemId[Type] = 1;<br></br>
-	/// Item.useStyle = ItemUseStyleID.Shoot;<br></br>
-	/// Item.width = 24;<br></br>
-	/// Item.height = 24;<br></br>
-	/// Item.noUseGraphic = true;<br></br>
-	/// Item.UseSound = SoundID.Item1;<br></br>
-	/// Item.DamageType = DamageClass.Melee;<br></br>
-	/// Item.channel = true;<br></br>
-	/// Item.useAnimation = 5;<br></br>
-	/// Item.useTime = 5;<br></br>
-	/// Item.shootSpeed = 0f;<br></br>
-	/// Item.knockBack = 5f;<br></br>
-	/// Item.noMelee = true;<br></br>
-	/// ItemID.Sets.Yoyo[Item.type] = true;<br></br><br></br>
-	/// Necessary fields: value, shoot, damage, rare;<br></br>
-	/// Vanilla Reference Price: Wooden Yoyo_100, Rally_5000, Malaise_10000, Hel-Fire_40000, Terrarian_100000
+	/// This is where you set all your item's properties, such as width, damage, shootSpeed, defense, etc.
+	/// <br/>Same to <see cref="ModItem.SetDefaults"/>. Looking for base values in <see cref="YoyoItem.SetDefaults"/>.
 	/// </summary>
+	/// <remarks>
+	/// Necessary defaults:
+	/// <br/><see cref="Item.shoot"/>
+	/// <br/><see cref="Item.damage"/>
+	/// <br/><see cref="Item.rare"/>
+	/// <br/><see cref="Item.value"/>
+	/// </remarks>
 	public virtual void SetCustomDefaults()
 	{
 	}
