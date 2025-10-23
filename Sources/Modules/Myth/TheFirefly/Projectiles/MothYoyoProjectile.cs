5e731025ddc9964506abdb1867b394529b8db03e
Omnielement <103014909+Omnielement@users.noreply.github.com>
2025-10-23 20:36:05 +0800

    refactor(Function): Optimize template of Yoyo. (#325)
    * Finish yoyo projectile.
    Allow modify yoyo string(draw and even you can do some predicate by the string unit pos)
    The code can now be better adapted to vanilla.
    
    * fix(Function): Optimize code style of YoyoProjectile, fix some default values and comment.
    
    * feat(Function): Optimize yoyo ai
    
    * fix(Function): Resolve some reference error and renaming properties for readability.
    
    * fix(Function): Fix yoyo implementations
    
    * fix(Function): Adjust allowed prefixes of yoyos
    
    * refactor: optimize yoyo template
    
    * fix(Function): Fix yoyo implementations
    
    ---------
    
    Co-authored-by: Cloudea <cloudeasoft@qq.com>

--- Sources/Modules/Myth/TheFirefly/Projectiles/MothYoyoProjectile.cs
+++ Sources/Modules/Myth/TheFirefly/Projectiles/MothYoyoProjectile.cs
@@ -1,37 +1,27 @@
-﻿using Everglow.Myth.TheFirefly.Items.Accessories;
-using Terraria.GameContent;
+using Everglow.Commons.Templates.Weapons.Yoyos;
+using Everglow.Myth.TheFirefly.Items.Accessories;
 
 namespace Everglow.Myth.TheFirefly.Projectiles;
 
-internal class MothYoyoProjectile : ModProjectile
+internal class MothYoyoProjectile : YoyoProjectile
 {
-	FireflyBiome fireflyBiome = ModContent.GetInstance<FireflyBiome>();
-	private readonly Vector3[] cubeVec = new Vector3[]
-	{
-		new Vector3(1,1,1),
-		new Vector3(1,1,-1),
-		new Vector3(1,-1,-1),
-		new Vector3(1,-1,1),
-		new Vector3(-1,1,1),
-		new Vector3(-1,1,-1),
-		new Vector3(-1,-1,-1),
-		new Vector3(-1,-1,1)
-	};
+	private readonly Vector3[] cubeVec =
+	[
+		new Vector3(1, 1, 1),
+		new Vector3(1, 1, -1),
+		new Vector3(1, -1, -1),
+		new Vector3(1, -1, 1),
+		new Vector3(-1, 1, 1),
+		new Vector3(-1, 1, -1),
+		new Vector3(-1, -1, -1),
+		new Vector3(-1, -1, 1),
+	];
 
-	public override void SetDefaults()
+	public override void SetStaticDefaults()
 	{
 		ProjectileID.Sets.YoyosLifeTimeMultiplier[Projectile.type] = 40f;
 		ProjectileID.Sets.YoyosMaximumRange[Projectile.type] = 300f;
 		ProjectileID.Sets.YoyosTopSpeed[Projectile.type] = 16f;
-
-		Projectile.extraUpdates = 0;
-		Projectile.width = 16;
-		Projectile.height = 16;
-		Projectile.aiStyle = 99;
-		Projectile.friendly = true;
-		Projectile.penetrate = -1;
-		Projectile.DamageType = DamageClass.Melee;
-		Projectile.scale = 1f;
 	}
 
 	public override void PostAI()
@@ -47,7 +37,7 @@ internal class MothYoyoProjectile : ModProjectile
 			}
 			if (MothEye.LocalOwner != null && MothEye.LocalOwner.TryGetModPlayer(out MothEyePlayer mothEyePlayer))
 			{
-				if (mothEyePlayer.MothEyeEquipped && fireflyBiome.IsBiomeActive(Main.LocalPlayer) && Main.hardMode)
+				if (mothEyePlayer.MothEyeEquipped && ModContent.GetInstance<FireflyBiome>().IsBiomeActive(Main.LocalPlayer) && Main.hardMode)
 				{
 					for (int i = 0; i < cubeVec.Length; i++)
 					{
@@ -61,115 +51,11 @@ internal class MothYoyoProjectile : ModProjectile
 		}
 	}
 
-	public override bool PreDraw(ref Color lightColor)
+	public override void PostDraw(Color lightColor)
 	{
-		#region 抄抄原版的线（
-
-		Vector2 mountedCenter = Main.player[Projectile.owner].MountedCenter;
-		Vector2 vector = mountedCenter;
-		vector.Y += Main.player[Projectile.owner].gfxOffY;
-		float num4 = Projectile.Center.X - vector.X;
-		float num5 = Projectile.Center.Y - vector.Y;
-		Math.Sqrt((double)(num4 * num4 + num5 * num5));
-		float rotation = (float)Math.Atan2((double)num5, (double)num4) - 1.57f;
-		if (!Projectile.counterweight)
-		{
-			int num6 = -1;
-			if (Projectile.position.X + Projectile.width / 2 < Main.player[Projectile.owner].position.X + Main.player[Projectile.owner].width / 2)
-				num6 = 1;
-			num6 *= -1;
-			Main.player[Projectile.owner].itemRotation = (float)Math.Atan2((double)(num5 * num6), (double)(num4 * num6));
-		}
-		bool flag = true;
-		if (num4 == 0f && num5 == 0f)
-			flag = false;
-		else
-		{
-			float num7 = (float)Math.Sqrt((double)(num4 * num4 + num5 * num5));
-			num7 = 12f / num7;
-			num4 *= num7;
-			num5 *= num7;
-			vector.X -= num4 * 0.1f;
-			vector.Y -= num5 * 0.1f;
-			num4 = Projectile.position.X + Projectile.width * 0.5f - vector.X;
-			num5 = Projectile.position.Y + Projectile.height * 0.5f - vector.Y;
-		}
-		while (flag)
-		{
-			float num8 = 12f;
-			float num9 = (float)Math.Sqrt((double)(num4 * num4 + num5 * num5));
-			float num10 = num9;
-			if (float.IsNaN(num9) || float.IsNaN(num10))
-				flag = false;
-			else
-			{
-				if (num9 < 20f)
-				{
-					num8 = num9 - 8f;
-					flag = false;
-				}
-				num9 = 12f / num9;
-				num4 *= num9;
-				num5 *= num9;
-				vector.X += num4;
-				vector.Y += num5;
-				num4 = Projectile.position.X + Projectile.width * 0.5f - vector.X;
-				num5 = Projectile.position.Y + Projectile.height * 0.1f - vector.Y;
-				if (num10 > 12f)
-				{
-					float num11 = 0.3f;
-					float num12 = Math.Abs(Projectile.velocity.X) + Math.Abs(Projectile.velocity.Y);
-					if (num12 > 16f)
-						num12 = 16f;
-					num12 = 1f - num12 / 16f;
-					num11 *= num12;
-					num12 = num10 / 80f;
-					if (num12 > 1f)
-						num12 = 1f;
-					num11 *= num12;
-					if (num11 < 0f)
-						num11 = 0f;
-					num11 *= num12;
-					num11 *= 0.5f;
-					if (num5 > 0f)
-					{
-						num5 *= 1f + num11;
-						num4 *= 1f - num11;
-					}
-					else
-					{
-						num12 = Math.Abs(Projectile.velocity.X) / 3f;
-						if (num12 > 1f)
-							num12 = 1f;
-						num12 -= 0.5f;
-						num11 *= num12;
-						if (num11 > 0f)
-							num11 *= 2f;
-						num5 *= 1f + num11;
-						num4 *= 1f - num11;
-					}
-				}
-				rotation = (float)Math.Atan2((double)num5, (double)num4) - 1.57f;
-				Color color = Color.White;
-				color.A = (byte)(color.A * 0.4f);
-				color = new Color(0, 150, 255, 0) * 0.5f;//改个色
-														 //Main.NewText(color);
-														 //color = Lighting.GetColor((int)vector.X / 16, (int)(vector.Y / 16f), color);
-														 //color = new Microsoft.Xna.Framework.Color((int)((byte)((float)color.R * num13)), (int)((byte)((float)color.G * num13)), (int)((byte)((float)color.B * num13)), (int)((byte)((float)color.A * num13)));
-				Main.EntitySpriteDraw(TextureAssets.FishingLine.Value, new Vector2(vector.X - Main.screenPosition.X + TextureAssets.FishingLine.Width() * 0.5f, vector.Y - Main.screenPosition.Y + TextureAssets.FishingLine.Height() * 0.5f) - new Vector2(6f, 0f), new Rectangle?(new Rectangle(0, 0, TextureAssets.FishingLine.Width(), (int)num8)), color, rotation, new Vector2(TextureAssets.FishingLine.Width() * 0.5f, 0f), 1f, SpriteEffects.None, 0);
-			}
-		}
-
-		#endregion 抄抄原版的线（
-
 		Texture2D tex = ModContent.Request<Texture2D>(Texture).Value;
-		Main.EntitySpriteDraw(tex, Projectile.Center - Main.screenPosition, null, lightColor, Projectile.rotation, tex.Size() / 2, Projectile.scale, 0, 0);
-		Main.EntitySpriteDraw(tex, Projectile.Center - Main.screenPosition, null, Projectile.GetAlpha(lightColor), Projectile.rotation, tex.Size() / 2, Projectile.scale, 0, 0);
-		return false;
+		Main.EntitySpriteDraw(tex, Projectile.Center - Main.screenPosition, null, new Color(255, 255, 255, 0), Projectile.rotation, tex.Size() / 2, Projectile.scale, 0, 0);
 	}
 
-	public override Color? GetAlpha(Color lightColor)
-	{
-		return new Color(255, 255, 255, 0);
-	}
+	public override Color ModifyYoyoStringColor_VanillaRender(int playerStringColor, Vector2 worldPos, float index, float stringCount) => new Color(0, 150, 255, 0) * 0.5f;
 }
