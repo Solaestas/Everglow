718eecb5e80d502f3752aacaf7b5553b59790083
CloudeaSoft <cloudeasoft@qq.com>
2025-11-20 21:03:44 +0800

    refactor: Remove unused code in examples

--- Sources/Modules/Example/Items/ExampleStabbingSword.cs
+++ Sources/Modules/Example/Items/ExampleStabbingSword.cs
@@ -1,38 +1,19 @@
 using Everglow.Commons.Templates.Weapons.StabbingSwords;
 using Everglow.Example.Projectiles;
 
-namespace Everglow.Example.Items
+namespace Everglow.Example.Items;
+
+public class ExampleStabbingSword : StabbingSwordItem
 {
-	public class ExampleStabbingSword : StabbingSwordItem
+	public override void SetCustomDefault()
 	{
-		public int HoldingTimer = 0;
-
-		public override void SetCustomDefault()
-		{
-			StaminaCost += 0.3f;
-			Item.damage = 3;
-			Item.knockBack = 1f;
-			Item.rare = ItemRarityID.White;
-			Item.value = Item.sellPrice(0, 0, 12, 0);
-			Item.shoot = ModContent.ProjectileType<ExampleStabbingSword_Pro>();
-			PowerfulStabDamageFlat = 4f;
-			PowerfulStabProj = ModContent.ProjectileType<ExampleStabbingSword_Pro_Stab>();
-		}
-
-		public override void HoldItem(Player player)
-		{
-			// if (HoldingTimer == 0)
-			// {
-			// ExampleStabbingSword_Config config = new ExampleStabbingSword_Config()
-			// {
-			// Owner = player,
-			// Visible = true,
-			// Active = true,
-			// };
-			// Ins.VFXManager.Add(config);
-			// }
-			// HoldingTimer++;
-			base.HoldItem(player);
-		}
+		StaminaCost += 0.3f;
+		Item.damage = 3;
+		Item.knockBack = 1f;
+		Item.rare = ItemRarityID.White;
+		Item.value = Item.sellPrice(0, 0, 12, 0);
+		Item.shoot = ModContent.ProjectileType<ExampleStabbingSword_Pro>();
+		PowerfulStabDamageFlat = 4f;
+		PowerfulStabProj = ModContent.ProjectileType<ExampleStabbingSword_Pro_Stab>();
 	}
 }
--- Sources/Modules/Example/Items/ExampleStabbingSword_Config.cs
+++ Sources/Modules/Example/Items/ExampleStabbingSword_Config.cs
@@ -171,7 +171,6 @@ public class ExampleStabbingSword_Config : ModPlayer
 				}
 			}
 		}
-		base.UpdateEquips();
 	}
 
 	public void Draw()
