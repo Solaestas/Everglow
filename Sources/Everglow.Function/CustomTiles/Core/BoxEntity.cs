0d218fb34faa682dfc86a65fa32d09a449ddf07f
CloudeaSoft <cloudeasoft@qq.com>
2025-11-27 18:52:47 +0800

    fix: Unexpected behavior of box entity when position.Y is large (#341)
    Related Work Items: #3

--- Sources/Everglow.Function/CustomTiles/Core/BoxEntity.cs
+++ Sources/Everglow.Function/CustomTiles/Core/BoxEntity.cs
@@ -15,12 +15,84 @@ public class BoxEntity : RigidEntity, IBox, IHookable
 
 	public float Gravity => 1;
 
-	public override bool Collision(IBox obj, Vector2 stride, out CollisionResult result)
+	public override bool Collision(IBox other, Vector2 stride, out CollisionResult result)
 	{
 		result = default;
 		result.Collider = this;
 
-		AABB box = obj.Box;
+		AABB collider = Box;
+		Vector2 rvel = stride - this.Velocity;
+		Vector2 pos0 = other.Position;
+		var otherBox = other.Box;
+
+		if (!otherBox.Scan(rvel).Intersect(collider))
+		{
+			result.Stride = stride;
+			result.Normal = Vector2.Zero;
+			return false;
+		}
+
+		const float SmallScale = 7;
+		AABB smallBox = collider;
+		smallBox.TopLeft += new Vector2(SmallScale, SmallScale);
+		smallBox.BottomRight -= new Vector2(SmallScale, SmallScale);
+		if (smallBox.Intersect(otherBox))
+		{
+			result.Stride = stride;
+			result.Normal = Vector2.Zero;
+			return true;
+		}
+
+		bool onGround = other.Velocity.Y + Velocity.Y == 0;
+		bool? isX = null;
+		Vector2 target = otherBox.position + rvel;
+		do
+		{
+			otherBox.position = otherBox.position.Approach(target, 1);
+			if (otherBox.Intersect(collider, out var area))
+			{
+				isX = area.size.X < area.size.Y;
+				break;
+			}
+		}
+		while (otherBox.position != target);
+
+		if (isX is null)
+		{
+			result.Stride = stride;
+			result.Normal = Vector2.Zero;
+			return false;
+		}
+		else if (isX.Value)
+		{
+			otherBox.position.X = otherBox.Left < collider.Left ? collider.Left - otherBox.size.X : collider.Right;
+			otherBox.position.Y = target.Y;
+			result.Stride = otherBox.position - pos0 + this.Velocity;
+			result.Normal = otherBox.Left < collider.Left ? -Vector2.UnitX : Vector2.UnitX;
+			other.Velocity = other.Velocity with { X = this.Velocity.X };
+			return true;
+		}
+		else
+		{
+			otherBox.position.Y = otherBox.Top < collider.Top ? collider.Top - otherBox.size.Y : collider.Bottom;
+			otherBox.position.X = target.X;
+			result.Stride = otherBox.position - pos0 + this.Velocity;
+			other.Velocity = other.Velocity with
+			{
+				Y = this.Velocity.Y == 0
+					? onGround
+						? 0
+						: CollisionUtils.Epsilon * 10
+					: this.Velocity.Y,
+			};
+			result.Normal = otherBox.Top < collider.Top ? Vector2.UnitY : -Vector2.UnitY;
+			return true;
+		}
+
+		/*result = default;
+		result.Collider = this;
+
+		AABB box = other.Box;
 		AABB collider = Box;
 
 		if (box.Intersect(collider, true))
@@ -71,18 +143,18 @@ public class BoxEntity : RigidEntity, IBox, IHookable
 			{
 				result.Normal = new Vector2(0, Math.Sign(stride.Y));
 				result.Stride = stride with { Y = stride.Y * tyMin - 0.001f * Math.Sign(stride.Y) } + Velocity;
-				obj.Velocity = obj.Velocity with { Y = Velocity.Y };
+				other.Velocity = other.Velocity with { Y = Velocity.Y };
 			}
-			else if(txMin > tyMin)
+			else if (txMin > tyMin)
 			{
 				result.Normal = new Vector2(Math.Sign(stride.X), 0);
 				result.Stride = stride with { X = stride.X * txMin } + Velocity;
-				obj.Velocity = obj.Velocity with { X = Velocity.X };
+				other.Velocity = other.Velocity with { X = Velocity.X };
 			}
 			return true;
 		}
 
-		return false;
+		return false;*/
 	}
 
 	public override void Draw()
