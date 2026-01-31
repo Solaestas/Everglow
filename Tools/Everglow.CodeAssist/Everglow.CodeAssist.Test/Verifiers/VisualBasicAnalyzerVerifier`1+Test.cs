4a14e5cfbdb0e29b092fa7c88c4031fa5d2f13e7
Cloudea <cloudeasoft@qq.com>
2026-01-31 13:02:24 +0800

    feat: Add code assist template

---
+++ Tools/Everglow.CodeAssist/Everglow.CodeAssist.Test/Verifiers/VisualBasicAnalyzerVerifier`1+Test.cs
@@ -0,0 +1,17 @@
+﻿using Microsoft.CodeAnalysis.Diagnostics;
+using Microsoft.CodeAnalysis.Testing.Verifiers;
+using Microsoft.CodeAnalysis.VisualBasic.Testing;
+
+namespace Everglow.CodeAssist.Test
+{
+	public static partial class VisualBasicAnalyzerVerifier<TAnalyzer>
+		where TAnalyzer : DiagnosticAnalyzer, new()
+	{
+		public class Test : VisualBasicAnalyzerTest<TAnalyzer, MSTestVerifier>
+		{
+			public Test()
+			{
+			}
+		}
+	}
+}
---
+++ Tools/Everglow.CodeAssist/Everglow.CodeAssist.Test/Verifiers/VisualBasicCodeFixVerifier`2+Test.cs
@@ -0,0 +1,16 @@
+﻿using Microsoft.CodeAnalysis.CodeFixes;
+using Microsoft.CodeAnalysis.Diagnostics;
+using Microsoft.CodeAnalysis.Testing.Verifiers;
+using Microsoft.CodeAnalysis.VisualBasic.Testing;
+
+namespace Everglow.CodeAssist.Test
+{
+	public static partial class VisualBasicCodeFixVerifier<TAnalyzer, TCodeFix>
+		where TAnalyzer : DiagnosticAnalyzer, new()
+		where TCodeFix : CodeFixProvider, new()
+	{
+		public class Test : VisualBasicCodeFixTest<TAnalyzer, TCodeFix, MSTestVerifier>
+		{
+		}
+	}
+}
