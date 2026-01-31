978884e8b31b331f4384f4eea6c246f831faa046
Cloudea <cloudeasoft@qq.com>
2026-01-31 13:02:24 +0800

    feat: Add code assist template

---
+++ Tools/Everglow.CodeAssist/Everglow.CodeAssist.Test/Verifiers/VisualBasicCodeRefactoringVerifier`1+Test.cs
@@ -0,0 +1,14 @@
+﻿using Microsoft.CodeAnalysis.CodeRefactorings;
+using Microsoft.CodeAnalysis.Testing.Verifiers;
+using Microsoft.CodeAnalysis.VisualBasic.Testing;
+
+namespace Everglow.CodeAssist.Test
+{
+	public static partial class VisualBasicCodeRefactoringVerifier<TCodeRefactoring>
+		where TCodeRefactoring : CodeRefactoringProvider, new()
+	{
+		public class Test : VisualBasicCodeRefactoringTest<TCodeRefactoring, MSTestVerifier>
+		{
+		}
+	}
+}
