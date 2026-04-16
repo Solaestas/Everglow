b6cdd63952e69ecc2361e0c64f747d1c9b5ea549
CloudeaSoft <cloudeasoft@qq.com>
2026-04-16 08:36:19 +0800

    fix

--- Sources/Modules/Yggdrasil/Common/YggdrasilGlobalNPC.cs
+++ Sources/Modules/Yggdrasil/Common/YggdrasilGlobalNPC.cs
@@ -1,6 +1,18 @@
+using Everglow.Yggdrasil.YggdrasilTown.Buffs;
+
 namespace Everglow.Yggdrasil.Common;
 
 public class YggdrasilGlobalNPC : GlobalNPC
 {
+	public bool CharredActive { get; set; }
+
 	public override bool InstancePerEntity => true;
+
+	public override void ModifyIncomingHit(NPC npc, ref NPC.HitModifiers modifiers)
+	{
+		if (CharredActive)
+		{
+			modifiers.Defense.Flat -= npc.defense * Charred.DefenseReduction;
+		}
+	}
 }
